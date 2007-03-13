a7e6d2505d423e0e761477dee64dbb8a29e9570b
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-13 14:01:20 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@8 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/compras/modules/almrazcom/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * almrazcom actions.
+ *
+ * @package    siga
+ * @subpackage almrazcom
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class almrazcomActions extends autoalmrazcomActions
+{
+}
