7c7fde7510408094c377376c286d4017f737bd1a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-16 21:01:13 +0000

    Luis Hernández Se arreglo el problema con la tabla "empresa" al generar el modelo
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@97 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/Carecaud.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'carecaud' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Carecaud extends BaseCarecaud
+{
+}
---
+++ lib/model/CarecaudPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'carecaud' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class CarecaudPeer extends BaseCarecaudPeer
+{
+}
