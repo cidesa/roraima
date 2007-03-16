50032ae55eb2d113a2945fea8e0b23b7f6fdc005
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-16 13:55:23 +0000

    Jaime actualizo modulo proyecto,tipo salida, tipo de entrada, rangos de cotizaciones y definicion de ubicaciones
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@60 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/compras/modules/alminvfis/actions/actions.class.php
+++ apps/compras/modules/alminvfis/actions/actions.class.php
@@ -10,4 +10,57 @@
  */
 class alminvfisActions extends autoalminvfisActions
 {
-}
+    //public function executeAlumnosPorDia()
+    public function executeSQL()    
+    {
+    	//Funcion que ejecuta sql
+        $con =
+        sfContext::getInstance()->getDatabaseConnection($connection='propel');
+        $sql = "SELECT * FROM cainvfis";
+        $stmt = $con->createStatement();
+        $stmt->setLimit(50000);
+        $rs = $stmt->executeQuery($sql, ResultSet::FETCHMODE_NUM);
+        $resultado=array();
+        //aqui lleno el array con los resultados:
+           while ($rs->next())
+             {
+                $resultado[]=$rs->getRow();
+             }
+        //y la envio al template:
+        $this->rs=$resultado;
+        return $this->rs;
+    }
+
+  public function executeEdit()
+  {
+    $this->cainvfis = $this->getCainvfisOrCreate();
+    $this->rs = $this->executeSQL();    
+
+    if ($this->getRequest()->getMethod() == sfRequest::POST)
+    {
+      $this->updateCainvfisFromRequest();
+
+      $this->saveCainvfis($this->cainvfis);
+
+      $this->setFlash('notice', 'Your modifications have been saved');
+
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('alminvfis/create');
+      }
+      else if ($this->getRequestParameter('save_and_list'))
+      {
+        return $this->redirect('alminvfis/list');
+      }
+      else
+      {
+        return $this->redirect('alminvfis/edit?id='.$this->cainvfis->getId());
+      }
+    }
+    else
+    {
+      $this->labels = $this->getLabels();
+    }
+  }
+
+ }
