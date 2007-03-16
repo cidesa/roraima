39737742ab7c5b8fb20d2858610b169ef2b5637a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-16 01:01:27 +0000

    Luis Hernández Hay un archivo LEEME con mas informacion en la raiz del proyecto Se coloco mas informacion de referencia en "varios"
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@50 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/autenticacion/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// include project configuration
+include(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+
+if($so == 'Windows')
+{
+	// symfony bootstraping para Windows
+	require_once($sf_symfony_lib_dir.'\util\sfCore.class.php');	
+}
+else
+{
+	// symfony bootstraping para Linux
+	require_once($sf_symfony_lib_dir.'/util/sfCore.class.php');	
+}
+
+	sfCore::bootstrap($sf_symfony_lib_dir, $sf_symfony_data_dir);
+
+include('Constantes.class.php');
