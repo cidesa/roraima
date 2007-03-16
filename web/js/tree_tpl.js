adadbd4f1d958d79bcd56784403989e86fa30f7d
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-16 02:03:40 +0000

    Luis Hernández Se arreglo el problema con la tabla "empresa" al generar el modelo
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@54 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ web/js/tree_tpl.js
@@ -0,0 +1,35 @@
+/*
+	Feel free to use your custom icons for the tree. Make sure they are all of the same size.
+	User icons collections are welcome, we'll publish them giving all regards.
+*/
+
+var tree_tpl = {
+	'target'  : '_top',	// name of the frame links will be opened in
+							// other possible values are: _blank, _parent, _search, _self and _top
+
+	'icon_e'  : '../images/js/empty.gif', // empty image
+	'icon_l'  : 'js/line.gif',  // vertical line
+
+        'icon_32' : '../images/js/icons/base.gif',   // root leaf icon normal
+        'icon_36' : '../images/js/icons/base.gif',   // root leaf icon selected
+
+	'icon_48' : '../images/js/icons/base.gif',   // root icon normal
+	'icon_52' : '../images/js/icons/base.gif',   // root icon selected
+	'icon_56' : '../images/js/icons/base.gif',   // root icon opened
+	'icon_60' : '../images/js/icons/base.gif',   // root icon selected
+	
+	'icon_16' : '../images/js/icons/folder.gif', // node icon normal
+	'icon_20' : '../images/js/icons/folderopen.gif', // node icon selected
+	'icon_24' : '../images/js/icons/folderopen.gif', // node icon opened
+	'icon_28' : '../images/js/icons/folderopen.gif', // node icon selected opened
+
+	'icon_0'  : '../images/js/icons/page.gif', // leaf icon normal
+	'icon_4'  : '../images/js/icons/page.gif', // leaf icon selected
+	
+	'icon_2'  : '../images/js/icons/joinbottom.gif', // junction for leaf
+	'icon_3'  : '../images/js/icons/join.gif',       // junction for last leaf
+	'icon_18' : '../images/js/icons/plusbottom.gif', // junction for closed node
+	'icon_19' : '../images/js/icons/plus.gif',       // junctioin for last closed node
+	'icon_26' : '../images/js/icons/minusbottom.gif',// junction for opened node
+	'icon_27' : '../images/js/icons/minus.gif'       // junctioin for last opended node
+};
