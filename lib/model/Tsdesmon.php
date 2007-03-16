c992baa222e073924b8fd19fdda34cb7e1931a15
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-16 20:15:34 +0000

    Gerana, actualizacion modulos tesoreria
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@89 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/Tsdesmon.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'tsdesmon' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Tsdesmon extends BaseTsdesmon
+{
+}
---
+++ lib/model/TsdesmonPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'tsdesmon' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class TsdesmonPeer extends BaseTsdesmonPeer
+{
+}
