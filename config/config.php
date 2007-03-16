ccdb22144fc4358829617d615dfa5d4214fa8c09
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-16 16:32:35 +0000

    Jaime actualizo modulo proyecto,tipo salida, tipo de entrada, rangos de cotizaciones y definicion de ubicaciones
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@72 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- config/config.php
+++ config/config.php
@@ -1,5 +1,12 @@
 <?php
 
+<<<<<<< .mine
+// symfony directories
+$sf_symfony_lib_dir  = 'C:\AppServ\php5\pear\symfony';
+$sf_symfony_data_dir = 'C:\AppServ\php5\pear\data\symfony';
+$sg_len_reg = 32;
+
+=======
 $so = 'Windows';
 //$so = 'Linux';
 
@@ -19,3 +26,4 @@ else
 
 
 
+>>>>>>> .r71
