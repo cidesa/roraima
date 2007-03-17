8e352c8a974811a50723217c36fd48ba3e3d6859
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-17 02:48:12 +0000

    Luis Hernández
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@128 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/CadetentPeer.php
+++ lib/model/CadetentPeer.php
@@ -9,4 +9,12 @@
  */ 
 class CadetentPeer extends BaseCadetentPeer
 {
+	
+		private static $fieldNames = array (
+		BasePeer::TYPE_PHPNAME => array ('Número', 'Código Artículo', 'Canrec', 'Monto Total', 'Cosart', 'Id', ),
+		BasePeer::TYPE_COLNAME => array (CadetentPeer::RCPART, CadetentPeer::CODART, CadetentPeer::CANREC, CadetentPeer::MONTOT, CadetentPeer::COSART, CadetentPeer::ID, ),
+		BasePeer::TYPE_FIELDNAME => array ('rcpart', 'codart', 'canrec', 'montot', 'cosart', 'id', ),
+		BasePeer::TYPE_NUM => array (0, 1, 2, 3, 4, 5, )
+	);
+	
 }
