eb1c3e3cd0318bf7e796dea5412c7199380a0459
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-17 20:24:19 +0000

    Desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@136 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/tesoreria/modules/pagmodret/actions/actions.class.php
+++ apps/tesoreria/modules/pagmodret/actions/actions.class.php
@@ -10,4 +10,67 @@
  */
 class pagmodretActions extends autopagmodretActions
 {
+public function getMostrar_tipo()
+  {
+  	  $c = new Criteria;
+  	  $this->campo = str_pad($this->opordpag->getTipcau(),3,' ');
+  	  $c->add(CpdoccauPeer::TIPCAU, $this->campo);
+  	  $this->hola = CpdoccauPeer::doSelect($c);
+	  if ($this->hola)
+	  	return $this->hola[0]->getNomext();
+	  else 
+	    return ' ';
+  }
+	
+	public function presupuesto()    
+    {
+        $con = sfContext::getInstance()->getDatabaseConnection($connection='propel');
+        $sql = "SELECT opretord.codpre, cpdeftit.nompre, opretord.codtip, optipret.destip, opretord.monret, opretord.refere FROM opretord, opordpag, cpdeftit, optipret where (opretord.numord ='".($this->opordpag->getNumord())."') AND (opretord.codpre=cpdeftit.codpre) AND (opretord.codtip=optipret.codtip)";
+        $stmt = $con->createStatement();
+        $stmt->setLimit(10000);
+        $rs = $stmt->executeQuery($sql, ResultSet::FETCHMODE_NUM);
+        $resultado=array();
+        //aqui lleno el array con los resultados:
+           while ($rs->next())
+             {
+                $resultado[]=$rs->getRow();
+             }
+        //y la envio al template:
+        $this->rs=$resultado;
+        return $this->rs;
+    }
+	
+	
+public function executeEdit()
+  {
+    $this->opordpag = $this->getOpordpagOrCreate();
+    $this->rs = $this->presupuesto(); 
+    $this->reten = $this->getMostrar_tipo();
+
+    if ($this->getRequest()->getMethod() == sfRequest::POST)
+    {
+      $this->updateOpordpagFromRequest();
+
+      $this->saveOpordpag($this->opordpag);
+
+      $this->setFlash('notice', 'Your modifications have been saved');
+
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('pagmodret/create');
+      }
+      else if ($this->getRequestParameter('save_and_list'))
+      {
+        return $this->redirect('pagmodret/list');
+      }
+      else
+      {
+        return $this->redirect('pagmodret/edit?id='.$this->opordpag->getId());
+      }
+    }
+    else
+    {
+      $this->labels = $this->getLabels();
+    }
+  }
 }
