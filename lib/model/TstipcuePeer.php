dac4990075456e9a821b5ae4f852ec9cae1f06f0
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-19 12:52:10 +0000

    Miki
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@149 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/TstipcuePeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'tstipcue' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class TstipcuePeer extends BaseTstipcuePeer
+{
+}
