e59f7dd3864963c77c57c031cefcf1895b6f169a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-19 13:14:07 +0000

    Jesus Lobaton - compras genral
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@157 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/compras/modules/almcotiza/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * almcotiza actions.
+ *
+ * @package    siga
+ * @subpackage almcotiza
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class almcotizaActions extends autoalmcotizaActions
+{
+}
