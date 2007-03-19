6ef29395915dacfdbfc74287dbeb4ac1ceaf504c
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-19 12:53:19 +0000

    Miki
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@151 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/Npnomina.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'npnomina' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Npnomina extends BaseNpnomina
+{
+}
---
+++ lib/model/NpnominaPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'npnomina' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class NpnominaPeer extends BaseNpnominaPeer
+{
+}
