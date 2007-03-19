9387318d8c03f20bab7000f7a290ca53f5f48150
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-19 12:51:35 +0000

    Miki
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@148 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/Opdefemp.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'opdefemp' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Opdefemp extends BaseOpdefemp
+{
+}
---
+++ lib/model/OpdefempPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'opdefemp' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class OpdefempPeer extends BaseOpdefempPeer
+{
+}
