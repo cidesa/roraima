5db6cc2232961cc5702239bd203c76a167af4525
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-19 12:52:48 +0000

    Miki
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@150 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/Npdefcpt.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'npdefcpt' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Npdefcpt extends BaseNpdefcpt
+{
+}
---
+++ lib/model/NpdefcptPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'npdefcpt' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class NpdefcptPeer extends BaseNpdefcptPeer
+{
+}
