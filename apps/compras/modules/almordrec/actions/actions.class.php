2916c4b9ff41f16d5ef24804038ce3a400cf5d4c
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-20 22:08:32 +0000

    Gerana, actualizacion modulos tesoreria
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@203 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/compras/modules/almordrec/actions/actions.class.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * almordrec actions.
+ *
+ * @package    siga
+ * @subpackage almordrec
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class almordrecActions extends autoalmordrecActions
+{
+  public function getDes_pro()
+  {
+  	// nombre proveedor
+  	  $c = new Criteria;
+  	  $c->add(CaproveePeer::CODPRO, str_pad($this->carcpart->getCodpro(),10,' '));
+  	  $this->nompro = CaproveePeer::doSelect($c);
+	  if ($this->nompro)
+	  	return $this->nompro[0]->getNompro();
+	  else 
+	    return ' ';
+  }
+  public function getDes_alm()
+  {
+  	// nombre proveedor
+  	  $c = new Criteria;
+  	  $c->add(CadefalmPeer::CODALM,$this->carcpart->getCodalm());
+  	  $this->nompro = CadefalmPeer::doSelect($c);
+	  if ($this->nompro)
+	  	return $this->nompro[0]->getNomalm();
+	  else 
+	    return ' ';
+  }
+  
+  public function detalleSQL()    
+    {
+        $con = sfContext::getInstance()->getDatabaseConnection($connection='propel');
+        $sql ="Select A.codart, B.DesArt,A.canrec, A.candev,A.mondes,A.monrgo,A.montot,A.codcat,A.codfal,A.fecest From CaArtRcp A,CaRegArt B Where A.codart= RPAD(B.codart,20,' ')  and A.RcpArt = '" . $this->carcpart->getRcpart() . "' order By A.CodArt";
+        $stmt = $con->createStatement();
+        $stmt->setLimit(5000);
+        $rs = $stmt->executeQuery($sql, ResultSet::FETCHMODE_NUM);
+        $resultado=array();
+        //aqui lleno el array con los resultados:
+           while ($rs->next())
+             {
+                $resultado[]=$rs->getRow();
+             }
+        //y la envio al template:
+        $this->rs=$resultado;	
+        return $this->rs;
+    }
+  
+  public function executeEdit()
+  {
+    $this->carcpart = $this->getCarcpartOrCreate();
+    $this->nomprovee = $this->getDes_pro();
+    $this->nomalmace = $this->getDes_alm();
+    $this->detrec = $this->detalleSQL();
+
+    if ($this->getRequest()->getMethod() == sfRequest::POST)
+    {
+      $this->updateCarcpartFromRequest();
+
+      $this->saveCarcpart($this->carcpart);
+
+      $this->setFlash('notice', 'Your modifications have been saved');
+
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('almordrec/create');
+      }
+      else if ($this->getRequestParameter('save_and_list'))
+      {
+        return $this->redirect('almordrec/list');
+      }
+      else
+      {
+        return $this->redirect('almordrec/edit?id='.$this->carcpart->getId());
+      }
+    }
+    else
+    {
+      $this->labels = $this->getLabels();
+    }
+  }
+  
+}
---
+++ apps/compras/modules/almpriori/actions/actions.class.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * almpriori actions.
+ *
+ * @package    siga
+ * @subpackage almpriori
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class almprioriActions extends autoalmprioriActions
+{
+   public function detallesSQL()    
+    {
+     if ($this->casolart->getReqart()!='')
+     {
+        $con = sfContext::getInstance()->getDatabaseConnection($connection='propel');
+        $sql="Select A.codart,B.desart,E.costo,C.Priori,D.Nompro from CAArtSol A, CARegArt B,CaCotiza C,CAProvee D,CaDetCot E where A.CodArt=B.codart and A.ReqARt=C.RefSol and C.codpro=D.codpro and C.RefCot=E.RefCot and A.codart=E.codart and trim(A.ReqARt)='" .trim($this->casolart->getReqart()) . "' order by c.Codpro,a.codart";	
+     
+        $stmt = $con->createStatement();
+        $stmt->setLimit(5000);
+        $detalles = $stmt->executeQuery($sql, ResultSet::FETCHMODE_NUM);
+        $resultado=array();
+        //aqui lleno el array con los resultados:
+           while ($detalles->next())
+             {
+                $resultado[]=$detalles->getRow();
+             }
+        //y la envio al template:
+	    $this->detalles=$resultado;
+         return $this->detalles;
+     }
+     else
+     {
+      return '';	
+     }
+    }
+    
+    
+ public function executeEdit()
+  {
+    $this->casolart = $this->getCasolartOrCreate();
+    $this->detalles = $this->detallesSQL();
+    
+
+    if ($this->getRequest()->getMethod() == sfRequest::POST)
+    {
+      $this->updateCasolartFromRequest();
+
+      $this->saveCasolart($this->casolart);
+
+      $this->setFlash('notice', 'Your modifications have been saved');
+
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('almpriori/create');
+      }
+      else if ($this->getRequestParameter('save_and_list'))
+      {
+        return $this->redirect('almpriori/list');
+      }
+      else
+      {
+        return $this->redirect('almpriori/edit?id='.$this->casolart->getId());
+      }
+    }
+    else
+    {
+      $this->labels = $this->getLabels();
+    }
+  }
+    
+  public function executeList()
+  {
+    $this->processSort();
+
+    $this->processFilters();
+
+    $this->filters = $this->getUser()->getAttributeHolder()->getAll('sf_admin/casolart/filters');
+
+    // pager
+    $this->pager = new sfPropelPager('Casolart', 8);
+    $c = new Criteria();
+    
+    $c->addJoin(CasolartPeer::REQART, CacotizaPeer::REFSOL);
+    $c->setDistinct();
+    
+    $this->addSortCriteria($c);
+    $this->addFiltersCriteria($c);
+    $this->pager->setCriteria($c);
+    $this->pager->setPage($this->getRequestParameter('page', 1));
+    $this->pager->init();
+  }
+	
+}
