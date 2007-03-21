ac7f470e626e77eee2a101bc3c57eed86163ebd1
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-21 16:01:09 +0000

    Jesus Lobaton - nomina - nomdesfespmotfalpre
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@213 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/nomina/modules/nomdefespmotfalpre/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * nomdefespmotfalpre actions.
+ *
+ * @package    siga
+ * @subpackage nomdefespmotfalpre
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class nomdefespmotfalpreActions extends autonomdefespmotfalpreActions
+{
+}
