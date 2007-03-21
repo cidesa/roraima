fa65ffd04cd860bf64b126b76547977ed2bde50e
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-21 15:16:17 +0000

    Jesus Lobaton - nomina - nomdefespgrgulab
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@211 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/nomina/modules/nomdefespban/actions/actions.class.php
+++ apps/nomina/modules/nomdefespban/actions/actions.class.php
@@ -10,4 +10,18 @@
  */
 class nomdefespbanActions extends autonomdefespbanActions
 {
+	protected function updateNpbancosFromRequest()
+	{
+		$npbancos = $this->getRequestParameter('npbancos');
+
+	if (isset($npbancos['codban']))
+    {
+      $this->npbancos->setCodban(str_pad($npbancos['codban'],2,'0',STR_PAD_LEFT));
+    }
+    if (isset($npbancos['nomban']))
+    {
+      $this->npbancos->setNomban($npbancos['nomban']);
+    }
+  }
+	
 }
---
+++ apps/nomina/modules/nomdefespgrulab/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * nomdefespgrulab actions.
+ *
+ * @package    siga
+ * @subpackage nomdefespgrulab
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class nomdefespgrulabActions extends autonomdefespgrulabActions
+{
+	protected function updateNpgrulabFromRequest()
+	{
+		$npgrulab = $this->getRequestParameter('npgrulab');
+
+		if (isset($npgrulab['codgrulab']))
+		{
+      		$this->npgrulab->setCodgrulab(str_pad($npgrulab['codgrulab'],4,'0',STR_PAD_LEFT));
+   		}
+    	if (isset($npgrulab['desgrulab']))
+   		{
+      		$this->npgrulab->setDesgrulab($npgrulab['desgrulab']);
+        }
+  }	
+}
