35723ff809950454cf0f67b6583ebe384eb09ce6
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-21 01:22:27 +0000

    Jesus Lobaton - nomina nomdefespban
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@206 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/nomina/modules/nomdefespban/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * nomdefespban actions.
+ *
+ * @package    siga
+ * @subpackage nomdefespban
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class nomdefespbanActions extends autonomdefespbanActions
+{
+}
