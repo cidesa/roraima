202a48a08646321e256ec33ff5ab74e62c5ed0d7
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-21 16:37:17 +0000

    Jesus Lobaton - nomina - nomdefespmotfalpre
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@214 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/nomina/modules/nomdefespmotfalpre/actions/actions.class.php
+++ apps/nomina/modules/nomdefespmotfalpre/actions/actions.class.php
@@ -10,4 +10,23 @@
  */
 class nomdefespmotfalpreActions extends autonomdefespmotfalpreActions
 {
+	protected function updateNpmotfalFromRequest()
+	{
+		$npmotfal = $this->getRequestParameter('npmotfal');
+
+		if (isset($npmotfal['codmotfal']))
+		{
+			$this->npmotfal->setCodmotfal(str_pad($npmotfal['codmotfal'],4,'0',STR_PAD_LEFT));
+		}
+		if (isset($npmotfal['desmotfal']))
+		{
+			$this->npmotfal->setDesmotfal($npmotfal['desmotfal']);
+    	}
+  		//if (isset($npmotfal['causa']))
+  	    //{
+  	    //    $this->npmotfal->setCausa($npmotfal['causa']);
+			  $this->npmotfal->setCausa($this->getRequestParameter('checkbox1'));
+  	    //}
+  }
+	
 }
