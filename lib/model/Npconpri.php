2181793b657bd9c2767859c9b6167a39697e5390
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-22 20:06:58 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@231 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npconpri.php
+++ lib/model/Npconpri.php
@@ -9,4 +9,25 @@
  */ 
 class Npconpri extends BaseNpconpri
 {
+  public function getNomina()
+  {
+      $c = new Criteria();
+  	  $c->add(NpnominaPeer::CODNOM,self::getCodnom());
+  	  $nomina = NpnominaPeer::doSelectone($c);
+	  if ($nomina)
+	  	return $nomina->getNomnom();
+	  else 
+	    return '<!Nombre no encontrado!>';
+  }
+  public function getConcepto()
+  {
+      $c = new Criteria();
+  	  $c->add(NpdefcptPeer::CODCON,self::getCodcon());
+  	  $concepto = NpdefcptPeer::doSelectone($c);
+	  if ($concepto)
+	  	return $concepto->getNomcon();
+	  else 
+	    return '<!Nombre no encontrado!>';
+  }	
+  
 }
--- lib/model/Npnomina.php
+++ lib/model/Npnomina.php
@@ -9,4 +9,57 @@
  */ 
 class Npnomina extends BaseNpnomina
 {
+  public function getMonto()
+  {
+  	//consulta a 3 tablas para saber la foma de entrega
+  	  $c = new Criteria;
+  	  $this->campo = self::getCodnom();	  
+  	  $c->add(NpdefcptPeer::CODCON, $this->campo);	  
+  	  $c->addJoin(NpasiconnomPeer::CODCON,NpdefcptPeer::CODCON);
+  	  $this->rt = NpasiconnomPeer::doSelect($c);
+  	  if ($this->rt)
+	  	{
+	  		$this->codcon = $this->rt[0]->getCodcon();
+            $c->add(NpdefmovPeer::CODNOM, $this->campo);
+            $c->add(NpdefmovPeer::CODCON, $this->codcon);
+            $c->add(NpdefmovPeer::STATUS, 'M');	
+            $this->rt = NpdefmovPeer::doSelect($c);       
+            if ($this->rt)
+            {
+               return $this->monto = '<img src="/images/check.gif" width="16" height="15">'; 
+            }
+	  	}
+	  	else
+	  	{
+	  	    return  $this->monto = false;
+	  	}
+	  	 
+  }	 	
+  public function getCantidad()
+  {
+  	//consulta a 3 tablas para saber la foma de entrega
+  	  $c = new Criteria;
+  	  $this->campo = self::getCodnom();	  
+  	  $c->add(NpdefcptPeer::CODCON, $this->campo);	  
+  	  $c->addJoin(NpasiconnomPeer::CODCON,NpdefcptPeer::CODCON);
+  	  $this->rt = NpasiconnomPeer::doSelect($c);
+  	  if ($this->rt)
+	  	{
+	  		$this->codcon = $this->rt[0]->getCodcon();
+            $c->add(NpdefmovPeer::CODNOM, $this->campo);
+            $c->add(NpdefmovPeer::CODCON, $this->codcon);
+            $c->add(NpdefmovPeer::STATUS, 'C');	
+            $this->rt = NpdefmovPeer::doSelect($c);       
+            if ($this->rt)
+            {
+               return $this->cantidad = '<img src="/images/check.gif" width="16" height="15">'; 
+            }            
+	  	}
+	  	else
+	  	{
+	  	    return $this->cantidad = false;	  	    
+	  	}
+	  	 
+  }	 	  
+	
 }
