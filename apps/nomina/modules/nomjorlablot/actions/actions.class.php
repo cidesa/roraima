d4d2405fee90f831634d33c6bec17282dea295fa
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-22 00:29:04 +0000

    Luis Hernández
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@219 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/nomina/modules/nomjorlablot/actions/actions.class.php
+++ apps/nomina/modules/nomjorlablot/actions/actions.class.php
@@ -10,4 +10,54 @@
  */
 class nomjorlablotActions extends autonomjorlablotActions
 {
+	protected function executeEdit()
+	{
+		$c = new Criteria();
+		$this->pagerNphojint = NphojintPeer::getPagerByCriteria($c);
+		
+		parent::executeEdit();
+	}
+	
+  protected function updateNpdefjorlabFromRequest()
+  {
+    $npdefjorlab = $this->getRequestParameter('npdefjorlab');
+
+    if (isset($npdefjorlab['codnom']))
+    {
+      $this->npdefjorlab->setCodnom($npdefjorlab['codnom']);
+    }
+    if (isset($npdefjorlab['idejor']))
+    {
+      $this->npdefjorlab->setIdejor($npdefjorlab['idejor']);
+    }
+    if (isset($npdefjorlab['lunes']))
+    {
+    	$this->npdefjorlab->setLunes('2');
+    }else $this->npdefjorlab->setLunes(null);
+    if (isset($npdefjorlab['martes']))
+    {
+      $this->npdefjorlab->setMartes('3');
+    }else $this->npdefjorlab->setMartes(null);
+    if (isset($npdefjorlab['miercoles']))
+    {
+      $this->npdefjorlab->setMiercoles('4');
+    }else $this->npdefjorlab->setMiercoles(null);
+    if (isset($npdefjorlab['jueves']))
+    {
+      $this->npdefjorlab->setJueves('5');
+    }else $this->npdefjorlab->setJueves(null);
+    if (isset($npdefjorlab['viernes']))
+    {
+      $this->npdefjorlab->setViernes('6');
+    }else $this->npdefjorlab->setViernes(null);
+    if (isset($npdefjorlab['sabado']))
+    {
+      $this->npdefjorlab->setSabado('7');
+    }else $this->npdefjorlab->setSabado(null);
+    if (isset($npdefjorlab['domingo']))
+    {
+      $this->npdefjorlab->setDomingo('1');
+    }else $this->npdefjorlab->setDomingo(null);
+  }
+	
 }
