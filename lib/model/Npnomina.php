9435a4e74c30e7f9e0690da8efa958c6f55f951f
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-23 01:08:21 +0000

    Jesus Lobaton - npnomina
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@234 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npnomina.php
+++ lib/model/Npnomina.php
@@ -9,26 +9,26 @@
  */ 
 class Npnomina extends BaseNpnomina
 {
-  public function getMonto()
-  {
-  	//consulta a 3 tablas para saber la foma de entrega
-  	  $c = new Criteria;
-  	  $this->campo = self::getCodnom();	  
-  	  $c->add(NpdefcptPeer::CODCON, $this->campo);	  
-  	  $c->addJoin(NpasiconnomPeer::CODCON,NpdefcptPeer::CODCON);
-  	  $this->rt = NpasiconnomPeer::doSelect($c);
-  	  if ($this->rt)
-	  	{
-	  		$this->codcon = $this->rt[0]->getCodcon();
-            $c->add(NpdefmovPeer::CODNOM, $this->campo);
-            $c->add(NpdefmovPeer::CODCON, $this->codcon);
-            $c->add(NpdefmovPeer::STATUS, 'M');	
-            $this->rt = NpdefmovPeer::doSelect($c);       
-            if ($this->rt)
-            {
-               return $this->monto = '<img src="/images/check.gif" width="16" height="15">'; 
-            }
-	  	}
+	public function getMonto()
+	{
+		//consulta a 3 tablas para saber la foma de entrega
+		$c = new Criteria;
+		$this->campo = self::getCodnom();
+		$c->add(NpdefcptPeer::CODCON, $this->campo);
+		$c->addJoin(NpasiconnomPeer::CODCON,NpdefcptPeer::CODCON);
+		$this->rt = NpasiconnomPeer::doSelect($c);
+		if ($this->rt)
+		{
+			$this->codcon = $this->rt[0]->getCodcon();
+			$c->add(NpdefmovPeer::CODNOM, $this->campo);
+			$c->add(NpdefmovPeer::CODCON, $this->codcon);
+			$c->add(NpdefmovPeer::STATUS, 'M');
+			$this->rt = NpdefmovPeer::doSelect($c);
+			if ($this->rt)
+			{
+				return $this->monto = '<img src="/images/check.gif" width="16" height="15">';
+			}
+		}
 	  	else
 	  	{
 	  	    return  $this->monto = false;
@@ -61,5 +61,30 @@ class Npnomina extends BaseNpnomina
 	  	}
 	  	 
   }	 	  
-	
+
+    public function getCodcon()
+    {
+  	    $c = new Criteria();
+  	    $c->add(NpcestaticketsPeer::CODNOM,self::getCodnom());
+  		$c->addJoin(NpdefcptPeer::CODCON,NpcestaticketsPeer::CODCON);
+  		$codcon = NpdefcptPeer::doSelectone($c);
+  		if ($codcon)
+	  	  return $codcon->getCodcon();
+	    else 
+	      return ' ';
+    }
+
+    public function getNomcon()
+    {
+    	$c = new Criteria();
+    	$c->add(NpcestaticketsPeer::CODNOM,self::getCodnom());
+    	$c->addJoin(NpdefcptPeer::CODCON,NpcestaticketsPeer::CODCON);
+    	$nomcon = NpdefcptPeer::doSelectone($c);
+    	if ($nomcon)
+    	  return $nomcon->getNomcon();
+	    else
+	      return ' ';
+    }
+      //$c->addJoin(NpcestaticketsPeer::CODNOM,NpnominaPeer::CODNOM);
+      // $c->addJoin(NpdefcptPeer::CODCON,NpcestaticketsPeer::CODCON);  
 }
