40f0302518ecb7d9cd839a6b75f877a186e594c5
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-23 21:29:26 +0000

    ultima vista
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@243 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/nomina/modules/nomtipcar/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * nomtipcar actions.
+ *
+ * @package    siga
+ * @subpackage nomtipcar
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class nomtipcarActions extends autonomtipcarActions
+{
+  protected function updateNptipcarFromRequest()
+  {
+    $nptipcar = $this->getRequestParameter('nptipcar');
+
+    if (isset($nptipcar['codtipcar']))
+    {
+      $this->nptipcar->setCodtipcar(str_pad($nptipcar['codtipcar'],3,'0',STR_PAD_LEFT));
+    }
+    if (isset($nptipcar['destipcar']))
+    {
+      $this->nptipcar->setDestipcar($nptipcar['destipcar']);
+    }
+  }
+}
