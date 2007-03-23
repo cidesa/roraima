fbe705fd1d6cf171b3aab27fd7dee3e63551987a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-23 21:24:20 +0000

    Luis Hernández
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@241 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npdefjorlab.php
+++ lib/model/Npdefjorlab.php
@@ -9,4 +9,33 @@
  */ 
 class Npdefjorlab extends BaseNpdefjorlab
 {
+	
+	public function getDias()
+	{
+		$dias = '';
+
+		if(self::getDomingo() == '1' ) $dias = $dias.'Domingo, ';  
+		if(self::getLunes() == '2' ) $dias = $dias.'Lunes, ';
+		if(self::getMartes() == '3' ) $dias = $dias.'Martes, ';
+		if(self::getMiercoles() == '4' ) $dias = $dias.'Miercoles, ';
+		if(self::getJueves() == '5' ) $dias = $dias.'Jueves, ';
+		if(self::getViernes() == '6' ) $dias = $dias.'Viernes, ';
+		if(self::getSabado() == '7' ) $dias = $dias.'Sabado, ';
+
+		return $dias;
+		
+	}
+	
+	public function getNomnom()
+	{
+		
+		$c = new Criteria();
+		$c->add(NpnominaPeer::CODNOM,self::getCodnom());
+		$nomnom = NpnominaPeer::doSelectOne($c);
+		
+		if($nomnom) return $nomnom->getNomnom();
+		else '';
+		
+	}
+	
 }
