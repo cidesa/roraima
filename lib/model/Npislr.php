947a13fed8c9e2cc4575fd562b00aad001b9b485
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-23 01:03:11 +0000

    Jesus Lobaton - lib
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@233 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npislr.php
+++ lib/model/Npislr.php
@@ -3,10 +3,50 @@
 /**
  * Subclass for representing a row from the 'npislr' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Npislr extends BaseNpislr
 {
+	public function getNomnom()
+	{
+		$c = new Criteria();
+		$c->add(NpnominaPeer::CODNOM,self::getCodnom());
+		//$c->addJoin(NpdefcptPeer::CODCON,NpcestaticketsPeer::CODCON);
+		$codigo = NpnominaPeer::doSelectone($c);
+		if ($codigo){
+			return $codigo->getNomnom();
+		}else{
+			return ' ';
+		}
+	}
+
+	public function getNomcon()
+	{
+		$c = new Criteria();
+		$c->add(NpdefcptPeer::CODCON,self::getCodnom());
+		//$c->addJoin(NpdefcptPeer::CODCON,NpcestaticketsPeer::CODCON);
+		$codigo = NpdefcptPeer::doSelectone($c);
+		if ($codigo){
+			return $codigo->getNomcon();
+		}else{
+			return ' ';
+		}
+	}
+
+	public function getNomconimp()
+	{
+		$c = new Criteria();
+		$c->add(NpdefcptPeer::CODCON,self::getCodconimp());
+		//$c->addJoin(NpdefcptPeer::CODCON,NpcestaticketsPeer::CODCON);
+		$codigo = NpdefcptPeer::doSelectone($c);
+		if ($codigo){
+			return $codigo->getNomcon();
+  		}else{
+	      return ' ';
+  		}
+    }	    
+    
+    
 }
