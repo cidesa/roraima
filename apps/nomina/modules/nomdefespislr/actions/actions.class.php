5898505cfed346c1d9267a60b424c3662770c68b
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-23 01:13:23 +0000

    Jesus Lobaton - nomina - nomdefespislr
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@235 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/nomina/modules/nomdefespislr/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * nomdefespislr actions.
+ *
+ * @package    siga
+ * @subpackage nomdefespislr
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class nomdefespislrActions extends autonomdefespislrActions
+{
+}
