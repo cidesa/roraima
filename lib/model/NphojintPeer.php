0f3c6b0e6201953951a08b2053a80a90f19f7eb6
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-23 21:29:15 +0000

    Luis Hernández
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@242 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/NphojintPeer.php
+++ lib/model/NphojintPeer.php
@@ -9,4 +9,24 @@
  */ 
 class NphojintPeer extends BaseNphojintPeer
 {
+	
+	const COLUMNS = 'columns';
+	
+	private static $columsname = array (
+	self::COLUMNS => array (NphojintPeer::CEDEMP => 'Cedula', NphojintPeer::NOMEMP => 'Nombre', NphojintPeer::ID =>  'Id', ),);
+	
+	public static function getPagerByCriteria($c,$pagina = 1)
+	{
+		$pager = new sfPropelPager('Nphojint', 10);
+	    $pager->setCriteria($c);
+	    $pager->setPage($pagina);
+	    $pager->init();
+	    return $pager; 
+	}
+	
+	static public function getColumName($colum)
+	{
+		return self::$columsname[self::COLUMNS][$colum];
+	}
+	
 }
