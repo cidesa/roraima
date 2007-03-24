69c5e7713afdb3fdc21a2f350785feb3d999bb04
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-24 21:58:39 +0000

    Jesus Lobaton - nomina
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@256 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/nomina/modules/vachistorico/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * vachistorico actions.
+ *
+ * @package    siga
+ * @subpackage vachistorico
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class vachistoricoActions extends autovachistoricoActions
+{
+}
