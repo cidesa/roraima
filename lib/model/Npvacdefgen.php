15ebf67a6a4d25b403b2d8ea7b60d1fce93d8e8d
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-24 18:12:26 +0000

    Jesus Lobaton - lib- mode- npvañcdefgen
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@250 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npvacdefgen.php
+++ lib/model/Npvacdefgen.php
@@ -3,10 +3,34 @@
 /**
  * Subclass for representing a row from the 'npvacdefgen' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Npvacdefgen extends BaseNpvacdefgen
 {
+	public function getNomnom()
+	{
+		$c = new Criteria();
+		$c->add(NpnominaPeer::CODNOM,self::getCodnomvac());
+		$codigo = NpnominaPeer::doSelectone($c);
+		if ($codigo){
+			return $codigo->getNomnom();
+		}else{
+			return ' ';
+		}
+	}
+
+	public function getNomcon()
+	{
+		$c = new Criteria();
+		$c->add(NpasiconnomPeer::CODNOM,self::getCodconvac());
+		$c->addJoin(NpdefcptPeer::CODCON,NpasiconnomPeer::CODCON);
+		$codigo = NPDefCptPeer::doSelectone($c);
+		if ($codigo){
+			return $codigo->getNomcon();
+  		}else{
+	      return ' ';
+  		}
+    }	  
 }
