e68ec95503d8875f9211ed3e61384f3644f60dee
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-24 21:57:39 +0000

    Jesus Lobaton - lib - model
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@255 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npasicaremp.php
+++ lib/model/Npasicaremp.php
@@ -3,10 +3,22 @@
 /**
  * Subclass for representing a row from the 'npasicaremp' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Npasicaremp extends BaseNpasicaremp
 {
+	public function getcodcon()
+	{
+		$c = new Criteria();
+		$c->add(NpasiconempPeer::CODEMP,self::getCodemp());
+		$c->addJoin(NpasiconempPeer::CODCAR,NpasicarempPeer::CODCAR);
+		$codigo = NpasiconempPeer::doSelectone($c);
+		if ($codigo){
+			return $codigo->getcodcon();
+  		}else{
+	      return ' ';
+  		}
+    }	  	
 }
