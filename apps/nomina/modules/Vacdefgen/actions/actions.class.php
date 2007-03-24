30c9714d6efac329dffb848b309ba88e25ff3257
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-24 18:11:17 +0000

    Jesus Lobaton - vacdefgen
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@249 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/nomina/modules/Vacdefgen/actions/actions.class.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Vacdefgen actions.
+ *
+ * @package    siga
+ * @subpackage Vacdefgen
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class VacdefgenActions extends autoVacdefgenActions
+{
+	protected function updateNpvacdefgenFromRequest()
+	{
+		$npvacdefgen = $this->getRequestParameter('npvacdefgen');
+
+		if (isset($npvacdefgen['codnomvac']))
+		{
+			$this->npvacdefgen->setCodnomvac($npvacdefgen['codnomvac']);
+		}
+		if (isset($npvacdefgen['nomnom']))
+		{
+			$this->npvacdefgen->setNomnom($npvacdefgen['nomnom']);
+		}
+		if (isset($npvacdefgen['codconvac']))
+		{
+			$this->npvacdefgen->setCodconvac($npvacdefgen['codconvac']);
+		}
+		if (isset($npvacdefgen['nomcon']))
+		{
+			$this->npvacdefgen->setNomcon($npvacdefgen['nomcon']);
+		}
+		//if (isset($npvacdefgen['pagoad']))
+    //{
+      //$this->npvacdefgen->setPagoad($npvacdefgen['pagoad']);
+	  $this->npvacdefgen->setPagoad($this->getRequestParameter('checkbox1'));
+    //}
+  }	
+}
