c3ed90bc6168d95f4059ae95a9fb08e824b6bbbf
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-26 20:42:28 +0000

    Jesus Lobaton - model/npoint
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@266 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Nphojint.php
+++ lib/model/Nphojint.php
@@ -3,10 +3,33 @@
 /**
  * Subclass for representing a row from the 'nphojint' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Nphojint extends BaseNphojint
 {
+	public function getfeccal()
+	{
+		$c = new Criteria();
+		$c->add(NpasiempcontPeer::CODEMP,self::getCodemp());		
+		$feccal = NpasiempcontPeer::doSelectone($c);
+		if ($feccal){
+			return $feccal->getfeccal();
+		}else{
+			return ' ';
+		}
+	}
+
+	public function getcodnom()
+	{
+		$c = new Criteria();
+		$c->add(NpasiempcontPeer::CODEMP,self::getCodemp());		
+		$codnom = NpasiempcontPeer::doSelectone($c);
+		if ($codnom){
+			return $codnom->getcodnom();
+		}else{
+			return ' ';
+		}
+	}	
 }
