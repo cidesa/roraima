be35266bdf83939ff67ba1108b060e958beaa98f
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 13:49:00 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@272 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/NpasiconempPeer.php
+++ lib/model/NpasiconempPeer.php
@@ -9,4 +9,22 @@
  */ 
 class NpasiconempPeer extends BaseNpasiconempPeer
 {
+const COLUMNS = 'columns';
+	
+	private static $columsname = array (
+	self::COLUMNS => array (NpasiconempPeer::CODEMP => 'Codigo del Trabajador', NpasiconempPeer::NOMEMP => 'Nombre', NpasiconempPeer::CODCAR => 'Codigo del Cargo', NpasiconempPeer::NOMCAR => 'Descripcion del Cargo', NpasiconempPeer::CANTIDAD => 'Cantidad', NpasiconempPeer::MONTO => 'Monto', NpasiconempPeer::FECINI => 'Fecha de Inicio', NpasiconempPeer::FECEXP => 'Fecha de Expiracion', NpasiconempPeer::FRECON => 'Frecuencia de Pago', NpasiconempPeer::ASIDED => 'Tipo de Operacion', NpasiconempPeer::ACUCON => 'Concepto Acumulativo', NpasiconempPeer::CALCON => 'Concepto Calculable', NpasiconempPeer::ACTIVO => 'Concepto Acumulativo', NpasiconempPeer::ACUMULADO => 'Acumulado', NpasiconempPeer::ID =>  'Id', ),);
+	
+	public static function getPagerByCriteria($c,$pagina = 1)
+	{
+		$pager = new sfPropelPager('Npasiconemp', 10);
+	    $pager->setCriteria($c);
+	    $pager->setPage($pagina);
+	    $pager->init();
+	    return $pager; 
+	}
+	
+	static public function getColumName($colum)
+	{
+		return self::$columsname[self::COLUMNS][$colum];
+	}
 }
--- lib/model/Nptippre.php
+++ lib/model/Nptippre.php
@@ -9,4 +9,15 @@
  */ 
 class Nptippre extends BaseNptippre
 {
+  public function getNomcon()
+  {
+  	  $c = new Criteria();
+  	  $c->add(NpasiconempPeer::CODCON,self::getCodcon());
+  	  $nombre = NpasiconempPeer::doSelectone($c);
+	  if ($nombre)
+	  	return $nombre->getNomcon();
+	  else 
+	    return ' ';
+  }
+	
 }
