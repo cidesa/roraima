8dc460f87b9d18f9ad23d2c4017a4d3e118ebb3c
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 19:20:45 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@284 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npasiconemp.php
+++ lib/model/Npasiconemp.php
@@ -3,10 +3,51 @@
 /**
  * Subclass for representing a row from the 'npasiconemp' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Npasiconemp extends BaseNpasiconemp
 {
+	public function getNomemp()
+	{
+		$c = new Criteria();
+		$c->add(NphojintPeer::CODEMP,str_pad(self::getCodemp(), 16, " "));
+		$nomemp = NphojintPeer::doSelectone($c);
+		if ($nomemp)
+		return $nomemp->getNomemp();
+		else
+		return ' ';
+	}
+	public function getNomcar()
+	{
+		$c = new Criteria();
+		$c->add(NpcargosPeer::CODCAR,self::getCodcar());
+		$nomcar = NpcargosPeer::doSelectone($c);
+		if ($nomcar)
+		return $nomcar->getNomcar();
+		else
+		return ' ';
+	}
+	public function getNomnom()
+	{
+		$c = new Criteria();
+		$c->add(NpdefmovPeer::CODCON,self::getCodcon());
+		$c->addJoin(NpnominaPeer::CODNOM,NpdefmovPeer::CODNOM);
+		$nomnom = NpnominaPeer::doSelectone($c);
+		if ($nomnom)
+		return $nomnom->getNomnom();
+		else
+		return ' ';
+	}
+	public function getNomcon()
+	{
+		$c = new Criteria();
+		$c->add(NpdefcptPeer::CODCON,self::getCodcon());
+		$nomcon = NpdefcptPeer::doSelectone($c);
+		if ($nomcon)
+		return $nomcon->getNomcon();
+		else
+		return ' ';
+	}	
 }
