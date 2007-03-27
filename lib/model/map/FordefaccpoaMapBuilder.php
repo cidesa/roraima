cdbaee7c17eaeaf992e20096d1a42e80c5b4928e
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 22:08:52 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@303 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/map/FordefaccpoaMapBuilder.php
@@ -0,0 +1,64 @@
+<?php
+
+
+	
+class FordefaccpoaMapBuilder {
+
+	
+	const CLASS_NAME = 'lib.model.map.FordefaccpoaMapBuilder';	
+
+    
+    private $dbMap;
+
+	
+    public function isBuilt()
+    {
+        return ($this->dbMap !== null);
+    }
+
+	
+    public function getDatabaseMap()
+    {
+        return $this->dbMap;
+    }
+
+    
+    public function doBuild()
+    {
+		$this->dbMap = Propel::getDatabaseMap('propel');
+		
+		$tMap = $this->dbMap->addTable('fordefaccpoa');
+		$tMap->setPhpName('Fordefaccpoa');
+
+		$tMap->setUseIdGenerator(false);
+
+		$tMap->addColumn('CODSUBACC', 'Codsubacc', 'string', CreoleTypes::VARCHAR, true, 5);
+
+		$tMap->addColumn('DESSUBACC', 'Dessubacc', 'string', CreoleTypes::VARCHAR, false, 250);
+
+		$tMap->addColumn('METSUBACC', 'Metsubacc', 'string', CreoleTypes::VARCHAR, false, 250);
+
+		$tMap->addColumn('LOCSUBACC', 'Locsubacc', 'string', CreoleTypes::VARCHAR, false, 250);
+
+		$tMap->addColumn('INDGESSUBACC', 'Indgessubacc', 'string', CreoleTypes::VARCHAR, false, 250);
+
+		$tMap->addColumn('CODUNIMED', 'Codunimed', 'string', CreoleTypes::VARCHAR, false, 3);
+
+		$tMap->addColumn('MEDVERSUBACC', 'Medversubacc', 'string', CreoleTypes::VARCHAR, false, 250);
+
+		$tMap->addColumn('SUPSUBACC', 'Supsubacc', 'string', CreoleTypes::VARCHAR, false, 250);
+
+		$tMap->addColumn('METPRITRI', 'Metpritri', 'double', CreoleTypes::NUMERIC, false);
+
+		$tMap->addColumn('METSEGTRI', 'Metsegtri', 'double', CreoleTypes::NUMERIC, false);
+
+		$tMap->addColumn('METTERTRI', 'Mettertri', 'double', CreoleTypes::NUMERIC, false);
+
+		$tMap->addColumn('METCUATRI', 'Metcuatri', 'double', CreoleTypes::NUMERIC, false);
+
+		$tMap->addColumn('METTOT', 'Mettot', 'double', CreoleTypes::NUMERIC, false);
+
+		$tMap->addPrimaryKey('ID', 'Id', 'int', CreoleTypes::INTEGER, true, null);
+				
+    } 
+} 
