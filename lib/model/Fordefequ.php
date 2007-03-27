889dfd67fceb7ef833729548340c286175cf3265
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 20:01:59 +0000

    Jesus Lobaton - lib
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@290 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Fordefequ.php
+++ lib/model/Fordefequ.php
@@ -3,10 +3,11 @@
 /**
  * Subclass for representing a row from the 'fordefequ' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Fordefequ extends BaseFordefequ
 {
+
 }
--- lib/model/Fordefsubobj.php
+++ lib/model/Fordefsubobj.php
@@ -3,10 +3,20 @@
 /**
  * Subclass for representing a row from the 'fordefsubobj' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Fordefsubobj extends BaseFordefsubobj
 {
+	public function getDesequ()
+	{
+		$c = new Criteria();
+		$c->add(FordefequPeer::CODEQU,self::getCodequ());
+		$desequ = FordefequPeer::doSelectone($c);
+  		if ($desequ)
+	  	  return $desequ->getDesequ();
+	    else 
+	      return ' ';
+    }		
 }
