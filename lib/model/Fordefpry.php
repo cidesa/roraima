e94867dd5f2a1543ffb056c93bef9a11da094172
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 22:08:08 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@301 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Fordefpry.php
+++ lib/model/Fordefpry.php
@@ -3,10 +3,16 @@
 /**
  * Subclass for representing a row from the 'fordefpry' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Fordefpry extends BaseFordefpry
 {
+	public function getFrepagcon()
+	{
+		$data = parent::getProacc();
+		if($data=='P') return 'Proyecto';
+		if($data=='A') return 'Acción Centralizada';
+	}	
 }
