611be4deb7acefb047247a675a78ce73173e24e0
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 22:08:01 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@300 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Fordefcatpre.php
+++ lib/model/Fordefcatpre.php
@@ -3,10 +3,21 @@
 /**
  * Subclass for representing a row from the 'fordefcatpre' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Fordefcatpre extends BaseFordefcatpre
 {
+	public function getNomemp()
+	{
+		$c = new Criteria();
+		$c->add(NpasicarempPeer::CODEMP,str_pad(self::getCodemp(),16 ,' '));
+		$nomemp = NpasicarempPeer::doSelectone($c);
+		if ($nomemp){
+			return $nomemp->getNomemp();
+		}else{
+			return ' ';
+		}
+	}	
 }
