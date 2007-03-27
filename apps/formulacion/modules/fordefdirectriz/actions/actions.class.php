305d8019f0dd6500b1d23b33787224cc9e42d262
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 20:57:25 +0000

    Jesus Lobaton - fomulacion
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@297 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/formulacion/modules/fordefdirectriz/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * fordefdirectriz actions.
+ *
+ * @package    siga
+ * @subpackage fordefdirectriz
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class fordefdirectrizActions extends autofordefdirectrizActions
+{
+}
