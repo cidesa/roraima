b90322566f57a9ed9b4c69399fb482ad455dec39
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 16:24:37 +0000

    Jesus Lobaton - Formulacion -
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@283 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/formulacion/modules/fordefsubobj/actions/actions.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * fordefsubobj actions.
+ *
+ * @package    siga
+ * @subpackage fordefsubobj
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class fordefsubobjActions extends autofordefsubobjActions
+{
+
+}
