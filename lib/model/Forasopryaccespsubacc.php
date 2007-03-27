114eadfbb2658453105f5539a896a62bf02fcdd9
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 22:07:22 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@298 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/Forasopryaccespsubacc.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'forasopryaccespsubacc' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Forasopryaccespsubacc extends BaseForasopryaccespsubacc
+{
+}
