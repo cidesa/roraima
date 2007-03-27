036581cb490cb7826faee457e32521944f1dc2eb
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 19:21:08 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@286 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Nptipcon.php
+++ lib/model/Nptipcon.php
@@ -3,10 +3,55 @@
 /**
  * Subclass for representing a row from the 'nptipcon' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Nptipcon extends BaseNptipcon
 {
+	public function getNomnom()
+	{
+		$c = new Criteria();
+		$c->add(NpasinomcontPeer::CODTIPCON,self::getCodtipcon());
+		$c->addJoin(NpnominaPeer::CODNOM,NpasinomcontPeer::CODNOM);		
+		$codnom = NpnominaPeer::doSelectone($c);
+		if ($codnom){
+			return $codnom->getNomnom();
+		}else{
+			return ' ';
+		}
+	}
+
+	public function getFrepagcon()
+	{
+		$data = parent::getFrepagcon();
+		if($data=='M') return 'MENSUAL';
+		if($data=='S') return 'SEMANAL';
+		if($data=='Q') return 'QUINCENAL';
+		if($data=='E') return 'MENSUAL ANTICIPO';
+	}
+
+	public function getAnovig()
+	{
+		$c = new Criteria();
+		$c->add(NpbonocontPeer::CODTIPCON,self::getCodtipcon());
+		$anovig = NpbonocontPeer::doSelectone($c);
+		if ($anovig){
+			return $anovig->getAnovig();
+		}else{
+			return ' ';
+		}
+	}
+	public function getAnovighas()
+	{
+		$c = new Criteria();
+		$c->add(NpbonocontPeer::CODTIPCON,self::getCodtipcon());
+		$anovighas = NpbonocontPeer::doSelectone($c);
+		if ($anovighas){
+			return $anovighas->getAnovighas();
+		}else{
+			return ' ';
+		}
+	}	
+	
 }
