076f290ae0528f31dcd065f6c6a3ef73fe66edb5
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 19:20:56 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@285 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/NpempleadosBanco.php
+++ lib/model/NpempleadosBanco.php
@@ -3,10 +3,33 @@
 /**
  * Subclass for representing a row from the 'npempleados_banco' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class NpempleadosBanco extends BaseNpempleadosBanco
 {
+	public function getNomemp()
+	{
+		$c = new Criteria();
+		$c->add(NphojintPeer::CODEMP,str_pad(self::getCodemp(), 16, " "));
+		$nomemp = NphojintPeer::doSelectone($c);
+		if ($nomemp)
+		return $nomemp->getNomemp();
+		else
+		return ' ';
+	}
+
+	public function getNomban()
+	{
+		$c = new Criteria();
+		$c->add(NpbancosPeer::CODBAN,self::getCodban());
+		$nomban = NpbancosPeer::doSelectone($c);
+    	if ($nomban)
+    	  return $nomban->getNomban();
+	    else
+	      return ' ';
+    }    
 }
+
+
