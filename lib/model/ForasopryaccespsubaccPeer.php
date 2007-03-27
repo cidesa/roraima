8ed50086516fd0f1cd86f9de75120c9099bbdae6
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 22:07:31 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@299 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/ForasopryaccespsubaccPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'forasopryaccespsubacc' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class ForasopryaccespsubaccPeer extends BaseForasopryaccespsubaccPeer
+{
+}
