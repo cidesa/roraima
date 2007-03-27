198152cb8c286ca49a1d17cb9750b4a23ca9932f
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 19:29:10 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@288 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/Fordefaccpoa.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'fordefaccpoa' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Fordefaccpoa extends BaseFordefaccpoa
+{
+}
---
+++ lib/model/FordefaccpoaPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'fordefaccpoa' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class FordefaccpoaPeer extends BaseFordefaccpoaPeer
+{
+}
