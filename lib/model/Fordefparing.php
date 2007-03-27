1aafca12a18aee68d60e065e7e9f68074da1f64f
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-27 23:46:46 +0000

    Jesus Lobaton - model
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@319 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Fordefparing.php
+++ lib/model/Fordefparing.php
@@ -3,10 +3,45 @@
 /**
  * Subclass for representing a row from the 'fordefparing' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Fordefparing extends BaseFordefparing
 {
+	public function getMontoing()
+	{
+		$c = new Criteria();
+		$c->add(ForparingPeer::CODPARING,str_pad(self::getCodparing(),32,' '));
+		$montoing = ForparingPeer::doSelectone($c);
+		if ($montoing)
+	  	  #return $montoing->getMontoing();
+	  	  return number_format($montoing->getMontoing(),2,'.',',');
+	  	  else
+	  	  return number_format('0.00',2,'.',',');
+	}
+
+	public function getCodtipfin()
+	{
+		$c = new Criteria();
+		$c->add(ForparingPeer::CODPARING,str_pad(self::getCodparing(),32,' '));
+		$rs = ForparingPeer::doSelectone($c);
+		if ($rs)
+			return $rs->getcodtipfin();		
+	  	  else
+	  	  	return ' ';
+	}	
+	
+	
+	public function getNomext()
+	{
+		$c = new Criteria();
+		$c->add(ForparingPeer::CODPARING,str_pad(self::getCodparing(),32,' '));
+		$c->addJoin(FortipfinPeer::CODFIN,ForparingPeer::CODTIPFIN);
+		$nomext = FortipfinPeer::doSelectone($c);
+		if ($nomext)		
+		    return $nomext->getNomext();
+	    else 
+	        return ' 1';
+    }	    
 }
