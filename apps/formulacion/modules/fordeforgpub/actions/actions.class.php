8b0f541e6fe1ef0a2f597803792f23b62ec62352
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-28 12:26:44 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@324 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/formulacion/modules/fordeforgpub/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * fordeforgpub actions.
+ *
+ * @package    siga
+ * @subpackage fordeforgpub
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class fordeforgpubActions extends autofordeforgpubActions
+{
+}
---
+++ apps/formulacion/modules/fordefstatus/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * fordefstatus actions.
+ *
+ * @package    siga
+ * @subpackage fordefstatus
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class fordefstatusActions extends autofordefstatusActions
+{
+}
