6911017a46146520a83ec58644d2b86719c8cfd5
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-28 18:25:20 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@349 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Fordisactperpryaccmet.php
+++ lib/model/Fordisactperpryaccmet.php
@@ -9,4 +9,25 @@
  */ 
 class Fordisactperpryaccmet extends BaseFordisactperpryaccmet
 {
+  public function getNompro()
+  {
+  	  $c = new Criteria();
+  	  $c->add(FordefpryPeer::CODPRO,self::getCodpro());
+  	  $nombre = FordefpryPeer::doSelectone($c);
+	  if ($nombre)
+	  	return $nombre->getNompro();
+	  else 
+	    return ' ';
+  }
+	
+public function getDesmet()
+  {
+  	  $c = new Criteria();
+  	  $c->add(FordefpryaccmetPeer::CODMET,self::getCodmet());
+  	  $descripcion = FordefpryaccmetPeer::doSelectone($c);
+	  if ($descripcion)
+	  	return $descripcion->getDesmet();
+	  else 
+	    return ' ';
+  }
 }
