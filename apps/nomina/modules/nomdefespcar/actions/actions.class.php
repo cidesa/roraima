8f1eb388bb1c15e32bd573c0f0a5b33cddec8e29
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-28 13:23:14 +0000

    Miki
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@329 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/nomina/modules/nomdefespcar/actions/actions.class.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * nomdefespcar actions.
+ *
+ * @package    siga
+ * @subpackage nomdefespcar
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class nomdefespcarActions extends autonomdefespcarActions
+{
+	public function gridperfil()    
+    {
+        $con = sfContext::getInstance()->getDatabaseConnection($connection='propel');
+        $sql = "select a.codperfil, b.desperfil, a.puntos FROM npperfil b,nppercar a where rpad(a.codcar,16,' ') =rpad('".$this->npcargos->getCodcar()."',16,' ') and a.codperfil=b.codperfil";
+        $stmt = $con->createStatement();
+                $rs = $stmt->executeQuery($sql, ResultSet::FETCHMODE_NUM);
+        $resultado=array();
+           while ($rs->next())
+             {
+                $resultado[]=$rs->getRow();
+             }
+        $this->rs=$resultado;
+        return $this->rs;
+    }
+    
+  public function executeEdit()
+  {
+    $this->npcargos = $this->getNpcargosOrCreate();
+    if ($this->npcargos->getId())
+    {
+    	$this->grid = $this->gridperfil();
+    }
+    else
+    {
+		$this->grid = '';    	
+    }
+    
+   
+    if ($this->getRequest()->getMethod() == sfRequest::POST)
+    {
+      $this->updateNpcargosFromRequest();
+
+      $this->saveNpcargos($this->npcargos);
+
+      $this->setFlash('notice', 'Your modifications have been saved');
+
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('nomdefespcar/create');
+      }
+      else if ($this->getRequestParameter('save_and_list'))
+      {
+        return $this->redirect('nomdefespcar/list');
+      }
+      else
+      {
+        return $this->redirect('nomdefespcar/edit?id='.$this->npcargos->getId());
+      }
+    }
+    else
+    {
+      $this->labels = $this->getLabels();
+    }
+  }
+  
+  protected function updateNpcargosFromRequest()
+  {
+    $npcargos = $this->getRequestParameter('npcargos');
+
+    if (isset($npcargos['codcar']))
+    {
+      $this->npcargos->setCodcar($npcargos['codcar']);
+    }
+    if (isset($npcargos['nomcar']))
+    {
+      $this->npcargos->setNomcar($npcargos['nomcar']);
+    }
+    if (isset($npcargos['codtip']))
+    {
+      $this->npcargos->setCodtip($npcargos['codtip']);
+    }
+    if (isset($npcargos['graocp']))
+    {
+      $this->npcargos->setGraocp($npcargos['graocp']);
+    }
+    if (isset($npcargos['suecar']))
+    {
+      $this->npcargos->setSuecar($npcargos['suecar']);
+    }
+    if (isset($npcargos['punmin']))
+    {
+      $this->npcargos->setPunmin($npcargos['punmin']);
+    }
+    //if (isset($npcargos['stacar']))
+    //{
+      $this->npcargos->setStacar($this->getRequestParameter('stacar'));
+    //}
+  }
+    
+}
---
+++ apps/nomina/modules/nomdefespgen/actions/actions.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * nomdefespgen actions.
+ *
+ * @package    siga
+ * @subpackage nomdefespgen
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class nomdefespgenActions extends autonomdefespgenActions
+{
+  protected function updateNpdefgenFromRequest()
+  {
+    $npdefgen = $this->getRequestParameter('npdefgen');
+
+    if (isset($npdefgen['codemp']))
+    {
+      $this->npdefgen->setCodemp($npdefgen['codemp']);
+    }
+    if (isset($npdefgen['nomemp']))
+    {
+      $this->npdefgen->setNomemp($npdefgen['nomemp']);
+    }
+    if (isset($npdefgen['forcar']))
+    {
+      $this->npdefgen->setForcar($npdefgen['forcar']);
+    }
+    if (isset($npdefgen['foremp']))
+    {
+      $this->npdefgen->setForemp($npdefgen['foremp']);
+    }
+    if (isset($npdefgen['fororg']))
+    {
+      $this->npdefgen->setFororg($npdefgen['fororg']);
+    }
+    if (isset($npdefgen['foruni']))
+    {
+      $this->npdefgen->setForuni($npdefgen['foruni']);
+    }
+    if (isset($npdefgen['unitrib']))
+    {
+      $this->npdefgen->setUnitrib($npdefgen['unitrib']);
+    }
+    //if (isset($npdefgen['redmon']))
+    //{
+      $this->npdefgen->setRedmon($this->getRequestParameter('redmon'));
+    //}
+    //if (isset($npdefgen['codpre']))
+    //{
+      $this->npdefgen->setCodpre($this->getRequestParameter('codpre'));
+    //}
+    //if (isset($npdefgen['asiconnom']))
+    //{
+      $this->npdefgen->setAsiconnom($this->getRequestParameter('asiconnom'));
+    //}
+  }
+	
+}
