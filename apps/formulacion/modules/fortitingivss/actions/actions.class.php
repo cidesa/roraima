5128ba312a57f9c56b4a0ff3a8da16e456a6ae30
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-28 00:03:32 +0000

    Jesus Lobaton - forttingivss
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@321 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/formulacion/modules/fortitingivss/actions/actions.class.php
+++ apps/formulacion/modules/fortitingivss/actions/actions.class.php
@@ -10,4 +10,29 @@
  */
 class fortitingivssActions extends autofortitingivssActions
 {
+	protected function updateFordefparingFromRequest()
+	{
+		$fordefparing = $this->getRequestParameter('fordefparing');
+
+		if (isset($fordefparing['codparing']))
+		{
+			$this->fordefparing->setCodparing(str_pad($fordefparing['codparing'],32,' '));
+		}
+		if (isset($fordefparing['nomparing']))
+		{
+			$this->fordefparing->setNomparing($fordefparing['nomparing']);
+		}
+		if (isset($fordefparing['montoing']))
+		{
+			$this->fordefparing->setMontoing($fordefparing['montoing']);
+		}
+		if (isset($fordefparing['codtipfin']))
+		{
+			$this->fordefparing->setCodtipfin(str_pad($fordefparing['codtipfin'],4,'0',STR_PAD_LEFT));
+    }
+    if (isset($fordefparing['nomext']))
+    {
+      $this->fordefparing->setNomext($fordefparing['nomext']);
+    }
+  }	
 }
