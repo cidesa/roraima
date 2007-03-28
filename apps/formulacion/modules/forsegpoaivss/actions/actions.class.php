46d356d5b517f918fd3ae4bf2e638ad252d25b00
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-28 18:24:54 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@348 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/formulacion/modules/forsegpoaivss/actions/actions.class.php
+++ apps/formulacion/modules/forsegpoaivss/actions/actions.class.php
@@ -10,4 +10,77 @@
  */
 class forsegpoaivssActions extends autoforsegpoaivssActions
 {
+  public function executeEdit()
+  {
+    $this->fordisactperpryaccmet = $this->getFordisactperpryaccmetOrCreate();
+
+    if ($this->getRequest()->getMethod() == sfRequest::POST)
+    {
+      //$this->updateFordisactperpryaccmetFromRequest();
+
+      //$this->saveFordisactperpryaccmet($this->fordisactperpryaccmet);
+
+      $this->setFlash('notice', 'Your modifications have been saved');
+
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('forsegpoaivss/create');
+      }
+      else if ($this->getRequestParameter('save_and_list'))
+      {
+        return $this->redirect('forsegpoaivss/list');
+      }
+      else
+      {
+        return $this->redirect('forsegpoaivss/edit?id='.$this->fordisactperpryaccmet->getId());
+      }
+    }
+    else
+    {
+      $this->labels = $this->getLabels();
+    }
+  }
+	
+  protected function updateFordisactperpryaccmetFromRequest()
+  {
+    $fordisactperpryaccmet = $this->getRequestParameter('fordisactperpryaccmet');
+
+    if (isset($fordisactperpryaccmet['codpro']))
+    {
+      $this->fordisactperpryaccmet->setCodpro($fordisactperpryaccmet['codpro']);
+    }
+    if (isset($fordisactperpryaccmet['codaccesp']))
+    {
+      $this->fordisactperpryaccmet->setCodaccesp($fordisactperpryaccmet['codaccesp']);
+    }
+    if (isset($fordisactperpryaccmet['codmet']))
+    {
+      $this->fordisactperpryaccmet->setCodmet($fordisactperpryaccmet['codmet']);
+    }
+    if (isset($fordisactperpryaccmet['codact']))
+    {
+      $this->fordisactperpryaccmet->setCodact($fordisactperpryaccmet['codact']);
+    }
+    if (isset($fordisactperpryaccmet['perpre']))
+    {
+      $this->fordisactperpryaccmet->setPerpre($fordisactperpryaccmet['perpre']);
+    }
+    if (isset($fordisactperpryaccmet['canact']))
+    {
+      $this->fordisactperpryaccmet->setCanact($fordisactperpryaccmet['canact']);
+    }
+    if (isset($fordisactperpryaccmet['canacteje']))
+    {
+      $this->fordisactperpryaccmet->setCanacteje($fordisactperpryaccmet['canacteje']);
+    }
+    if (isset($fordisactperpryaccmet['canmet']))
+    {
+      $this->fordisactperpryaccmet->setCanmet($fordisactperpryaccmet['canmet']);
+    }
+    if (isset($fordisactperpryaccmet['canmeteje']))
+    {
+      $this->fordisactperpryaccmet->setCanmeteje($fordisactperpryaccmet['canmeteje']);
+    }
+  }
+  
 }
