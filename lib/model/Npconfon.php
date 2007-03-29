9df49715de8ad1a7d74e9a94910ce684898f97a6
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-29 19:12:57 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@366 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npconfon.php
+++ lib/model/Npconfon.php
@@ -9,4 +9,26 @@
  */ 
 class Npconfon extends BaseNpconfon
 {
+  public function getNomnom()
+  {
+  	  $c = new Criteria();
+  	  $c->add(NpnominaPeer::CODNOM,self::getCodnom());
+  	  $nombre = NpnominaPeer::doSelectone($c);
+	  if ($nombre)
+	  	return $nombre->getNomnom();
+	  else 
+	    return ' ';
+  }
+  
+  public function getNomcon()
+  {
+  	  $c = new Criteria();
+  	  $c->add(NpdefcptPeer::CODCON,self::getCodCon());
+  	  $nombre1 = NpdefcptPeer::doSelectone($c);
+	  if ($nombre1)
+	  	return $nombre1->getNomcon();
+	  else 
+	    return ' ';
+  }
+	
 }
