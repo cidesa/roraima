3396f5dc26becfe26399d4b2f00a3fa02714db8f
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-29 20:56:11 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@372 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npasiempcont.php
+++ lib/model/Npasiempcont.php
@@ -9,4 +9,15 @@
  */ 
 class Npasiempcont extends BaseNpasiempcont
 {
+  public function getDestipcon()
+  {
+  	  $c = new Criteria();
+  	  $c->add(NptipconPeer::CODTIPCON,self::getCodtipcon());
+  	  $nombre2 = NptipconPeer::doSelectone($c);
+	  if ($nombre2)
+	  	return $nombre2->getDestipcon();
+	  else 
+	    return ' ';
+  }
+	
 }
