03eb43d7d67f7e12276cf2bbee2a9a6cf94c703a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-29 21:09:28 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@374 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npasicaremp.php
+++ lib/model/Npasicaremp.php
@@ -9,16 +9,51 @@
  */
 class Npasicaremp extends BaseNpasicaremp
 {
-	public function getcodcon()
+	public function getCodcon()
 	{
 		$c = new Criteria();
 		$c->add(NpasiconempPeer::CODEMP,self::getCodemp());
 		$c->addJoin(NpasiconempPeer::CODCAR,NpasicarempPeer::CODCAR);
 		$codigo = NpasiconempPeer::doSelectone($c);
 		if ($codigo){
-			return $codigo->getcodcon();
-  		}else{
-	      return ' ';
-  		}
-    }	  	
+			return $codigo->getCodcon();
+		}else{
+			return ' ';
+		}
+	}
+	public function getNomnom()
+	{
+		$c = new Criteria();
+		$c->add(NpnominaPeer::CODNOM,self::getCodnom());
+		$nomnom = NpnominaPeer::doSelectone($c);
+		if ($nomnom){
+			return $nomnom->getNomnom();
+		}else{
+			return ' ';
+		}
+	}
+	public function getDestipgas()
+	{
+		$c = new Criteria();
+		$c->add(NptipgasPeer::CODTIPGAS,self::getCodtipgas());
+		$destipgas = NptipgasPeer::doSelectone($c);
+		if ($destipgas){
+			return $destipgas->getDestipgas();
+		}else{
+			return ' ';
+		}
+	}
+	public function getDespaso()
+	{
+		$c = new Criteria();
+		$c->add(NpcargosPeer::CODCAR,self::getCodcar());
+		$c->add(NpcomocpPeer::CODTIPCAR,NpcargosPeer::CODTIP);
+		$c->add(NpcomocpPeer::GRACAR,NpcargosPeer::GRAOCP);
+		$suecar = NpcomOcpPeer::doSelectone($c);
+		if ($suecar){
+			return $suecar->getSuecar();
+		}else{
+			return ' ';
+		}
+	}	
 }
