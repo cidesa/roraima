542abb35431d46b918bebcf134b219f95424b94e
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-03-29 16:30:58 +0000

    Jesus Lobaton - npdefcpt
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@365 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Npdefcpt.php
+++ lib/model/Npdefcpt.php
@@ -3,10 +3,17 @@
 /**
  * Subclass for representing a row from the 'npdefcpt' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Npdefcpt extends BaseNpdefcpt
 {
+	public function getestado()
+	{
+		if (($this->getNomcon())=='S')
+			return 'Activo';
+		else
+ 		   return 'Inactivo';
+  	}	
 }
