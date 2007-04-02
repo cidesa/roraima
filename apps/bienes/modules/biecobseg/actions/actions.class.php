80b68d2650839b311861b032f9607669c603d19a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-02 19:32:28 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@405 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/bienes/modules/biecobseg/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * biecobseg actions.
+ *
+ * @package    siga
+ * @subpackage biecobseg
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class biecobsegActions extends autobiecobsegActions
+{
+}
---
+++ apps/bienes/modules/biedefemp/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * biedefemp actions.
+ *
+ * @package    siga
+ * @subpackage biedefemp
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class biedefempActions extends autobiedefempActions
+{
+}
