75498eaee1879627fc5b040b7d64dc465bec42b0
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-02 20:49:35 +0000

    Jesus Lobaton - bienes - biecatact
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@408 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/bienes/modules/biecatact/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * biecatact actions.
+ *
+ * @package    siga
+ * @subpackage biecatact
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class biecatactActions extends autobiecatactActions
+{
+}
