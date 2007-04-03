5aa3fa32727af04edd1995131346bfa1554f703c
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-03 19:00:27 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@428 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Bndisinm.php
+++ lib/model/Bndisinm.php
@@ -9,4 +9,36 @@
  */ 
 class Bndisinm extends BaseBndisinm
 {
+  public function getDesinm()
+  {
+  	  $c = new Criteria();
+  	  $c->add(BnreginmPeer::CODINM,self::getCodmue());
+  	  $descripcion = BnreginmPeer::doSelectone($c);
+	  if ($descripcion)
+	  	return $descripcion->getDesinm();
+	  else 
+	    return ' ';
+   }
+	
+  public function getDesubiori()
+  {
+  	  $c = new Criteria();
+  	  $c->add(BnubibiePeer::CODUBI,self::getCodubiori());
+  	  $origen = BnubibiePeer::doSelectone($c);
+	  if ($origen)
+	  	return $origen->getDesubi();
+	  else 
+	    return ' ';
+   }
+   
+  public function getDesubides()
+  {
+  	  $c = new Criteria();
+  	  $c->add(BnubibiePeer::CODUBI,self::getCodubides());
+  	  $destino = BnubibiePeer::doSelectone($c);
+	  if ($destino)
+	  	return $destino->getDesubi();
+	  else 
+	    return ' ';
+   }
 }
