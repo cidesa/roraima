09f9f45a02dfe46312f4a74e2707ae41a4bebb85
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-03 12:46:11 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@412 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/bienes/modules/biedefconi/actions/actions.class.php
+++ apps/bienes/modules/biedefconi/actions/actions.class.php
@@ -10,4 +10,89 @@
  */
 class biedefconiActions extends autobiedefconiActions
 {
+	protected function updateBndefconiFromRequest()
+	{
+		$bndefconi = $this->getRequestParameter('bndefconi');
+
+		if (isset($bndefconi['codact']))
+		{
+			$this->bndefconi->setCodact($bndefconi['codact']);
+		}
+		if (isset($bndefconi['codinm']))
+		{
+			$this->bndefconi->setCodinm($bndefconi['codinm']);
+		}
+		if (isset($bndefconi['desmue']))
+		{
+			$this->bndefconi->setDesmue($bndefconi['desmue']);
+		}
+		if (isset($bndefconi['ctadepcar']))
+		{
+			$this->bndefconi->setCtadepcar($bndefconi['ctadepcar']);
+		}
+		if (isset($bndefconi['descta']))
+		{
+			$this->bndefconi->setDescta($bndefconi['descta']);
+		}
+		if (isset($bndefconi['ctadepabo']))
+		{
+			$this->bndefconi->setCtadepabo($bndefconi['ctadepabo']);
+		}
+		if (isset($bndefconi['desctaabo']))
+		{
+			$this->bndefconi->setDesctaabo($bndefconi['desctaabo']);
+		}
+		if (isset($bndefconi['ctaajucar']))
+		{
+			$this->bndefconi->setCtaajucar($bndefconi['ctaajucar']);
+		}
+		if (isset($bndefconi['desctaajucar']))
+		{
+			$this->bndefconi->setDesctaajucar($bndefconi['desctaajucar']);
+		}
+		if (isset($bndefconi['ctaajuabo']))
+		{
+			$this->bndefconi->setCtaajuabo($bndefconi['ctaajuabo']);
+		}
+		if (isset($bndefconi['desctaajuabo']))
+		{
+			$this->bndefconi->setDesctaajuabo($bndefconi['desctaajuabo']);
+		}
+		if (isset($bndefconi['ctarevcar']))
+		{
+			$this->bndefconi->setCtarevcar($bndefconi['ctarevcar']);
+		}
+		if (isset($bndefconi['desctarevcar']))
+		{
+			$this->bndefconi->setDesctarevcar($bndefconi['desctarevcar']);
+		}
+		if (isset($bndefconi['ctarevabo']))
+		{
+			$this->bndefconi->setCtarevabo($bndefconi['ctarevabo']);
+		}
+		if (isset($bndefconi['desctarevabo']))
+		{
+			$this->bndefconi->setDesctarevabo($bndefconi['desctarevabo']);
+		}
+		if (isset($bndefconi['ctapercar']))
+		{
+			$this->bndefconi->setCtapercar($bndefconi['ctapercar']);
+		}
+		if (isset($bndefconi['desctapercar']))
+		{
+			$this->bndefconi->setDesctapercar($bndefconi['desctapercar']);
+		}
+		if (isset($bndefconi['ctaperabo']))
+		{
+			$this->bndefconi->setCtaperabo($bndefconi['ctaperabo']);
+		}
+		if (isset($bndefconi['desctaperabo']))
+    {
+      $this->bndefconi->setDesctaperabo($bndefconi['desctaperabo']);
+    }
+		if (!isset($bndefconi['stacta']))
+		{
+			$this->bndefconi->setStacta('A');
+		}       
+  }	
 }
