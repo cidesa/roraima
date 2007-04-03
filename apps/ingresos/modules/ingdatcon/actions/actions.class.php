0b961e6238ecb04030c0a46378a3f59475e3e290
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-03 21:16:08 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@434 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/ingresos/modules/ingdatcon/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * ingdatcon actions.
+ *
+ * @package    siga
+ * @subpackage ingdatcon
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class ingdatconActions extends autoingdatconActions
+{
+}
