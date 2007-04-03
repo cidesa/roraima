710cf45569f02c1a7e323aa9363c8ce6afd95091
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-03 15:11:08 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@421 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Bnparbie.php
+++ lib/model/Bnparbie.php
@@ -9,4 +9,25 @@
  */ 
 class Bnparbie extends BaseBnparbie
 {
+  public function getNompardes()
+  {
+  	  $c = new Criteria();
+  	  $c->add(NppartidasPeer::CODPAR,self::getPardes());
+  	  $nombredes = NppartidasPeer::doSelectone($c);
+	  if ($nombredes)
+	  	return $nombredes->getNompar();
+	  else 
+	    return ' ';
+   }
+   
+  public function getNomparhas()
+  {
+  	  $c = new Criteria();
+  	  $c->add(NppartidasPeer::CODPAR,self::getParhas());
+  	  $nombrehas = NppartidasPeer::doSelectone($c);
+	  if ($nombrehas)
+	  	return $nombrehas->getNompar();
+	  else 
+	    return ' ';
+   }
 }
--- lib/model/Npnomcal.php
+++ lib/model/Npnomcal.php
@@ -9,4 +9,25 @@
  */ 
 class Npnomcal extends BaseNpnomcal
 {
+  public function getNomemp()
+  {
+  	  $c = new Criteria();
+  	  $c->add(NphojintPeer::CODEMP,self::getCodemp());
+  	  $nombre = NphojintPeer::doSelectone($c);
+	  if ($nombre)
+	  	return $nombre->getNomemp();
+	  else 
+	    return ' ';
+  }
+	
+ public function getFrecal()
+  {
+  	  $c = new Criteria();
+  	  $c->add(NpnominaPeer::CODNOM,self::getCodnom());
+  	  $frecuencia = NpnominaPeer::doSelectone($c);
+	  if ($frecuencia)
+	  	return $frecuencia->getFrecal();
+	  else 
+	    return ' ';
+  }
 }
