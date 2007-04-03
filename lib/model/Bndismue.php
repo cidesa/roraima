8ca40a4c687fce27eeadd73539ad0f27e9bb83ee
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-03 17:54:46 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@426 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Bndismue.php
+++ lib/model/Bndismue.php
@@ -9,4 +9,49 @@
  */ 
 class Bndismue extends BaseBndismue
 {
+  public function getDesmue()
+  {
+  	  $c = new Criteria();
+  	  $c->add(BnregmuePeer::CODMUE,self::getCodmue());
+  	  $descripcion = BnregmuePeer::doSelectone($c);
+	  if ($descripcion)
+	  	return $descripcion->getDesmue();
+	  else 
+	    return ' ';
+   }
+   
+  public function getDesmot()
+  {
+  	  $c = new Criteria();
+  	  $c->add(BnmotdisPeer::CODMOT,self::getCodmot());
+  	  $motivo = BnmotdisPeer::doSelectone($c);
+	  if ($motivo)
+	  	return $motivo->getDesmot();
+	  else 
+	    return ' ';
+   }
+   
+  public function getDesubiori()
+  {
+  	  $c = new Criteria();
+  	  $c->add(BnubibiePeer::CODUBI,self::getCodubiori());
+  	  $origen = BnubibiePeer::doSelectone($c);
+	  if ($origen)
+	  	return $origen->getDesubi();
+	  else 
+	    return ' ';
+   }
+   
+  public function getDesubides()
+  {
+  	  $c = new Criteria();
+  	  $c->add(BnubibiePeer::CODUBI,self::getCodubides());
+  	  $destino = BnubibiePeer::doSelectone($c);
+	  if ($destino)
+	  	return $destino->getDesubi();
+	  else 
+	    return ' ';
+   }
 }
+
+
