8f6a47c9d463589bb79a6492f76b26304d939a2e
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-03 15:53:04 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@423 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/bienes/modules/biedefconlots/actions/actions.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * biedefconlots actions.
+ *
+ * @package    siga
+ * @subpackage biedefconlots
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class biedefconlotsActions extends autobiedefconlotsActions
+{
+  public function executeIndex()
+  {
+    return $this->forward('biedefconlots', 'edit');
+  }	
+}
