85fd7df7eb5bb0c0ce70d3d0d3a7043fe9aaf77a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-03 15:10:18 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@420 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/bienes/modules/biedefpar/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * biedefpar actions.
+ *
+ * @package    siga
+ * @subpackage biedefpar
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class biedefparActions extends autobiedefparActions
+{
+}
