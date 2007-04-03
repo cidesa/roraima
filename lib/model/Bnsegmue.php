5e6995ef3745b533fefacd232a7d892dde5f5b70
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-03 19:01:51 +0000

    Jesus Lobaton - model
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@430 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Bnsegmue.php
+++ lib/model/Bnsegmue.php
@@ -3,10 +3,22 @@
 /**
  * Subclass for representing a row from the 'bnsegmue' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Bnsegmue extends BaseBnsegmue
 {
+	public function getDesmue()
+	{
+		$c = new Criteria();
+		$c->add(BnregmuePeer::CODACT,self::getCodact());
+		$c->add(BnregmuePeer::CODMUE,self::getCodmue());
+		$registro = BnregmuePeer::doSelectOne($c);
+		if($registro)
+		return $registro->getDesmue();
+		else 
+                         return null; 
+		
+	}	
 }
