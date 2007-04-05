5c89ddaaed5309aaee59895fc3163e4eff4651db
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-05 14:07:36 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@441 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/obras/modules/oycdefrec/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * oycdefrec actions.
+ *
+ * @package    siga
+ * @subpackage oycdefrec
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class oycdefrecActions extends autooycdefrecActions
+{
+}
