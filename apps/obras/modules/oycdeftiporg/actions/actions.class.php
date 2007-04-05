6611289579f2bd5aace78e9f49f2a1fb0fb7e523
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-05 15:14:12 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@444 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/obras/modules/oycdeftiporg/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * oycdeftiporg actions.
+ *
+ * @package    siga
+ * @subpackage oycdeftiporg
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class oycdeftiporgActions extends autooycdeftiporgActions
+{
+}
