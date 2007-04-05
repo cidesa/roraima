950f7efa9f92b479085d709b17fdb8fd406d8cdc
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-05 14:07:51 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@442 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Carecaud.php
+++ lib/model/Carecaud.php
@@ -3,10 +3,22 @@
 /**
  * Subclass for representing a row from the 'carecaud' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Carecaud extends BaseCarecaud
 {
+	public function getDestiprec()
+	//JJSG******
+	{
+		$c = new Criteria();
+		$c->add(CatiprecPeer::CODTIPREC,self::getCodtiprec());
+		$destiprec = CatiprecPeer::doSelectone($c);
+		if ($destiprec){
+			return $destiprec->getDestiprec();
+		}else{
+			return '<!No Encontrado o Vacio> ';
+		}
+	}
 }
