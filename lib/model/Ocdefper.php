fda60d2913137b7da73a4d1a792a576df70b2d81
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-05 15:52:32 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@449 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Ocdefper.php
+++ lib/model/Ocdefper.php
@@ -3,10 +3,32 @@
 /**
  * Subclass for representing a row from the 'ocdefper' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Ocdefper extends BaseOcdefper
 {
+	public function getDestipcar()
+	{
+		$c = new Criteria();
+		$c->add(OctipcarPeer::CODTIPCAR,self::getCodtipcar());
+		$destipcar = OctipcarPeer::doSelectone($c);
+		if ($destipcar){
+			return $destipcar->getDestipcar();
+		}else{
+			return '<!Nombre no encontrado!>';
+		}
+	}
+	public function getDestippro()
+	{
+		$c = new Criteria();
+		$c->add(OctipproPeer::CODTIPPRO,self::getCodtippro());
+		$destippro = OctipproPeer::doSelectone($c);
+		if ($destippro){
+			return $destippro->getDestippro();
+		}else{
+			return '<!Nombre no encontrado!>';
+		}
+	}			
 }
