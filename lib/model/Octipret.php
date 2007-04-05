e96ef728d58dca6480b6c3d4c84b4c058812dd72
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-05 15:22:20 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@447 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Octipret.php
+++ lib/model/Octipret.php
@@ -3,10 +3,22 @@
 /**
  * Subclass for representing a row from the 'octipret' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Octipret extends BaseOctipret
 {
+	public function getDescta()
+	//JJSG******
+	{
+		$c = new Criteria();
+		$c->add(ContabbPeer::CODCTA,self::getCodcon());
+		$descta = ContabbPeer::doSelectone($c);
+		if ($descta){
+			return $descta->getDescta();
+		}else{
+			return '<!No Encontrado o Vacio> ';
+		}
+	}	
 }
