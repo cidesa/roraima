58e2f4ab8dd18ce420d2e5122ba6573390b2ad83
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-05 16:08:57 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@452 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/obras/modules/oycdefesp/actions/actions.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * oycdefesp actions.
+ *
+ * @package    siga
+ * @subpackage oycdefesp
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class oycdefespActions extends autooycdefespActions
+{
+	protected function updateOctipespFromRequest()
+	{
+		$octipesp = $this->getRequestParameter('octipesp');
+
+		if (isset($octipesp['codtipesp']))
+		{
+			$this->octipesp->setCodtipesp(str_pad($octipesp['codtipesp'],4,'0',STR_PAD_LEFT));
+    }
+    if (isset($octipesp['destipesp']))
+    {
+      $this->octipesp->setDestipesp($octipesp['destipesp']);
+    }
+  }
+	
+}
