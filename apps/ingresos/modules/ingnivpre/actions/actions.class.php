297d005b99a1d6703526d3306f31328f25e55abe
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-10 13:10:46 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@457 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/ingresos/modules/ingnivpre/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * ingnivpre actions.
+ *
+ * @package    siga
+ * @subpackage ingnivpre
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class ingnivpreActions extends autoingnivpreActions
+{
+}
