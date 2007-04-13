77cf655db4bdbdff7ae30f3970d83644e2914a97
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-13 23:07:48 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@467 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Ciasiini.php
+++ lib/model/Ciasiini.php
@@ -9,4 +9,14 @@
  */ 
 class Ciasiini extends BaseCiasiini
 {
+	public function getNompre()
+	{
+		// Se obtiene Nomnom de la tabla Npasicaremp
+		
+		$c = new Criteria();
+		$c->add(CideftitPeer::CODPRE,str_pad(self::getCodpre(),32,' '));
+		$registro = CideftitPeer::doSelectOne($c);
+		if($registro) return $registro->getNompre();
+		else return null; 
+	}
 }
