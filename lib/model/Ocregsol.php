4c67eee7065ea669797859a133ae0814d52901c8
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-16 12:51:29 +0000

    lib - obras
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@480 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Ocregsol.php
+++ lib/model/Ocregsol.php
@@ -9,4 +9,50 @@
  */ 
 class Ocregsol extends BaseOcregsol
 {
+	public function getNomste()
+	{
+		$c = new Criteria();
+		$c->add(OcdatstePeer::CEDSTE,self::getCedste());
+		$registro = OcdatstePeer::doSelectOne($c);
+		if($registro) 
+                         return $registro->getNomste();
+		else 
+                         return null; 
+		
+	}
+	public function getDessol()
+	{
+		$c = new Criteria();
+		$c->add(OctipsolPeer::CODSOL,self::getCodsol());
+		$registro = OctipsolPeer::doSelectOne($c);
+		if($registro) 
+                         return $registro->getDessol();
+		else 
+                         return null; 
+		
+	}
+	public function getDesorg()
+	{
+		$c = new Criteria();
+		$c->add(OcdeforgPeer::CODORG,self::getCodorg());
+		$registro = OcdeforgPeer::doSelectOne($c);
+		if($registro) 
+                         return $registro->getDesorg();
+		else 
+                         return null; 
+		
+	}	
+		
+	public function getNomemp()
+	{
+		$c = new Criteria();
+		$c->add(NphojintPeer::CODEMP,self::getCodemp());
+		$registro = NphojintPeer::doSelectOne($c);
+		if($registro) 
+                         return $registro->getNomemp();
+		else 
+                         return null; 
+		
+	}		
+				
 }
