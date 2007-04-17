02918e10036c069113a6eb533f726f5d7a1df11d
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-17 20:25:14 +0000

    lib - businnes - compras - Articulos.class.php
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@500 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/business/compras/Articulos.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class Articulos
+{
+//Almregart	
+	public static function Grabar_Articulo($articulo)
+		{			
+			$articulo->save();
+		}
+			
+	public static function salvarAlmregart($articulo)
+		{
+			self::Grabar_Articulo($articulo);			
+		}
+//
+
+}
+
