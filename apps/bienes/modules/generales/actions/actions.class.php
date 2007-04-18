a2339a016f9baebe03b269ff491886a5eaf2cf61
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-18 16:20:56 +0000

    Luelher
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@524 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/bienes/modules/generales/actions/actions.class.php
+++ apps/bienes/modules/generales/actions/actions.class.php
@@ -16,4 +16,30 @@ class generalesActions extends sfActions
   	
   	
   }
+  
+  public function executeCatalogo()
+  {
+  	
+  	$clase = $this->getRequestParameter('clase');
+  	if($clase)
+  	{
+  		$c = new Criteria();
+  		$this->registros = array();
+  		$this->campos = array();
+  		$this->columnas = array();
+  		$this->tabla = '';  		
+  		$str = '$this->registros = '.$clase.'Peer::doSelect($c);';
+  		eval($str);
+  		$str = '$this->campos = '.$clase.'Peer::getArrayFieldsNames();';
+  		eval($str);
+  		$str = '$this->tabla = ucfirst(strtolower('.$clase.'Peer::TABLE_NAME));';
+  		eval($str);
+  		$str = '$this->columnas = '.$clase.'Peer::getColumsNames();';
+  		eval($str);
+  	}
+  	else return sfView::SUCCESS;
+  	
+  	
+  } 
+  
 }
