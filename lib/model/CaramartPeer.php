2ad767e99e4d0ee703d89148481b015b03eb1931
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-18 18:51:02 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@531 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/CaramartPeer.php
+++ lib/model/CaramartPeer.php
@@ -9,6 +9,38 @@
  */ 
 class CaramartPeer extends BaseCaramartPeer
 {
+  const COLUMNS = 'columns';
+	
+	public static $columsname = array (
+	self::COLUMNS => array (CaramartPeer::RAMART => 'Código', CaramartPeer::NOMRAM => 'Descripción'),);
+	
+	
+	static public function getColumName($colum)
+	{
+		return self::$columsname[self::COLUMNS][$colum];
+	}
+
+	static public function getColumsNames()
+	{
+		return self::$columsname[self::COLUMNS];
+	}
+	
+	
+	static public function getArrayFieldsNames()
+	{
+		$col = self::$columsname[self::COLUMNS];
+		$columnas = array();
+		foreach($col as $key => $value)
+		{
+			$punto = strpos($key,'.');
+			$tabla = substr($key,0,$punto);
+			$campo = substr($key,$punto+1);
+			$columnas[] = ucfirst(strtolower($campo));
+			
+		}
+		return $columnas;
+	}
+	
 	public static function getDesramo($codram)
 	{
 		$c = new Criteria();
@@ -20,4 +52,5 @@ class CaramartPeer extends BaseCaramartPeer
 			return '<!Nombre no encontrado!>';
 		}
 	}
+
 }
