c8dfd24aab991544cc30c8cbde472935c6beb04a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-25 18:59:50 +0000

    Luelher
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@570 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/helper/PopUpHelper.php
+++ lib/helper/PopUpHelper.php
@@ -1,9 +1,10 @@
 <?php
 
-	function button_to_popup($name, $internal_uri)
+	function button_to_popup($name, $internal_uri, $class = '')
 	{
 		return button_to($name,$internal_uri,array(
 			'popup' => array('true','menubar=no,toolbar=no,scrollbars=yes,width=490,height=490,resizable=yes,left=500,top=80'),
+		    'class'  => $class,
   		));
 	}
 
