34d5c892671c24b9759baa34082e562b3d95c7f1
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-26 13:13:21 +0000

    Jesus - obras - oyctartip
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@583 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/obras/modules/oyctartip/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * oyctartip actions.
+ *
+ * @package    siga
+ * @subpackage oyctartip
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class oyctartipActions extends autooyctartipActions
+{
+}
