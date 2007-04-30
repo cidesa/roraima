5c7ea215437f3042c4a6eb8c370eff16cfc7f4d4
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-04-30 22:08:13 +0000

    Luelher
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@629 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/business/compras/Articulos.class.php
+++ lib/business/compras/Articulos.class.php
@@ -1,57 +1,99 @@
 <?php
 
+/**
+ * Clase para el Manejo de Artículos
+ *
+ * @package    Siga
+ * @subpackage lib
+ * @author     Grupo Desarrollo Cidesa <desarrollo@cidesa.com.ve>
+ * @version    SVN: $Id: $
+ * @copyright  Copyright 2007, Cidesa C.A.
+ * 
+ */
 class Articulos
 {
-//Almregart	
-	public static function Grabar_Articulo($articulo,$grid)
-	{	
-			// Se graba el Artículo		
-			$articulo->save();
-			
-			// Se graban los almacenes del articulo
-			self::Grabar_ArticulosAlmacen($articulo,$grid);
-			
-	}
+
+	/**
+	 * Función para registrar artículos
+	 *  
+	 * @static
+	 * @param $articulo Object Artículo a guardar
+	 * @param $grid Array de Objects Almacen. 
+	 * @return void 
+	 */   
+    public static function Grabar_Articulo($articulo,$grid){	
+	  	//Se graba el Artículo		
+	  $articulo->save();
 	
-	public static function Grabar_ArticulosAlmacen($articulo,$grid)
-	{
-		$codart=$articulo->getCodart();
-		$x=$grid[0];		
-			$j=0;	
-			while ($j<count($x))
-			{
-				$x[$j]->setCodart($codart);
-				$codubi=$x[$j]->getCodubi();
-				$x[$j]->setCodubi(str_pad($codubi, 20 , ' '));
-								
-				$x[$j]->save();
-			$j++;			
-			}
-			$z=$grid[1];		  
-			$j=0;
-			if ($z[$j])	
-			{
-			while ($j<count($z))
-			{											
-				$z[$j]->delete();				
-			$j++;
-			}
-			
-			}
-			
-	} 
-			
-	public static function salvarAlmregart($articulo,$grid)
-		{
-			self::Grabar_Articulo($articulo,$grid);			
+		// Se graban los almacenes del articulo
+	  self::Grabar_ArticulosAlmacen($articulo,$grid);
+    }
+
+   	/**
+	 * Función para registrar los artículos en los diferentes Alamacenes 
+	 *  
+	 * @static
+	 * @param $articulo Object Artículo a guardar
+	 * @param $grid Array de Objects Almacen. 
+	 * @return void 
+	 */   
+    public static function Grabar_ArticulosAlmacen($articulo,$grid){
+	  $codart=$articulo->getCodart();
+	  $x=$grid[0];		
+		  $j=0;	
+		  while ($j<count($x)) {
+			$x[$j]->setCodart($codart);
+			$codubi=$x[$j]->getCodubi();
+			$x[$j]->setCodubi(str_pad($codubi, 20 , ' '));
+							
+			$x[$j]->save();
+		    $j++;			
+		  }
+      $z=$grid[1];		  
+      $j=0;
+      if ($z[$j]) {
+		while ($j<count($z)) {											
+			$z[$j]->delete();				
+		$j++;
 		}
-//
-	public static function validarAlmregart($articulo)
-		{
-			return self::validarCodart($articulo);
 		
-	    }
+	  }
+    } 
+			
+	/**
+	 * Función Principal para guardar datos del formulario Almregart
+	 * TODO Esta función (y todas las demás de su clase) deben retornar un
+	 * código de error para validar cualquier problema al guardar los datos.
+	 *  
+	 * @static
+	 * @param $articulo Object Artículo a guardar
+	 * @param $grid Array de Objects Almacen. 
+	 * @return void 
+	 */
+    public static function salvarAlmregart($articulo,$grid){
+      self::Grabar_Articulo($articulo,$grid);			
+    }
+
+  
+	/**
+	 * Función Principal para validar datos del formulario Almregart
+	 *  
+	 * @static
+	 * @param $articulo Object Artículo a guardar
+	 * @param $grid Array de Objects Almacen. 
+	 * @return void 
+	 */
+    public static function validarAlmregart($articulo,$grid) {
+      return self::validarCodart($articulo);
+    }
 	    
+	/**
+	 * Función Principal para validar datos del formulario Almregart
+	 *  
+	 * @static
+	 * @param $articulo Object Artículo a guardar
+	 * @return void 
+	 */
 	public static function validarCodart($articulo)
 	{
        	$codart=$articulo->getCodart();
@@ -66,7 +108,6 @@ class Articulos
 	  	  		return 1;
 	  	  	} else return -1;	
 	  	  }else return -1;
-	  	    	
-	}
-}
+	    }
+    }
 
