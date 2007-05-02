6e64fc7868a354b052530579d753e8b349e2256a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-02 13:15:02 +0000

    Gerana, nomdefespgrulab
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@632 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/ingresos/modules/ingdatcon/actions/actions.class.php
+++ apps/ingresos/modules/ingdatcon/actions/actions.class.php
@@ -10,4 +10,64 @@
  */
 class ingdatconActions extends autoingdatconActions
 {
+	  protected function updateCiconrepFromRequest()
+  {
+    $ciconrep = $this->getRequestParameter('ciconrep');
+
+   	
+    if (isset($ciconrep['rifcon']))
+    {
+      $this->ciconrep->setRifcon($ciconrep['rifcon']);
+    }
+    if (isset($ciconrep['nomcon']))
+    {
+      $this->ciconrep->setNomcon($ciconrep['nomcon']);
+    }
+    if (isset($ciconrep['nitcon']))
+    {
+      $this->ciconrep->setNitcon($ciconrep['nitcon']);
+    }
+    if (isset($ciconrep['naccon']))
+    {
+      $this->ciconrep->setNaccon($ciconrep['naccon']);
+    }
+    if (isset($ciconrep['dircon']))
+    {
+      $this->ciconrep->setDircon($ciconrep['dircon']);
+    }
+    if (isset($ciconrep['ciucon']))
+    {
+      $this->ciconrep->setCiucon($ciconrep['ciucon']);
+    }
+    if (isset($ciconrep['cpocon']))
+    {
+      $this->ciconrep->setCpocon($ciconrep['cpocon']);
+    }
+    if (isset($ciconrep['apocon']))
+    {
+      $this->ciconrep->setApocon($ciconrep['apocon']);
+    }
+    if (isset($ciconrep['telcon']))
+    {
+      $this->ciconrep->setTelcon($ciconrep['telcon']);
+    }
+    if (isset($ciconrep['faxcon']))
+    {
+      $this->ciconrep->setFaxcon($ciconrep['faxcon']);
+    }
+    if (isset($ciconrep['emacon']))
+    {
+      $this->ciconrep->setEmacon($ciconrep['emacon']);
+    }
+    if (isset($ciconrep['urlcon']))
+    {
+      $this->ciconrep->setUrlcon($ciconrep['urlcon']);
+    }
+    if (isset($ciconrep['tipcon']))
+    {
+      $this->ciconrep->setTipcon($ciconrep['tipcon']);
+    }
+    $this->ciconrep->setRepcon('C');
+  }
+	
 }
