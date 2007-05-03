3062a5ec7ac3229ae105a1590c63962bab6d6b26
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-03 15:27:22 +0000

    Gerana, nomdefespgrulab
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@644 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/obras/modules/oycsitobr/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * oycsitobr actions.
+ *
+ * @package    siga
+ * @subpackage oycsitobr
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class oycsitobrActions extends autooycsitobrActions
+{
+}
