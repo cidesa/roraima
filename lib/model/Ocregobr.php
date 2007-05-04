a787a66a90c7079534612771b6067f8fd0e4c752
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-04 23:17:08 +0000

    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@674 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Ocregobr.php
+++ lib/model/Ocregobr.php
@@ -17,4 +17,23 @@ class Ocregobr extends BaseOcregobr
   {
   	return Herramientas::getX('CODPRO','Caprovee','Nompro',self::getCodpro());  	
   }	 
+ public function getDestipobr()
+  {
+  	return Herramientas::getX('CODTIPOBR','Octipobr','Destipobr',self::getCodtipobr());  	
+  }	 
+ public function getNompai()
+  {
+  	return Herramientas::getX('CODPAI','Ocpais','Nompai',self::getCodpai());  	
+  }	 
+ public function getNomedo()
+  {
+/*
+		return $destipact= Herramientas::getXx('Ocasiact',$filtros_tablas,$filtros_variables,'Destipact');
+  */
+		$filtros_tablas=array('CODPAI','CODEDO');//arreglo donde mando los filtros de las clases
+		$filtros_variables=array(self::getCodpai(),self::getCodedo());//arreglo donde mando los parametros de la funcion
+  
+  	//return $nomedo= Herramientas::getX('Ocestado',$filtros_tablas,$filtros_variables,'Nomedo');  	
+  }	 
+
 }
