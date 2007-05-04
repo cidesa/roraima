f0c53ec2a113f4ab5e0e0c79e8b5079253c00495
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-04 21:05:34 +0000

    Gerana, nomdefespgrulab
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@668 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/obras/modules/oycdescon/actions/actions.class.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * oycdescon actions.
+ *
+ * @package    siga
+ * @subpackage oycdescon
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class oycdesconActions extends autooycdesconActions
+{
+ 	public function executeEdit()
+   {
+    $this->ocregcon = $this->getOcregconOrCreate();
+  	$this->configGrid();
+  	
+    if ($this->getRequest()->getMethod() == sfRequest::POST)
+    {
+      $this->updateOcregconFromRequest();
+
+      $this->saveOcregcon($this->ocregcon);
+
+      $this->setFlash('notice', 'Your modifications have been saved');
+
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('oycdescon/create');
+      }
+      else if ($this->getRequestParameter('save_and_list'))
+      {
+        return $this->redirect('oycdescon/list');
+      }
+      else
+      {
+        return $this->redirect('oycdescon/edit?id='.$this->ocregcon->getId());
+      }
+    }
+    else
+    {
+      $this->labels = $this->getLabels();
+    }
+  }
+	
+	public function configGrid()
+	{
+       
+       $per = array();
+	  
+		$filas=5;
+		$cabeza="Ingenieros Residentes";
+		$eliminar=true;
+		$titulos=array("Cédula","Nombre","Nro. C.I.V.");
+		$ancho="1100";
+		$alignf=array('left','left','left');
+		$alignt=array('left','left','left');
+		$campos=array('','','');
+		$catalogos=array('','','');
+		$ajax=array('','',''); //parametro-cajitamostrar-autocompletar
+		$tipos=array('t','t','t'); //texto, monto, fecha --solo de los campos a grabar, no de todo el grid
+		$montos=array("","","");
+		$totales=array("","","");
+		$html=array('','','');
+		$js=array('','','');
+		$grabar=array("1","2","3");
+		$filatotal='';
+		 
+		
+		$this->obj=array('cabeza'=>$cabeza, 'filas'=>$filas, 'eliminar'=>$eliminar, 'titulos'=>$titulos, 
+		'ancho'=>$ancho, 'alignf'=>$alignf, 'alignt'=>$alignt, 'campos'=>$campos, 'catalogos' => $catalogos, 
+		'ajax' => $ajax, 'tipos' => $tipos, 'montos'=>$montos, 'filatotal' => $filatotal, 'totales'=>$totales, 
+		'html'=>$html, 'js'=>$js, 'datos'=>$per, 'grabar'=>$grabar, 'tabla' => 'Caartalm');
+		////////////////////// 
+	  
+	}
+	  
+}
