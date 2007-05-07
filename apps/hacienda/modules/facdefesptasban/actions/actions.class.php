2b994b5db96f65802195f409c35dc46299c05d11
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-07 19:29:46 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@685 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/hacienda/modules/facdefesptasban/actions/actions.class.php
+++ apps/hacienda/modules/facdefesptasban/actions/actions.class.php
@@ -10,4 +10,42 @@
  */
 class facdefesptasbanActions extends autofacdefesptasbanActions
 {
+
+	public function executeList()
+	{
+		$this->processSort();
+
+		$this->processFilters();
+
+		$this->filters = $this->getUser()->getAttributeHolder()->getAll('sf_admin/fctasban/filters');
+
+		// pager
+		$this->pager = new sfPropelPager('Fctasban', 10);
+		$c = new Criteria();
+   		$c->setDistinct();
+		$this->addSortCriteria($c);
+	    $this->addFiltersCriteria($c);
+	    $this->pager->setCriteria($c);
+	    $this->pager->setPage($this->getRequestParameter('page', 1));
+	    $this->pager->init();
+  }	
+	
+	protected function updateFctasbanFromRequest()
+	{
+		$mes=array(0 => 'enero', 1 => 'febrero',2 => 'marzo', 3 => 'abril', 4 => 'mayo', 5=> 'junio', 6 => 'julio', 7 => 'agosto', 8 => 'septiembre', 9 => 'octubre', 10 => 'noviembre', 11 => 'diciembre');
+		for($b=0;$b<=11;$b++)
+		{
+			$fctasban=new Fctasban();
+			$mes_actual=$mes[$b];
+			$ano = $this->getRequestParameter('fctasban');
+			$arreglo = $this->getRequestParameter($mes[$b]);
+			if (isset($ano['tasano']))
+			{
+				$fctasban->setTasano($ano['tasano']);
+				$fctasban->setTaspor($ano[$mes[$b]]);
+				$fctasban->setTasmes($b);				
+			}		
+		    $fctasban->save();
+		}   
+    }	
 }
---
+++ apps/hacienda/modules/facranpaginm/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * facranpaginm actions.
+ *
+ * @package    siga
+ * @subpackage facranpaginm
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class facranpaginmActions extends autofacranpaginmActions
+{
+}
