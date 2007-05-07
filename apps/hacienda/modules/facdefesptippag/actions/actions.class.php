3fd45f14693375bdbbc4c787c47c69efbc625fe7
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-07 21:57:07 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@700 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/hacienda/modules/facdefesptippag/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * facdefesptippag actions.
+ *
+ * @package    siga
+ * @subpackage facdefesptippag
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class facdefesptippagActions extends autofacdefesptippagActions
+{
+}
