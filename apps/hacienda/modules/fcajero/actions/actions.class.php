66ce8f264816e6ffed80f4c3a5f4cd6e3f176726
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-07 21:57:28 +0000

    desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@701 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/hacienda/modules/fcajero/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * fcajero actions.
+ *
+ * @package    siga
+ * @subpackage fcajero
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class fcajeroActions extends autofcajeroActions
+{
+}
