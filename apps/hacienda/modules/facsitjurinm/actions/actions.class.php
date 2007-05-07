a42b2f577143e4bcfa7c27869c1edf56a4b58151
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-07 16:45:12 +0000

    Luelher
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@681 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/hacienda/modules/facsitjurinm/actions/actions.class.php
+++ apps/hacienda/modules/facsitjurinm/actions/actions.class.php
@@ -10,4 +10,73 @@
  */
 class facsitjurinmActions extends autofacsitjurinmActions
 {
+  
+  public function executeEdit()
+  {
+    $this->fcsitjurinm = $this->getFcsitjurinmOrCreate();
+
+    if ($this->getRequest()->getMethod() == sfRequest::POST)
+    {
+      $this->updateFcsitjurinmFromRequest();
+
+      $this->saveFcsitjurinm($this->fcsitjurinm);
+
+      $this->setFlash('notice', 'Your modifications have been saved');
+
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('facsitjurinm/create');
+      }
+      else if ($this->getRequestParameter('save_and_list'))
+      {
+        return $this->redirect('facsitjurinm/list');
+      }
+      else
+      {
+        return $this->redirect('facsitjurinm/edit?id='.$this->fcsitjurinm->getId());
+      }
+    }
+    else
+    {
+      $this->labels = $this->getLabels();
+    }
+  }
+  
+  
+  public function handleErrorEdit()
+  {
+    $this->preExecute();
+    $this->fcsitjurinm = $this->getFcsitjurinmOrCreate();
+    $this->updateFcsitjurinmFromRequest();
+
+    $this->labels = $this->getLabels();
+
+    return sfView::SUCCESS;
+  }
+  
+  protected function saveFcsitjurinm($fcsitjurinm)
+  {
+    $fcsitjurinm->save();
+
+  }
+  
+  protected function updateFcsitjurinmFromRequest()
+  {
+    $fcsitjurinm = $this->getRequestParameter('fcsitjurinm');
+
+    if (isset($fcsitjurinm['codsitinm']))
+    {
+      $this->fcsitjurinm->setCodsitinm($fcsitjurinm['codsitinm']);
+    }
+    if (isset($fcsitjurinm['nomsitinm']))
+    {
+      $this->fcsitjurinm->setNomsitinm($fcsitjurinm['nomsitinm']);
+    }
+    if (isset($fcsitjurinm['stasitinm']))
+    {
+      $this->fcsitjurinm->setStasitinm($fcsitjurinm['stasitinm']);
+    }
+  }
+  
+  
 }
