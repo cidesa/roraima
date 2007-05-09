f26251c6da690c94cfe5d517ed2c74aad938e7eb
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-09 13:29:10 +0000

    nomsg
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@726 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Fcsollic.php
+++ lib/model/Fcsollic.php
@@ -3,10 +3,34 @@
 /**
  * Subclass for representing a row from the 'fcsollic' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Fcsollic extends BaseFcsollic
 {
+	public function getNacconrep()
+	{
+		return Herramientas::getX('rifcon','fcconrep','naccon',str_pad(self::getRifcon(), 14 , ' '));
+	}
+	public function getNomconrep()
+	{
+		return Herramientas::getX('rifcon','fcconrep','nomcon',str_pad(self::getRifcon(), 14 , ' '));
+	}
+	public function getDirconrep()
+	{
+		return Herramientas::getX('rifcon','fcconrep','dircon',str_pad(self::getRifcon(), 14 , ' '));
+	}
+	public function getRepconrep()
+	{
+		return Herramientas::getX('rifcon','fcconrep','repcon',str_pad(self::getRifcon(), 14 , ' '));
+	}
+	public function getTipconrep()
+	{
+		return Herramientas::getX('rifcon','fcconrep','tipcon',str_pad(self::getRifcon(), 14 , ' '));
+	}
+	public function getDirinm()
+	{
+		return Herramientas::getX('CodCatInm','FCRegInm','Dirinm',self::getCatcon());
+	}		
 }
