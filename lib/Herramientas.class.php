b93aa89c7679973b0b2bb1271c3d3c6e7fcb5875
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-14 22:35:37 +0000

    Luelher
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@749 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/Herramientas.class.php
+++ lib/Herramientas.class.php
@@ -389,6 +389,66 @@ class Herramientas
 	    if ($resto == 0) return 0;
 	    else return 1;
 	}  
+	
+	/**
+	 * Función similar al datediff de VB
+	 *  
+	 * @static
+	 * @param string $interval Tipo de Conparación.
+	 * @param date $dateTimeBegin Fecha Inicial.
+	 * @param date $dateTimeEnd Fecha Final.
+	 * @return string Diferencia
+	 */   	
+  public static function dateDiff($interval,$dateTimeBegin,$dateTimeEnd) {
+    //Parse about any English textual datetime
+    //$dateTimeBegin, $dateTimeEnd
+
+    $dateTimeBegin=strtotime($dateTimeBegin);
+    if($dateTimeBegin === -1) {
+      return("..begin date Invalid");
+    }
+
+    $dateTimeEnd=strtotime($dateTimeEnd);
+    if($dateTimeEnd === -1) {
+      return("..end date Invalid");
+    }
+
+    $dif=$dateTimeEnd - $dateTimeBegin;
+
+    switch($interval) {
+      case "s"://seconds
+          return($dif);
+
+      case "n"://minutes
+          return(floor($dif/60)); //60s=1m
+
+      case "h"://hours
+          return(floor($dif/3600)); //3600s=1h
+
+      case "d"://days
+          return(floor($dif/86400)); //86400s=1d
+
+      case "ww"://Week
+          return(floor($dif/604800)); //604800s=1week=1semana
+
+      case "m": //similar result "m" dateDiff Microsoft
+          $monthBegin=(date("Y",$dateTimeBegin)*12)+
+            date("n",$dateTimeBegin);
+          $monthEnd=(date("Y",$dateTimeEnd)*12)+
+            date("n",$dateTimeEnd);
+          $monthDiff=$monthEnd-$monthBegin;
+          return($monthDiff);
+
+      case "yyyy": //similar result "yyyy" dateDiff Microsoft
+          return(date("Y",$dateTimeEnd) - date("Y",$dateTimeBegin));
+
+      default:
+          return(floor($dif/86400)); //86400s=1d
+    }
+
+  }
+	
+	
 }
 
 
--- lib/model/Fcreginm.php
+++ lib/model/Fcreginm.php
@@ -9,4 +9,52 @@
  */ 
 class Fcreginm extends BaseFcreginm
 {
+  
+  public function getNomuso(){
+    return 'Nomuso';
+  }
+
+  public function getNomcarinm(){
+    return 'Nomcarinm';
+  }
+  
+  public function getNomcatfis(){
+    return 'Nomcatfis';
+  }
+
+  public function getNomsitinm(){
+    return 'Nomsitinm';
+  }
+  
+  public function getRifconant(){
+    return 'Rifconant';
+  }
+
+  public function getNomrep(){
+    return 'Nomrep';
+  }
+
+  public function getDirrep(){
+    return 'Dirrep';
+  }
+  
+  public function getNaccon(){
+    return 'Naccon';
+  }
+  
+  public function getTipcon(){
+    return 'Tipcon';
+  }
+  
+  public function getNacrep(){
+    return 'Nacrep';
+  }
+  
+  public function getTiprep(){
+    return 'Tiprep';
+  }
+  
+  
 }
+
+
