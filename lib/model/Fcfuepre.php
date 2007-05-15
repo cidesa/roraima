221eabfaebe117c70907d14de3bf13fd58e7c07a
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-15 17:47:53 +0000

    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@772 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Fcfuepre.php
+++ lib/model/Fcfuepre.php
@@ -8,5 +8,21 @@
  * @package lib.model
  */ 
 class Fcfuepre extends BaseFcfuepre
-{
+{	
+  public function getDescta()
+  {  	
+    return Herramientas::getX('codcta','contabb','descta',self::getIngrec());
+  }	
+
+  public function getDescta1()
+  {  	
+    return Herramientas::getX('codcta','contabb','descta',self::getFueing());
+  }	
+ 
+  public function getDescta2()
+  {  	
+    return Herramientas::getX('codpar','fcpreing','nompar',self::getCodprei());
+  }	
+  
+
 }
