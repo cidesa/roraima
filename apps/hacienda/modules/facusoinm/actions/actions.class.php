f4d97413f334c848509c66cb87fc2e80295e26de
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-15 18:03:57 +0000

    J.L - Hacienda facusoinm
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@775 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/hacienda/modules/facusoinm/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * facusoinm actions.
+ *
+ * @package    siga
+ * @subpackage facusoinm
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class facusoinmActions extends autofacusoinmActions
+{
+}
