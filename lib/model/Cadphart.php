2e0f548683df4fdf3515a09c8d0c06eb5cf85592
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-23 15:23:52 +0000

    Desi
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@944 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Cadphart.php
+++ lib/model/Cadphart.php
@@ -9,4 +9,23 @@
  */ 
 class Cadphart extends BaseCadphart
 {
+	public function getNomalm()
+	{
+		return Herramientas::getX('CODALM','Cadefalm','Nomalm',self::getCodalm());
+	}
+	
+	public function getDesreq()
+	{
+		return Herramientas::getX('REQART','Careqart','Desreq',self::getReqart());
+	}
+	
+	public function getFecreq()
+	{
+		return Herramientas::getX('REQART','Careqart','Fecreq',self::getReqart());
+	}
+	
+	public function getDescat()
+	{
+		return Herramientas::getX('CODCAT','Npcatpre','Descat',self::getCodori());
+	}
 }
--- lib/model/CareqartPeer.php
+++ lib/model/CareqartPeer.php
@@ -9,4 +9,36 @@
  */ 
 class CareqartPeer extends BaseCareqartPeer
 {
+	const COLUMNS = 'columns';
+	
+	public static $columsname = array (
+	self::COLUMNS => array (CareqartPeer::REQART => 'Código', CareqartPeer::DESREQ => 'Descripción', ),);
+	
+	
+	static public function getColumName($colum)
+	{
+		return self::$columsname[self::COLUMNS][$colum];
+	}
+
+	static public function getColumsNames()
+	{
+		return self::$columsname[self::COLUMNS];
+	}
+	
+	
+	static public function getArrayFieldsNames()
+	{
+		$col = self::$columsname[self::COLUMNS];
+		$columnas = array();
+		foreach($col as $key => $value)
+		{
+			$punto = strpos($key,'.');
+			$tabla = substr($key,0,$punto);
+			$campo = substr($key,$punto+1);
+			$columnas[] = ucfirst(strtolower($campo));
+			
+		}
+		return $columnas;
+	}
+	
 }
