243d88ce5cf88cc2805a1d3e58d071abe3140a8c
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-23 20:50:42 +0000

    Gerana, nomdefespgrulab
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@952 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- web/js/tools.js
+++ web/js/tools.js
@@ -260,3 +260,8 @@
 			
 		}
 	
+	String.prototype.pad = function(l, s, t){
+	      return s || (s = " "), (l -= this.length) > 0 ? (s = new Array(Math.ceil(l / s.length)
+		+ 1).join(s)).substr(0, t = !t ? l : t == 1 ? 0 : Math.ceil(l / 2))
+		+ this + s.substr(0, l - t) : this;
+         };
