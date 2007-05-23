1a83c13230208138921165a0d1bf6501630a0007
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-23 01:22:43 +0000

    J.L. lib - compras - Modificacion Compras y Creacion Proveedor
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@925 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/business/compras/Compras.class.php
+++ lib/business/compras/Compras.class.php
@@ -1,5 +1,14 @@
 <?php
-
+/**
+ * Clase para el Manejo de Compras
+ *
+ * @package    Siga
+ * @subpackage lib
+ * @author     Grupo Desarrollo Cidesa <desarrollo@cidesa.com.ve>
+ * @version    SVN: $Id: $
+ * @copyright  Copyright 2007, Cidesa C.A.
+ * 
+ */
 class Compras
 {
 
