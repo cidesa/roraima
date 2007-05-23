44aff0b2fe9b4f1a7d1d54ad5695f0277f00f5ba
desarrollo <desarrollo@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2007-05-23 21:41:35 +0000

    financiamiento jaime
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/trunk@954 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/compras/modules/almreq/actions/actions.class.php
+++ apps/compras/modules/almreq/actions/actions.class.php
@@ -35,13 +35,91 @@ class almreqActions extends autoalmreqActions
       }
       else
       {
-        return $this->redirect('almreq/edit?id='.$this->careqart->getId());
+      	return $this->redirect('almreq/edit?id='.$this->careqart->getId());
       }
     }
     else
     {
-      $this->labels = $this->getLabels();
+    	$this->labels = $this->getLabels();
     }
   }
-	
+
+  protected function updateCareqartFromRequest()
+  {
+  	$careqart = $this->getRequestParameter('careqart');
+
+  	if (isset($careqart['reqart']))
+  	{
+  		$this->careqart->setReqart($careqart['reqart']);
+  	}
+  	if (isset($careqart['fecreq']))
+  	{
+  		if ($careqart['fecreq'])
+  		{
+  			try
+  			{
+  				$dateFormat = new sfDateFormat($this->getUser()->getCulture());
+  				if (!is_array($careqart['fecreq']))
+  				{
+  					$value = $dateFormat->format($careqart['fecreq'], 'i', $dateFormat->getInputPattern('d'));
+  				}
+  				else
+  				{
+  					$value_array = $careqart['fecreq'];
+  					$value = $value_array['year'].'-'.$value_array['month'].'-'.$value_array['day'].(isset($value_array['hour']) ? ' '.$value_array['hour'].':'.$value_array['minute'].(isset($value_array['second']) ? ':'.$value_array['second'] : '') : '');
+  				}
+  				$this->careqart->setFecreq($value);
+  			}
+  			catch (sfException $e)
+  			{
+  				// not a date
+  			}
+  		}
+  		else
+  		{
+  			$this->careqart->setFecreq(null);
+  		}
+  	}
+    if (isset($careqart['codcatreq']))
+  	{
+  		$this->careqart->setCodcatreq($careqart['codcatreq']);
+    }  	
+  	if (isset($careqart['desreq']))
+  	{
+  		$this->careqart->setDesreq($careqart['desreq']);
+  	}
+  	if (isset($careqart['monreq']))
+  	{
+  		$this->careqart->setMonreq($careqart['monreq']);
+  	}
+
+    if (isset($careqart['desubi']))
+    {
+      $this->careqart->setDesubi($careqart['desubi']);
+    }
+  }
+  
+
+  public function executeAutocomplete()
+  {
+  	if ($this->getRequestParameter('ajax')=='1')
+  	{
+		 	$this->tags=Herramientas::autocompleteAjax('CODUBI','Bnubibie','Codubi',trim($this->getRequestParameter('codcatreq')));    
+  	} 	  	
+  }
+  
+  public function executeAjax()
+  {
+  	$cajtexmos=$this->getRequestParameter('cajtexmos');
+  	$cajtexcom=$this->getRequestParameter('cajtexcom');
+	  if ($this->getRequestParameter('ajax')=='1')
+	  {
+	  	$dato=BnubibiePeer::getDesubicacion($this->getRequestParameter('codigo'));
+        $output = '[["'.$cajtexmos.'","'.$dato.'",""]]';		 			 	    
+      } 	
+	    	  
+  	    $this->getResponse()->setHttpHeader("X-JSON", '('.$output.')'); 
+	    return sfView::HEADER_ONLY;
+	}	  
+  
 }
