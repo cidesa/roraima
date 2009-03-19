e0227ac9e35f31200497515e09c6e2d035ad734d
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-03-19 21:43:47 +0000

    3 decimales en elresumen de la orden de compra
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@25885 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Caresordcom.php
+++ lib/model/Caresordcom.php
@@ -9,4 +9,23 @@
  */ 
 class Caresordcom extends BaseCaresordcom
 {
+  public function getCanord($val=false)
+  {
+
+    if($val) return number_format($this->canord,3,',','.');
+    else return $this->canord;
+
+  }
+ 
+	public function setCanord($v)
+	{
+
+    if ($this->canord !== $v) {
+        $this->canord = Herramientas::toFloat($v,3);
+        $this->modifiedColumns[] = CaresordcomPeer::CANORD;
+      }
+  
+	} 
+
+
 }
