c2ce2bfabbb37228c8b1a0eed9460824dd393940
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-04-24 17:21:21 +0000

    Se actualizo cxc con respecto a desarrollo
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@27293 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Cobdestra.php
+++ lib/model/Cobdestra.php
@@ -3,10 +3,12 @@
 /**
  * Subclass for representing a row from the 'cobdestra' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Cobdestra extends BaseCobdestra
 {
+  protected $codcon="";
+  protected $monori="0,00";
 }
--- lib/model/Cobdetfor.php
+++ lib/model/Cobdetfor.php
@@ -3,10 +3,42 @@
 /**
  * Subclass for representing a row from the 'cobdetfor' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Cobdetfor extends BaseCobdetfor
 {
+  protected $genmov="";
+  protected $gening="";
+  protected $codtip="";
+  protected $destippag="";
+  protected $numide2="";
+
+  public function getDestippag(){
+     return Herramientas::getX('Id','Fatippag','Destippag',self::getFatippagId());
+  }
+
+  public function getGenmov(){
+     return Herramientas::getX('Id','Fatippag','Genmov',self::getFatippagId());
+  }
+
+  public function getGening(){
+     return Herramientas::getX('Id','Fatippag','Gening',self::getFatippagId());
+  }
+
+  public function getCodtip(){
+
+  	 $val=substr(self::getNumide(),0,4);
+
+     return $val;
+  }
+
+  public function getNumide2(){
+
+     $longitud=strlen(self::getNumide());
+  	 $val=substr(self::getNumide(),4,$longitud);
+
+     return $val;
+  }
 }
