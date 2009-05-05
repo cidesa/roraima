65d7b5b3a4a3a0938fc9f0b3a17da2f6c8e9b45f
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-05-05 23:03:24 +0000

    Ultimas adecuaciones luego del taller
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@27642 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/ciudadanos/modules/aciayudas/templates/_atbenefi.php
+++ apps/ciudadanos/modules/aciayudas/templates/_atbenefi.php
@@ -7,7 +7,7 @@
   'camposecundario'=> 'nomben',
   'campobase' => 'atbenefi',
   ), 'Atbenefi_Aciayudas', 'atciudadano', '' );
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;". "<a href=\"javascript: var w = window.open('/ciudadanos_dev.php/aciciudadano/create')\">Nuevo Paciente</a>"
+      echo "&nbsp;&nbsp;&nbsp;&nbsp;". "<a href=\"javascript: var w = window.open('/ciudadanos'+getEnv()+'.php/aciciudadano/create','','dependent=1,toolbar=0,directories=0,status=0,menubar=0,scrollbars=1,resizable=1,width=ancho,height=alto')\">Nuevo Paciente</a>"
 ?>
 
 
