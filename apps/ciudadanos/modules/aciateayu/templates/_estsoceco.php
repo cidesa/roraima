2e2e8edbb8dd45a6a28e1409d3d833521eca6b2c
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-05-06 14:49:03 +0000

    Se solventaron problemas varios en el formulario de atencio de solicitudes
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@27649 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/ciudadanos/modules/aciateayu/templates/_estsoceco.php
+++ apps/ciudadanos/modules/aciateayu/templates/_estsoceco.php
@@ -4,8 +4,8 @@
   if(SF_ENVIRONMENT=='dev') $dev = '_dev';
   else $dev = '';
 
-  if($atayudas->countAtestsocecos()==1) echo "<a href=\"javascript: var w = window.open('/ciudadanos".$dev.".php/aciestsoceco/edit/id/".$atayudas->getId()."')\">Editar Estudio Socio-Economico</a>";
-  else echo '<b>No existe Informe de Visita Domiciliaria para esta solicitud<b> <br>'."<a href=\"javascript: var w = window.open('/ciudadanos".$dev.".php/aciestsoceco/create')\">Crear Estudio Informe de Visita Domiciliaria</a>"; 
+  if($atayudas->countAtestsocecos()==1) echo "<a href=\"javascript: var w = window.open('/ciudadanos'+getEnv()+'.php/aciestsoceco/edit/id/".$atayudas->getId()."','','dependent=1,toolbar=0,directories=0,status=0,menubar=0,scrollbars=1,resizable=1,width=ancho,height=alto')\">Editar Estudio Socio-Economico</a>";
+  else echo '<b>No existe Informe de Visita Domiciliaria para esta solicitud<b> <br>'."<a href=\"javascript: var w = window.open('/ciudadanos'+getEnv()+'.php/aciestsoceco/create','','dependent=1,toolbar=0,directories=0,status=0,menubar=0,scrollbars=1,resizable=1,width=ancho,height=alto')\">Crear Estudio Informe de Visita Domiciliaria</a>"; 
 
   ?>
 
