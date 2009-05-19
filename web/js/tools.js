b2f7af56a79c7eb06c2b39e69ea0737137460b9f
jlobaton <jlobaton@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-05-19 22:08:18 +0000

    cuando el valor es de un digito aparece la palabra undefined
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@28271 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- web/js/tools.js
+++ web/js/tools.js
@@ -493,7 +493,7 @@ function enternumero(e,id)
 
         long=tira.length;
         i=1;
-        if (long > 1)
+        if (long > 0)
         {
           i=long;
           while (i>0)
@@ -1193,32 +1193,32 @@ function totalregistros(letra,posicion,filas)
     function getScriptname() {
       return window.location.toString().split('/')[3];
   	}
-    
+
     function getEnv(){
       var script = getScriptname();
       if(script.split('_')[1]=='dev.php') return '_dev';
       else return '';
     }
 
-    
+
     function getNombreArchivo(){
       var archivo = prompt('Introduzca el Nombre del Archivo a Generar');
       return archivo;
     }
-    
+
     function GuardarEstado(){
       var archivo = getNombreArchivo();
       a = $('sf_admin_edit_form').getInputs();
 
       var serializado = Form.serializeElements(a);
-      
+
       serializado = serializado+'&archivo='+archivo;
-      
+
       new Ajax.Updater('descarga', '/herramientas'+getEnv()+'.php/generales/guardarestado', {asynchronous:true, evalScripts:true, parameters:serializado});
-      
+
       return true;
     }
-	
+
 	//Carlos Ramirez funciona igual que la de php
 	function number_format( number, decimals, dec_point, thousands_sep ) {
 	    // http://kevin.vanzonneveld.net
@@ -1245,55 +1245,55 @@ function totalregistros(letra,posicion,filas)
 	    // *     returns 5: '1,000'
 	    // *     example 6: number_format(67.311, 2);
 	    // *     returns 6: '67.31'
-	 
+
 	    var n = number, prec = decimals;
 	    n = !isFinite(+n) ? 0 : +n;
 	    prec = !isFinite(+prec) ? 0 : Math.abs(prec);
 	    var sep = (typeof thousands_sep == "undefined") ? ',' : thousands_sep;
 	    var dec = (typeof dec_point == "undefined") ? '.' : dec_point;
-	 
+
 	    var s = (prec > 0) ? n.toFixed(prec) : Math.round(n).toFixed(prec); //fix for IE parseFloat(0.55).toFixed(0) = 0;
-	 
+
 	    var abs = Math.abs(n).toFixed(prec);
 	    var _, i;
-	 
+
 	    if (abs >= 1000) {
 	        _ = abs.split(/\D/);
 	        i = _[0].length % 3 || 3;
-	 
+
 	        _[0] = s.slice(0,i + (n < 0)) +
 	              _[0].slice(i).replace(/(\d{3})/g, sep+'$1');
-	 
+
 	        s = _.join(dec);
 	    } else {
 	        s = s.replace('.', dec);
 	    }
-	 
+
 	    return s;
 	}
 	//Carlos Ramirez funcion que se utiliza para actualizar los saldos totales del gridv2
 	function actualizartotales(id,idtotal)
 	{
 		if($(idtotal))
-		{			    
+		{
 			var auxid = id.split('_');
 			var nomgrid = auxid[0];
 			var fil = auxid[1];
-			var col = auxid[2]; 
+			var col = auxid[2];
 			var total = 0;
 			var val = 0;
 			var letragrid = nomgrid.substring(0,1);
 			var totfil = obtener_filas_grid(letragrid,1);
 			for(i=0;i<totfil;i++)
 			{
-				val = $(nomgrid+'_'+i+'_'+col).value;								 
+				val = $(nomgrid+'_'+i+'_'+col).value;
 			    val = val.replace(".","");
 				val = val.replace(",",".");
 				if(parseFloat(val)!=0 && parseFloat(val)>0)
-				  total = parseFloat(total) + parseFloat(val);				
-			}						
-			$(idtotal).value=number_format(total,2,',','.');						
+				  total = parseFloat(total) + parseFloat(val);
+			}
+			$(idtotal).value=number_format(total,2,',','.');
 			//alert(id+'='+total+'--'+idtotal+'---'+$(idtotal).value);
-		}		
-		
+		}
+
 	}
