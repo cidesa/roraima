3309cf0ec4a3eda7937c474dbf3f1fe30d985c6e
jlobaton <jlobaton@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-05-27 16:32:07 +0000

    Nuevo yml para configuracion de los correlativos de los comprobantes
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@28556 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/autenticacion/modules/principal/actions/actions.class.php
+++ apps/autenticacion/modules/principal/actions/actions.class.php
@@ -18,6 +18,15 @@ class principalActions extends sfActions
   {
     $this->menu=$this->getUser()->obtenerModulos();
 
+    $dir2=CIDESA_CONFIG.'/configuracion/contabilidad.yml';
+    cidesaTools::exitsfile($dir2) ? $configuracion = CIDESA_CONFIG.'/configuracion/contabilidad.yml' : $configuracion =  sfConfig::get('sf_config_dir').'/configuracion/contabilidad.yml';
+    $confyml = sfYaml::load($configuracion);
+
+	if(is_array($confyml)){
+		H::printr($confyml);
+	  if(array_key_exists('comprobantes',$confyml)) $this->getUser()->setAttribute('confcorcom', H::iif(($confyml["comprobantes"]["confcorcom"])=='S','S','N'));
+	  else $this->getUser()->setAttribute('confcorcom', 'S');
+	}
   }
 
   public function executeMenu()
@@ -26,7 +35,6 @@ class principalActions extends sfActions
     $this->modulo = $this->getRequestParameter('m','');
 
     $this->getUser()->setAttribute('menu',$this->modulo,'autenticacion');
-
 
   }
 }
