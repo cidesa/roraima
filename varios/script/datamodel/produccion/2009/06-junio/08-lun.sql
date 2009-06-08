212d0d0370447a8bc6fd449e972794f5374f94ea
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-06-08 08:22:38 +0000

    se agrego la tabla de categoria de profesores
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@28970 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- varios/script/datamodel/produccion/2009/06-junio/08-lun.sql
+++ varios/script/datamodel/produccion/2009/06-junio/08-lun.sql
@@ -44,3 +44,20 @@ alter table npasiempcont
     add column fecdes date,
     add column fechas date,
     add column status varchar(1);
+    
+--CARLOS RAMIREZ
+CREATE SEQUENCE "nptipcat_seq";
+
+CREATE TABLE nptipcat
+(
+  codtipcat character varying(4) NOT NULL,
+  destipcat character varying(100) NOT NULL,
+  id integer NOT NULL DEFAULT nextval('nptipcat_seq'::regclass)
+)
+WITH (OIDS=FALSE);
+ALTER TABLE nptipcat OWNER TO postgres;
+
+alter table npcomocp
+  alter column gracar type varchar(20),
+  alter column pascar type varchar(20);
+    
