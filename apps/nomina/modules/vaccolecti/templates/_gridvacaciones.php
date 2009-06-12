d3f2c82c013acecbf1febc1459f366fbd9f2ab44
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-06-12 18:13:43 +0000

    Forma nueva de vacaciones colectivas
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@29282 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/nomina/modules/vaccolecti/templates/_gridvacaciones.php
@@ -0,0 +1,10 @@
+<?php use_helper('Object', 'ObjectAdmin', 'I18N', 'Grid') ?>
+
+<div id='gridvacaciones'>
+<?php
+echo grid_tag_v2($npvacdisfrute->getObj()); ?>
+</div>
+
+<script language="JavaScript">
+
+</script>
