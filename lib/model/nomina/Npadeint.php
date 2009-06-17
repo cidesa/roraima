b807ec23626e9fa01e0442d3dcfd648e46f140be
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-06-17 20:25:42 +0000

    se agregaron funciones para la fomra de adelanto de intereses
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@29480 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/nomina/Npadeint.php
+++ lib/model/nomina/Npadeint.php
@@ -9,4 +9,27 @@
  */ 
 class Npadeint extends BaseNpadeint
 {
+	protected $codtipcon='';
+	protected $destipcon='';
+	protected $nomemp='';
+	protected $monantint=0;
+	
+   public function hydrate(ResultSet $rs, $startcol = 1)
+   {
+      parent::hydrate($rs, $startcol);
+      $this->codtipcon=self::getCodcon();
+   }
+   
+	public  function getDestipcon()
+	{
+		return H::getx('Codtipcon','Nptipcon','destipcon',self::getCodcon());	
+	}
+	public  function getNomemp()
+	{
+		return H::getx('Codemp','Nphojint','Nomemp',self::getCodemp());	
+	}
+	public  function getMonantint()
+	{
+		return H::formatoMonto(H::getx('Codemp','nppresoc','Intacu',self::getCodemp()));	
+	}
 }
