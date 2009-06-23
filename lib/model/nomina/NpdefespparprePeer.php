bb0f7cc13d1cae9f8c34b20873171362617476e0
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-06-23 15:26:05 +0000

    se creo la tabla npdefespparpre
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@29692 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/nomina/NpdefespparprePeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'npdefespparpre' table.
+ *
+ * 
+ *
+ * @package lib.model.nomina
+ */ 
+class NpdefespparprePeer extends BaseNpdefespparprePeer
+{
+}
