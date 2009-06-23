20cc5d3f22a0d3e2fd609539136f865409b9ed1e
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-06-23 21:03:28 +0000

    se agrego la tabla nueva nppernom
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@29725 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- varios/script/datamodel/produccion/2009/06-junio/23-mar.sql
+++ varios/script/datamodel/produccion/2009/06-junio/23-mar.sql
@@ -8,4 +8,14 @@ CREATE TABLE  npdefespparpre (
   id integer NOT NULL DEFAULT nextval('npdefespparpre_seq'::regclass)
 );alter table npdefespparpre owner to postgres;
 
+CREATE SEQUENCE nppernom_seq;
+
+CREATE TABLE  nppernom(
+   codnom varchar(3),
+   anno integer,
+   mes varchar(2),
+   fecini date,
+   fecfin date,
+   id integer NOT NULL DEFAULT nextval('nppernom_seq'::regclass)
+
 
