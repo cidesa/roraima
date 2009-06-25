527c59612691c39aa24f8fee821d3821a07ac7d5
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-06-25 16:34:07 +0000

    se ajusto el total de la liquidacion
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@29775 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/nomina/modules/presnomliquidacion/templates/ajaxSuccess.php
+++ apps/nomina/modules/presnomliquidacion/templates/ajaxSuccess.php
@@ -50,5 +50,5 @@ echo grid_tag_v2($objdeduc); ?>
   </ul>  
 <?php }?>
 <script language="JavaScript">
-	$('total_liq').value = number_format(FloatVEtoFloat($('total_vac').value) + FloatVEtoFloat($('total_asi').value) - FloatVEtoFloat($('total_ded').value),"2",",",".");		
+	$('total_liq').value = number_format(FloatVEtoFloat($('total_asi').value) - FloatVEtoFloat($('total_ded').value),"2",",",".");		
 </script>
