baa573c7cf559bacabfa8237ea24bc6f0398168a
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-06-26 23:15:00 +0000

    agregaron los campos afealibv y afealibf a la tabla npasipre, se agrego el salario integral a la forma de liquidacion
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@29906 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/nomina/modules/presnomliquidacion/templates/_salarios.php
+++ apps/nomina/modules/presnomliquidacion/templates/_salarios.php
@@ -1,56 +1,82 @@
 <fieldset>
 <legend>Salarios</legend>
 <div class="form-row">
+<table>
+	<tr>
+		<th>
+			<?php if($labels['npliquidacion_det{sue311296}']!='.:') { ?>
+			  <?php echo label_for('npliquidacion_det[sue311296]', __($labels['npliquidacion_det{sue311296}' ]), 'class="required" Style="text-align:left; width:100px"') ?>
+			  <div class=" divlado">
+			  <?php if ($sf_request->hasError('npliquidacion_det{sue311296}')): ?>
+			    <?php echo form_error('npliquidacion_det{sue311296}', array('class' => 'form-error-msg')) ?>
+			  <?php endif; }?>
+			
+			  <?php $value = object_input_tag($npliquidacion_det, 'getSue311296', array (
+			  'readonly' => true,
+			  'control_name' => 'npliquidacion_det[sue311296]',
+			)); echo $value ? $value : '&nbsp;' ?>
+			    <?php if($labels['npliquidacion_det{sue311296}']!='.:') { ?>
+			  </div>
+			  <?php  } ?>		
+		</th>
+		<th></th>
+		<th>
+			<?php if($labels['npliquidacion_det{sue180697}']!='.:') { ?>
+			  <?php echo  label_for('npliquidacion_det[sue180697]', __($labels['npliquidacion_det{sue180697}' ]), 'class="required" Style="text-align:left; width:100px"') ?>
+			  <div class=" divlado">
+			  <?php if ($sf_request->hasError('npliquidacion_det{sue180697}')): ?>
+			    <?php echo form_error('npliquidacion_det{sue180697}', array('class' => 'form-error-msg')) ?>
+			  <?php endif; }?>
+			
+			  <?php $value = object_input_tag($npliquidacion_det, 'getSue180697', array (
+			  'readonly' => true,
+			  'control_name' => 'npliquidacion_det[sue180697]',
+			)); echo $value ? $value : '&nbsp;' ?>
+			    <?php if($labels['npliquidacion_det{sue180697}']!='.:') { ?>
+			  </div>
+			  <?php  } ?>		
+		</th>
+	</tr>
+	<tr>
+		<th>&nbsp;&nbsp;</th>
+	</tr>
+	<tr>
+		<th>
+			<?php if($labels['npliquidacion_det{ultimosueldo}']!='.:') { ?>
+			  <?php echo label_for('npliquidacion_det[ultimosueldo]', __($labels['npliquidacion_det{ultimosueldo}' ]), 'class="required" Style="text-align:left; width:100px"') ?>
+			  <div class=" ">
+			  <?php if ($sf_request->hasError('npliquidacion_det{ultimosueldo}')): ?>
+			    <?php echo form_error('npliquidacion_det{ultimosueldo}', array('class' => 'form-error-msg')) ?>
+			  <?php endif; }?>
+			
+			  <?php $value = object_input_tag($npliquidacion_det, 'getUltimosueldo', array (
+			  'readonly' => true,
+			  'control_name' => 'npliquidacion_det[ultimosueldo]',
+			)); echo $value ? $value : '&nbsp;' ?>
+			    <?php if($labels['npliquidacion_det{ultimosueldo}']!='.:') { ?>
+			  </div>
+			  <?php  } ?>
 
-  <?php if($labels['npliquidacion_det{sue311296}']!='.:') { ?>
-  <?php echo label_for('npliquidacion_det[sue311296]', __($labels['npliquidacion_det{sue311296}' ]), 'class="required" Style="text-align:left; width:100px"') ?>
-  <div class=" divlado">
-  <?php if ($sf_request->hasError('npliquidacion_det{sue311296}')): ?>
-    <?php echo form_error('npliquidacion_det{sue311296}', array('class' => 'form-error-msg')) ?>
-  <?php endif; }?>
-
-  <?php $value = object_input_tag($npliquidacion_det, 'getSue311296', array (
-  'readonly' => true,
-  'control_name' => 'npliquidacion_det[sue311296]',
-)); echo $value ? $value : '&nbsp;' ?>
-    <?php if($labels['npliquidacion_det{sue311296}']!='.:') { ?>
-  </div>
-  <?php  } ?>
-
-
-
-  <?php if($labels['npliquidacion_det{sue180697}']!='.:') { ?>
-  <?php echo  label_for('npliquidacion_det[sue180697]', __($labels['npliquidacion_det{sue180697}' ]), 'class="required" Style="text-align:left; width:100px"') ?>
-  <div class=" divlado">
-  <?php if ($sf_request->hasError('npliquidacion_det{sue180697}')): ?>
-    <?php echo form_error('npliquidacion_det{sue180697}', array('class' => 'form-error-msg')) ?>
-  <?php endif; }?>
-
-  <?php $value = object_input_tag($npliquidacion_det, 'getSue180697', array (
-  'readonly' => true,
-  'control_name' => 'npliquidacion_det[sue180697]',
-)); echo $value ? $value : '&nbsp;' ?>
-    <?php if($labels['npliquidacion_det{sue180697}']!='.:') { ?>
-  </div>
-  <?php  } ?>
-
-
-
-  <?php if($labels['npliquidacion_det{ultimosueldo}']!='.:') { ?>
-  <?php echo label_for('npliquidacion_det[ultimosueldo]', __($labels['npliquidacion_det{ultimosueldo}' ]), 'class="required" Style="text-align:left; width:100px"') ?>
-  <div class=" divlado">
-  <?php if ($sf_request->hasError('npliquidacion_det{ultimosueldo}')): ?>
-    <?php echo form_error('npliquidacion_det{ultimosueldo}', array('class' => 'form-error-msg')) ?>
-  <?php endif; }?>
-
-  <?php $value = object_input_tag($npliquidacion_det, 'getUltimosueldo', array (
-  'readonly' => true,
-  'control_name' => 'npliquidacion_det[ultimosueldo]',
-)); echo $value ? $value : '&nbsp;' ?>
-    <?php if($labels['npliquidacion_det{ultimosueldo}']!='.:') { ?>
-  </div>
-  <?php  } ?>
-  <br><br>
+		</th>
+		<th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+		<th>
+			<?php if($labels['npliquidacion_det{salarioint}']!='.:') { ?>
+			  <?php echo label_for('npliquidacion_det[salarioint]', __($labels['npliquidacion_det{salarioint}' ]), 'class="required" Style="text-align:left; width:100px"') ?>
+			  <div class=" ">
+			  <?php if ($sf_request->hasError('npliquidacion_det{salarioint}')): ?>
+			    <?php echo form_error('npliquidacion_det{salarioint}', array('class' => 'form-error-msg')) ?>
+			  <?php endif; }?>
+			
+			  <?php $value = object_input_tag($npliquidacion_det, 'getSalarioint', array (
+			  'readonly' => true,
+			  'control_name' => 'npliquidacion_det[salarioint]',
+			)); echo $value ? $value : '&nbsp;' ?>
+			    <?php if($labels['npliquidacion_det{salarioint}']!='.:') { ?>
+			  </div>
+			  <?php  } ?>
+		</th>
+	</tr>
+</table> 
 </div>
 </fieldset>
 <script language="JavaScript">
