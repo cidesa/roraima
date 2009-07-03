fef505196a887b89311b85976d3a03aeb22bc9f0
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-07-03 21:26:05 +0000

    se agrego la tabla npbenefiemb para la forma de embargados, definicion de beneficiarios de embargos!
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@30191 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- varios/script/datamodel/produccion/2009/07-julio/03-vie.sql
+++ varios/script/datamodel/produccion/2009/07-julio/03-vie.sql
@@ -19,3 +19,24 @@ comment on column NPJUZGADOS.PERCONJUZ  is 'Persona Contacto del Juzgado';
 comment on column NPJUZGADOS.CARPERJUZ   is 'Cargo de la Persona Contacto del Juzgado';
 comment on column NPJUZGADOS.DIRJUZ   is 'Dirección del Juzgado';
 comment on column NPJUZGADOS.TELJUZ   is 'Teléfono del Juzgado';
+
+create SEQUENCE npbenefiemb_seq;
+
+create table NPBENEFIEMB(
+        CEDRIF    VARCHAR(16) NOT NULL,
+        NOMBEN    VARCHAR(200),
+        FECNACBEN DATE,
+        FECREG    DATE,
+        DIRBEN    VARCHAR(200),
+        TELBEN    VARCHAR(100),        
+	id integer NOT NULL DEFAULT nextval('npbenefiemb_seq'::regclass)
+        );
+ALTER TABLE npbenefiemb OWNER TO postgres;                
+  
+-- Add comments to the columns 
+comment on column NPBENEFIEMB.CEDRIF   is 'Cédula ó Rif del Beneficiario:';
+comment on column NPBENEFIEMB.NOMBEN   is 'Nombre del Beneficiario:';
+comment on column NPBENEFIEMB.FECNACBEN   is 'Fecha de Nacimiento del Beneficiario:';
+comment on column NPBENEFIEMB.FECREG   is 'Fecha de Registro del Beneficiario:';
+comment on column NPBENEFIEMB.DIRBEN   is 'Dirección de Habitación del Beneficiario:';
+comment on column NPBENEFIEMB.TELBEN   is 'Teléfono del Beneficiario:';
