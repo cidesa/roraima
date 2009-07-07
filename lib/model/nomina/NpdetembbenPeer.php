0450ca4a65323a1c2fcd3140a4b9ea8737bdd4ad
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-07-07 17:10:27 +0000

    se agregaron las tablas nuevas npbenefiemb, npdetembben, npdetembcon para la forma de embargos registro de embargos
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@30261 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/nomina/NpdetembbenPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'npdetembben' table.
+ *
+ * 
+ *
+ * @package lib.model.nomina
+ */ 
+class NpdetembbenPeer extends BaseNpdetembbenPeer
+{
+}
---
+++ lib/model/nomina/NpmaeembPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'npmaeemb' table.
+ *
+ * 
+ *
+ * @package lib.model.nomina
+ */ 
+class NpmaeembPeer extends BaseNpmaeembPeer
+{
+}
