44c7fbdb05901408cd5f3499e8f2acd0e4d5fe9f
gespinoza <gespinoza@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-07-13 18:47:10 +0000

    colocar por defecto signo positivo
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@30511 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/nomina/modules/nomperhispre/templates/_signo.php
+++ apps/nomina/modules/nomperhispre/templates/_signo.php
@@ -3,7 +3,7 @@
 
 
 <?
-	echo radiobutton_tag('nphispre[signo]','-', true, array('id'=>'nphispre_signo')) .'  '. "Negativo"."<br>";
-	echo radiobutton_tag('nphispre[signo]','+', false,array('id'=>'nphispre_signo')) .'  '. "Positivo"."<br>";
+	echo radiobutton_tag('nphispre[signo]','-', false, array('id'=>'nphispre_signo')) .'  '. "Negativo"."<br>";
+	echo radiobutton_tag('nphispre[signo]','+', true, array('id'=>'nphispre_signo')) .'  '. "Positivo"."<br>";
 ?>
 
