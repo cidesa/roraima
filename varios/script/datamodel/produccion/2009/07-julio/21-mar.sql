ce6d9b9f3ede2ceda51f6eafcf9a6acf43abcd81
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-07-21 16:12:36 +0000

    se agregaron campos a la tabla nphojint
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@30823 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ varios/script/datamodel/produccion/2009/07-julio/21-mar.sql
@@ -0,0 +1,8 @@
+--CARLOS RAMIREZ, Requerimientos para Chacao
+alter table nphojint 
+  add column feccoracu date,
+  add column capactacu numeric(14,2),
+  add column intacu numeric(14,2),
+  add column antacu numeric(14,2),
+  add column diaacu integer,
+  add column diaadiacu integer;
