271b1c0ff13525223c18f3b4a3e3fe7f70ad99ac
jlobaton <jlobaton@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-07-22 21:29:00 +0000

    script de nomina, falto colocalos
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@30893 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- varios/script/datamodel/produccion/2009/07-julio/22-mie.sql
+++ varios/script/datamodel/produccion/2009/07-julio/22-mie.sql
@@ -1,2 +1,14 @@
 alter table npestorg
   add column email varchar(40);
+
+--- // Jesus Nomina
+ALTER TABLE "npinffam"
+  ADD COLUMN "ocupac" varchar(1);
+
+--Se aumento el campo a 16
+alter table nphojint
+alter column codempant type VARCHAR(16);
+
+ALTER TABLE "nphojint"
+  ADD COLUMN "ubifis" varchar(100);
+---
