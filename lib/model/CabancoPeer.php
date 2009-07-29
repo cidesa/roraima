e7e545871f23c6af250ac852c495674029663e28
dmartinez <dmartinez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-07-29 16:28:01 +0000

    Actualizacion para el uso del Formularios de Bancos en el modulo de compra
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@31052 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/CabancoPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'cabanco' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class CabancoPeer extends BaseCabancoPeer
+{
+}
---
+++ lib/model/map/CabancoMapBuilder.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+class CabancoMapBuilder {
+
+	
+	const CLASS_NAME = 'lib.model.map.CabancoMapBuilder';
+
+	
+	private $dbMap;
+
+	
+	public function isBuilt()
+	{
+		return ($this->dbMap !== null);
+	}
+
+	
+	public function getDatabaseMap()
+	{
+		return $this->dbMap;
+	}
+
+	
+	public function doBuild()
+	{
+		$this->dbMap = Propel::getDatabaseMap('propel');
+
+		$tMap = $this->dbMap->addTable('cabanco');
+		$tMap->setPhpName('Cabanco');
+
+		$tMap->setUseIdGenerator(true);
+
+		$tMap->setPrimaryKeyMethodInfo('cabanco_SEQ');
+
+		$tMap->addColumn('CODBAN', 'Codban', 'string', CreoleTypes::VARCHAR, true, 4);
+
+		$tMap->addColumn('DESBAN', 'Desban', 'string', CreoleTypes::VARCHAR, true, 50);
+
+		$tMap->addPrimaryKey('ID', 'Id', 'int', CreoleTypes::INTEGER, true, null);
+
+	} 
+} 
