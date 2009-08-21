8f07c9ae29fd9213280b8a16dde3e55af75a21f5
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-08-21 13:17:13 +0000

    se agrego la tabla npparamsalint
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@31937 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/nomina/map/NpparamsalintMapBuilder.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+class NpparamsalintMapBuilder {
+
+	
+	const CLASS_NAME = 'lib.model.nomina.map.NpparamsalintMapBuilder';
+
+	
+	private $dbMap;
+
+	
+	public function isBuilt()
+	{
+		return ($this->dbMap !== null);
+	}
+
+	
+	public function getDatabaseMap()
+	{
+		return $this->dbMap;
+	}
+
+	
+	public function doBuild()
+	{
+		$this->dbMap = Propel::getDatabaseMap('propel');
+
+		$tMap = $this->dbMap->addTable('npparamsalint');
+		$tMap->setPhpName('Npparamsalint');
+
+		$tMap->setUseIdGenerator(true);
+
+		$tMap->setPrimaryKeyMethodInfo('npparamsalint_SEQ');
+
+		$tMap->addColumn('CODNOM', 'Codnom', 'string', CreoleTypes::VARCHAR, true, 3);
+
+		$tMap->addColumn('CODCON', 'Codcon', 'string', CreoleTypes::VARCHAR, true, 3);
+
+		$tMap->addColumn('AFECTA', 'Afecta', 'string', CreoleTypes::VARCHAR, true, 3);
+
+		$tMap->addPrimaryKey('ID', 'Id', 'int', CreoleTypes::INTEGER, true, null);
+
+	} 
+} 
