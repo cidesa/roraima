3d09379522b6e51759a733f527d3876214232c55
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-08-31 14:48:03 +0000

    Se mejoro la carga de credenciales, cuanto mas el usuario tenia privilegios mas lento era la autenticacion.
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@32288 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/sima_user/ApliUserPeer.php
+++ lib/model/sima_user/ApliUserPeer.php
@@ -9,4 +9,46 @@
  */ 
 class ApliUserPeer extends BaseApliUserPeer
 {
+  
+  public static function doSelectArray($c)
+  {
+    $rs = parent::doSelectRS($c);
+    $result = array();
+    $columns = self::getFieldNames(BasePeer::TYPE_FIELDNAME);
+    while($rs->next()) {
+      $reg = array();
+      foreach($columns as $colindex => $colname){
+        $reg[$colname] = $rs->get($colindex + 1);
+      }
+      $result[] = $reg;
+    }
+    return $result;
+  }
+  
+  private static function getColumnsForSelectSql()
+  {
+    $columns = self::getFieldNames(BasePeer::TYPE_COLNAME);
+    $strcolumns='';
+    foreach($columns as $colindex => $colname){
+      if($colindex+1 == self::NUM_COLUMNS) $strcolumns .= $colname;
+      else $strcolumns .= $colname.', ';
+    }
+    return $strcolumns;
+  }
+  
+  
+  public static function doSelectSql($sql)
+  {
+    
+    $sql = sprintf($sql,self::getColumnsForSelectSql());
+    
+    $con = Propel::getConnection(self::DATABASE_NAME);
+    $stmt = $con->createStatement();
+    $rs = $stmt->executeQuery($sql, ResultSet::FETCHMODE_NUM);
+
+    return self::populateObjects($rs);
+
+  }
+  
+  
 }
