840ff28e5c6631f00f506f4db0636891c8dd1423
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-08-31 15:17:27 +0000

    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@32298 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ varios/script/datamodel/produccion/2009/08-agosto/31-lun.sql
@@ -0,0 +1,9 @@
+--Carlos Ramirez
+alter table npantpre
+  add column fecsolant date;
+
+alter table npadeint 
+  add column fecsolade date;
+  
+alter table npnomesptipos
+  add column nomintpre varchar(1);  
