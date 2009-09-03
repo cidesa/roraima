2a50e3491bae030f3f9537f92ff7d99312426739
dmartinez <dmartinez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-09-03 15:59:53 +0000

    Actualizacion catastro
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@32568 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/catastro/modules/catlotinm/actions/actions.class.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * catlotinm actions.
+ *
+ * @package    siga
+ * @subpackage catlotinm
+ * @author     $Auhor$ <desarrollo@cidesa.com.ve>
+ * @version    SVN: $Id: actions.class.php 32323 2009-08-31 18:36:40Z lhernandez $
+ * @copyright  Copyright 2007, Cidesa C.A.
+ */
+
+class catlotinmActions extends autocatlotinmActions
+{
+  public function executeIndex()
+  {
+    return $this->forward('catlotinm', 'edit');
+  }
+
+  public function editing()
+  {
+  	$this->setVars();
+  }
+
+  public function executeAjax()
+  {
+    $codigo = $this->getRequestParameter('codigo','');
+    $ajax = $this->getRequestParameter('ajax','');
+
+    switch ($ajax){
+      case '1':
+        $output = '[["","",""],["","",""],["","",""]]';
+        break;
+      default:
+        $output = '[["","",""],["","",""],["","",""]]';
+        break;
+    }
+
+    $this->getResponse()->setHttpHeader("X-JSON", '('.$output.')');
+    return sfView::HEADER_ONLY;
+  }
+
+  public function validateEdit()
+  {
+    $this->coderr =-1;
+
+    if($this->getRequest()->getMethod() == sfRequest::POST){
+
+      if($this->coderr!=-1){
+        return false;
+      } else return true;
+
+    }else return true;
+
+
+
+  }
+
+  public function updateError()
+  {
+
+  }
+
+    public function setVars()
+  {
+  	$c = new Criteria();
+   	$reg = CatnivcatPeer::doselect($c);
+
+  	foreach ($reg as $datos)
+  	{
+  		if ($datos->getCatpar()=='Z')
+  		{
+            $this->loncc = $datos->getLonniv();
+  		}else{
+  			$this->nomabr = $this->nomabr .'-'.$datos->getNomabr();
+  		}
+  	}
+  	$this->params[0] = Herramientas::getX_vacio('catpar','catnivcat','forcodcat','Z');  //Z -> Cod.Catastral
+  	$this->params[1] = strlen(substr($this->params[0],0,strlen($this->params[0])-$this->loncc-1));
+  	$this->params[2] = substr($this->nomabr,1,strlen($this->nomabr));
+  	$this->params[3] = $this->loncc;
+  }
+
+  protected function saving($catreginm)
+  {
+  	Catastro::generarCedCas($catreginm);
+    return -1;
+
+  }
+
+}
--- lib/business/catastro/Catastro.class.php
+++ lib/business/catastro/Catastro.class.php
@@ -76,5 +76,157 @@ class Catastro {
 			 return 0;
 		}
     }
+
+    public static function BuscarNrocas($codivgeo,&$nro)
+    {
+      $t= new Criteria();
+      $t->add(CatreginmPeer::CODDIVGEO,$codivgeo);
+      $t->add(CatreginmPeer::NROCAS,$nro);
+      $regi= CatreginmPeer::doSelectOne($t);
+      if ($regi)
+      {
+      	$nro=$nro+1;
+      	self::BuscarNrocas($codivgeo,&$nro);
+      }
+      return $nro;
+    }
+
+    public static function generarCedCas($catreginm)
+    {
+    	$p= new Criteria();
+    	$p->add(CatreginmPeer::CODDIVGEO,$catreginm->getCoddivgeo());
+    	$p->add(CatreginmPeer::NROCAS,$catreginm->getNrocas());
+    	$reg= CatreginmPeer::doSelectOne($p);
+    	if ($reg)
+    	{
+    		$nro=$catreginm->getCorinicas();
+    		$i=1;
+    		while($i<=$catreginm->getCant())
+    		{
+    		  $catreginm2= new Catreginm();
+    		  $catreginm2->setCoddivgeo($reg->getCoddivgeo());
+    		  $catreginm2->setNrocas(self::BuscarNrocas($reg->getCoddivgeo(),&$nro));
+    		  $catreginm2->setFecreg(date('Y-m-d'));
+    		  $catreginm2->setNroinc($reg->getNroinc());
+    		  $catreginm2->setAsireg($reg->getAsireg());
+    		  $catreginm2->setNromat($reg->getNromat());
+    		  $catreginm2->setFolio($reg->getFolio());
+    		  $catreginm2->setCodcatant($reg->getCodcatant());
+    		  $catreginm2->setFecregant($reg->getFecregant());
+    		  $catreginm2->setNumregant($reg->getNumregant());
+    		  $catreginm2->setTriant($reg->getTriant());
+    		  $catreginm2->setProant($reg->getProant());
+    		  $catreginm2->setFolioant($reg->getFolioant());
+    		  $catreginm2->setCatbarurbId($reg->getCatbarurbId());
+    		  $catreginm2->setCattramofroId($reg->getCattramofroId());
+    		  $catreginm2->setCattramolatId($reg->getCattramolatId());
+    		  $catreginm2->setCattramolat2Id($reg->getCattramolat2Id());
+    		  $catreginm2->setEdicas($reg->getEdicas());
+    		  $catreginm2->setPisinm($reg->getPisinm());
+    		  $catreginm2->setNuminm($reg->getNuminm());
+    		  $catreginm2->setCatcodposId($reg->getCatcodposId());
+    		  $catreginm2->setUbigex($reg->getUbigex());
+    		  $catreginm2->setUbigey($reg->getUbigey());
+    		  $catreginm2->setUbigez($reg->getUbigez());
+    		  $catreginm2->setDirinm($reg->getDirinm());
+    		  $catreginm2->setLineste($reg->getLineste());
+    		  $catreginm2->setLinoes($reg->getLinoes());
+    		  $catreginm2->setLinnor($reg->getLinnor());
+    		  $catreginm2->setLinsur($reg->getLinsur());
+    		  $catreginm2->setCattipvivId($reg->getCattipvivId());
+    		  $catreginm2->setCatconinmId($reg->getCatconinmId());
+    		  $catreginm2->setCatconsocId($reg->getCatconsocId());
+    		  $catreginm2->setNumper($reg->getNumper());
+    		  $catreginm2->setNumhab($reg->getNumhab());
+    		  $catreginm2->setNumsan($reg->getNumsan());
+    		  $catreginm2->setArever($reg->getArever());
+    		  $catreginm2->setLocind($reg->getLocind());
+    		  $catreginm2->setCaptan($reg->getCaptan());
+    		  $catreginm2->setTrapis($reg->getTrapis());
+    		  $catreginm2->save();
+    		  //self::generarPersonas($reg->getId(),$catreginm2->getId());
+    		  self::generarConstruccion($reg->getId(),$catreginm2->getId());
+    		  self::generarTerreno($reg->getId(),$catreginm2->getId());
+    		  self::generarUsoEspecifico($reg->getId(),$catreginm2->getId());
+    		$i++;
+    		}
+    	}
+    }
+
+    public static function generarPersonas($idviejo,$idnuevo)
+    {
+      $p= new Criteria();
+      $p->add(CatperinmPeer::CATREGINM_ID,$idviejo);
+      $result= CatperinmPeer::doSelect($p);
+      if ($result)
+      {
+      	foreach ($result as $obj)
+      	{
+      		$catperinm= new Catperinm();
+      		$catperinm->setCatreginmId($idnuevo);
+      		$catperinm->setCatregperId($obj->getCatregperId());
+      		$catperinm->setConocu($obj->getConocu());
+      		$catperinm->setTipper($obj->getTipper());
+      		$catperinm->save();
+      	}
+      }
+    }
+
+    public static function generarConstruccion($idviejo,$idnuevo)
+    {
+      $p= new Criteria();
+      $p->add(CatcarconinmPeer::CATREGINM_ID,$idviejo);
+      $result= CatcarconinmPeer::doSelect($p);
+      if ($result)
+      {
+      	foreach ($result as $obj)
+      	{
+      		$catcarconinm= new Catcarconinm();
+      		$catcarconinm->setCatreginmId($idnuevo);
+      		$catcarconinm->setCatcarconId($obj->getCatcarconId());
+      		$catcarconinm->setCancar($obj->getCancar());
+      		$catcarconinm->setMetare($obj->getMetare());
+      		$catcarconinm->save();
+      	}
+      }
+    }
+
+    public static function generarTerreno($idviejo,$idnuevo)
+    {
+      $p= new Criteria();
+      $p->add(CatcarterinmPeer::CATREGINM_ID,$idviejo);
+      $result= CatcarterinmPeer::doSelect($p);
+      if ($result)
+      {
+      	foreach ($result as $obj)
+      	{
+      		$catcarterinm= new Catcarterinm();
+      		$catcarterinm->setCatreginmId($idnuevo);
+      		$catcarterinm->setCatcarterId($obj->getCatcarterId());
+      		$catcarterinm->setDimensiones($obj->getDimensiones());
+      		$catcarterinm->setValor($obj->getValor());
+      		$catcarterinm->save();
+      	}
+      }
+    }
+
+    public static function generarUsoEspecifico($idviejo,$idnuevo)
+    {
+      $p= new Criteria();
+      $p->add(CatusoespinmPeer::CATREGINM_ID,$idviejo);
+      $result= CatusoespinmPeer::doSelect($p);
+      if ($result)
+      {
+      	foreach ($result as $obj)
+      	{
+      		$catusoespinm= new Catusoespinm();
+      		$catusoespinm->setCatreginmId($idnuevo);
+      		$catusoespinm->setCatusoespId($obj->getCatusoespId());
+      		$catusoespinm->setTipo($obj->getTipo());
+      		$catusoespinm->setValor($obj->getValor());
+      		$catusoespinm->save();
+      	}
+      }
+    }
 }
 ?>
