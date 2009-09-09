d704631d7bc8bade6229b778b9103720ab4d010c
dmartinez <dmartinez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-09-09 21:29:49 +0000

    Errores al colocar comentarios
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@32902 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/nomina/modules/vacdiasbonovac/actions/actions.class.php
+++ apps/nomina/modules/vacdiasbonovac/actions/actions.class.php
@@ -127,13 +127,6 @@ $this->Bitacora('Guardo');
   	    $coderr = -1;
   	    $cod= $npvacjorlab->getCodnom();
 
-  	   /* $c= new Criteria();
-	    $c->add(NpvacjorlabPeer::CODNOM,$cod,Criteria::EQUAL);
-	    $per= NpvacjorlabPeer::doDelete($c);*/
-	   //$per= new Npvacjorlab();
-
-
-   //$per->setCodnom($cod);
     $l= $npvacjorlab->getLunes();
     if ($l==1)
     {
@@ -197,12 +190,8 @@ $this->Bitacora('Guardo');
     {
     	$npvacjorlab->setDomingo(null);
     }
-    //H::printr($npvacjorlab); exit;
 
     $npvacjorlab->save();
-   // $grid=Herramientas::CargarDatosGrid($this,$this->obj);
-   // $coderr = EmpleadosBanco::Grabar_grid_Vacdiasbonovac($this->getRequestParameter('npvacjorlab[codnom]'),$grid);
-
     $this->coderror=$coderr;
 
   }
@@ -235,159 +224,4 @@ $this->Bitacora('Guardo');
        }
       return sfView::SUCCESS;
   }
-  /*
-   /**
-   * Esta función permite definir la configuración del grid de datos
-   * que contiene el formulario. Esta función debe ser llamada
-   * en las acciones, create, edit y handleError para recargar en todo momento
-   * los datos del grid.
-   *
-   */
-  public function configGrid($codigo='')
-
-  {    $this->updateNpvacjorlabFromRequest();
-       $codigo= $this->npvacjorlab->getCodnom();
-       //print $codigo; exit;
-  	    $c= new Criteria();
-	    $c->add(NpvacdiasbonovacPeer::CODNOM,$codigo,Criteria::EQUAL);
-	    $per= NpvacdiasbonovacPeer::doSelect($c);
-
-
-		$opciones = new OpcionesGrid();
-		$opciones->setEliminar(true);
-		$opciones->setFilas(50);
-		$opciones->setTabla('Npvacdiasbonovac');
-		$opciones->setName('a');
-		$opciones->setAnchoGrid(50);
-		$opciones->setTitulo('Bono Vacacional');
-		$opciones->setHTMLTotalFilas(' ');
-
-
-        $col1 = new Columna('Periodo de Inicio');
-		$col1->setTipo(Columna::TEXTO);
-		$col1->setEsGrabable(true);
-		$col1->setAlineacionObjeto(Columna::CENTRO);
-		$col1->setAlineacionContenido(Columna::CENTRO);
-		$col1->setNombreCampo('perini');
-	//	$col1->setCatalogo('Nphojint','sf_admin_edit_form',array('codemp' => 1, 'nomemp' => 2, 'fecing' => 3),'Nphojint_presnomregsalint',$params);
-		$col1->setHTML('type="text" size="10"');
-		$col1->setJScript('onBlur="javascript:event.keyCode=13; ajax(event,this.id)"');
-		//$col1->setJScript('onKeyDown="javascript:return dFilter (event.keyCode, this,'.chr(39).$mascaraarticulo.chr(39).')" onKeyPress="javascript:cadena=rayaenter(event,this.value);if (event.keyCode==13 || event.keyCode==9){" onBlur="javascript:event.keyCode=13; ajaxmonto(event,this.id);"');
-
-		$col2 = new Columna('Periodo de Fin');
-		$col2->setTipo(Columna::TEXTO);
-		$col2->setEsGrabable(true);
-		$col2->setAlineacionObjeto(Columna::CENTRO);
-		$col2->setAlineacionContenido(Columna::CENTRO);
-		$col2->setNombreCampo('perfin');
-		$col2->setHTML('type="text" size="10"');
-
-		$col3 = new Columna('Rango Desde (Años de Antiguedad >=)');
-		$col3->setNombreCampo('rangodesde');
-		$col3->setTipo(Columna::MONTO);
-		$col3->setVacia(true);
-		$col3->setEsGrabable(true);
-	    $col3->setHTML('size="10"');
-
-		$params1=array("'+$('npsalint_codcon').value+'",'Val1');
-		$col4 = new Columna('Rango Hasta (Años de Antiguedad <=)');
-		$col4->setTipo(Columna::MONTO);
-		$col4->setEsGrabable(true);
-		$col4->setAlineacionObjeto(Columna::CENTRO);
-		$col4->setAlineacionContenido(Columna::CENTRO);
-		$col4->setNombreCampo('rangohasta');
-        $col4->setHTML('size="10"');
-
-        $col5 = new Columna('Dias de Bono Vacacional');
-		$col5->setTipo(Columna::MONTO);
-		$col5->setOculta(false);
-		$col5->setEsGrabable(true);
-		$col5->setAlineacionObjeto(Columna::CENTRO);
-		$col5->setAlineacionContenido(Columna::CENTRO);
-		$col5->setNombreCampo('diasbonovac');
-        $col5->setHTML('size="10"');
-
-
-		$opciones->addColumna($col1);
-		$opciones->addColumna($col2);
-		$opciones->addColumna($col3);
-		$opciones->addColumna($col4);
-		$opciones->addColumna($col5);
-
-
-		// Ee genera el arreglo de opciones necesario para generar el grid
-		$this->obj = $opciones->getConfig($per);
-
-
-
-  }
-
-
-  
-  
-  /**
-   *
-   * Función que se ejecuta luego los validadores del negocio (validators)   * Para realizar validaciones específicas del negocio del formulario
-   * Para mayor información vease http://www.symfony-project.org/book/1_0/06-Inside-the-Controller-Layer#chapter_06_validation_and_error_handling_methods
-   *
-   */
-  public function validateEdit()
-    {	$this->coderror= -1;
-
-      if($this->getRequest()->getMethod() == sfRequest::POST)
-      {
-       $this->npvacjorlab = $this->getNpvacjorlabOrCreate();
-
-       $this->updateNpvacjorlabFromRequest();
-       $this->configGrid();
-
-        $grid=Herramientas::CargarDatosGrid($this,$this->obj);
-
-
-        $this->coderror=EmpleadosBanco::Vacdiasbonovac_ValRegCompleto($grid);
-if ($this->coderror==-1){
-      $x=$grid[0];
-	  $j=0;
-	  $s=0;
-	  $a=0;
-	  $encontrado=false;
-	  //$cod= $this->npcatpre->getCodcat();
-     while ($s<count($x) and !$encontrado){
-     	$rangoini= $x[$s]->getRangodesde();
-     	$rangofin= $x[$s]->getRangohasta();
-        if ($rangoini >= $rangofin){
-
-        	$this->coderror= 414;
-        	//print $this->coderror; exit;
-             $encontrado= true;
-             break;
-
-        }
-
-	  $s++;}}
-
-
-
-        if (($this->coderror==-1))
-        {
-         //  $this->coderror=EmpleadosBanco::Validar_Vacdiasbonovac_datos($grid);
-
-	        if (($this->coderror==-1))
-	        {
-	       	$this->coderror=EmpleadosBanco::Validar_Vacdiasbonovac($grid,$this->getRequestParameter('npvacjorlab[codnom]'));
-
-	           if (($this->coderror==-1))
-	             {
-	                return true;
-	             }else return false;
-
-             }else return false;
-
-        }else return false;
-
-      }else return true;
-
-    }
-*/
-
 }
