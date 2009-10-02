b1dd91812fb26e2d768a43067dab84c1dab83229
jlobaton <jlobaton@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-10-02 14:40:45 +0000

    Arreglo Generales
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@33712 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/nomina/modules/presnomliquidacion/templates/_retiro.php
+++ apps/nomina/modules/presnomliquidacion/templates/_retiro.php
@@ -7,9 +7,9 @@
   <div class="content<?php if ($sf_request->hasError('npliquidacion_det{codret}')): ?> form-error<?php endif; ?>">
   <?php if ($sf_request->hasError('npliquidacion_det{codret}')): ?>
     <?php echo form_error('npliquidacion_det{codret}', array('class' => 'form-error-msg')) ?>
-  <?php endif; }?>  
-     
-  
+  <?php endif; }?>
+
+
   <?php echo select_tag('npliquidacion_det[codret]', options_for_select($arrret,$npliquidacion_det->getCodret()), array (
 			  'onChange'=> remote_function(array(
 			  'update'   => 'divgridvaca',
@@ -17,16 +17,16 @@
 			  'condition' => "$('npliquidacion_det_codemp').value != '' && $('id').value == '' && $('npliquidacion_det_codret').value != ''",
 			  'complete' => 'AjaxJSON(request, json)',
 			  'with' => "'ajax=1&codigo='+$('npliquidacion_det_codemp').value+'&tipret='+this.value",
-			        )),      
+			        )),
 )); ?>
-      
-		
-  <?php if($labels['npliquidacion_det{codret}']!='.:') { ?>  
-  
 
-   
+
+  <?php if($labels['npliquidacion_det{codret}']!='.:') { ?>
+
+
+
   </div>
-  <?php  } ?> 
+  <?php  } ?>
 
 
 
