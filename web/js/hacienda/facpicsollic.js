b38d69ba00ae8744ef752afa23bbc9983101b803
jlobaton <jlobaton@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-10-02 21:21:29 +0000

    eliminacion de capertas que no se utilizan
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@33742 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- web/js/hacienda/facpicsollic.js
+++ web/js/hacienda/facpicsollic.js
@@ -50,3 +50,95 @@ function Mostrar_orden_preimpresa()
       }
 }
 
+ function validargridAct(e,id)
+ {
+   if ($(id).value!="")
+   {
+		if (actividad_repetida(id))
+		{
+			alert_('La Actividad esta repetida');
+		}
+		else
+		{
+       ajaxactividad(e,id)
+	}
+   }
+ }
+
+  function actividad_repetida(id)
+  {
+   var aux = id.split("_");
+   var name=aux[0];
+   var fila=aux[1];
+   var col=parseInt(aux[2]);
+
+   var actividad=$(id).value;
+
+   var actividadrepetida=false;
+   var am=obtener_filas_grid('ax',1);
+   var i=0;
+   while (i<am)
+   {
+    var codigo="ax"+"_"+i+"_1";
+    if($(codigo))
+    {
+	    var actividad2=$(codigo).value;
+
+	    if (i!=fila)
+	    {
+	      if (actividad==actividad2)
+	      {
+	        actividadrepetida=true;
+	        break;
+	      }
+	    }
+    }
+   i++;
+   }
+   return actividadrepetida;
+ }
+
+ function ajaxactividad(e,id)
+ {
+   var aux = id.split("_");
+   var name=aux[0];
+   var fil=parseInt(aux[1]);
+   var col=parseInt(aux[2]);
+
+   var coldes=col+1;
+   var colexo1=col+3;
+   var colexo2=col+4;
+   var descripcion=name+"_"+fil+"_"+coldes;
+   var exonerable=name+"_"+fil+"_"+colexo1;
+   var exonerada=name+"_"+fil+"_"+colexo2;
+   var cod=$(id).value;
+
+   if (e.keyCode==13 || e.keyCode==9)
+   {
+    if ($(id).value!="")
+    {
+     new Ajax.Request(getUrlModuloAjax(), {asynchronous:true, evalScripts:false, onComplete:function(request, json){AjaxJSON(request, json)}, parameters:'ajax=6&cajtexmos='+descripcion+'&cajtexcom='+id+'&exo='+exonerable+'&exonerada='+exonerada+'&codigo='+cod})
+    }
+   }
+ }
+
+
+
+ function exonerado(id)
+ {
+   var aux = id.split("_");
+   var name=aux[0];
+   var fil=parseInt(aux[1]);
+   var col=parseInt(aux[2]);
+
+   var porexo=name+"_"+fil+"_"+6;
+
+ 	if ($F(id)=="S")
+ 	{
+		$(porexo).disabled=false;
+
+ 	}else{
+ 		$(porexo).disabled=true;
+ 	}
+ }
+
