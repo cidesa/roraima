573d7ebcace74e93d13651b438735be56aabdd56
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-11-17 13:39:31 +0000

    SISTEMA DE EVALUACION Y DESEMPEÑO - CARGA COMPLETA
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@34792 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ varios/script/datamodel/produccion/2009/11-noviembre/16-lun.sql
@@ -0,0 +1,67 @@
+--CARLOS RAMIREZ
+--EVALUACION Y DESEMPEÑO
+create SEQUENCE rhprocur_seq;
+
+create table rhprocur
+(
+  "codcur" varchar(10) not null,
+  "cedpro" varchar(10) not null,
+  "nompro" varchar(100),
+  "id" INTEGER  NOT NULL DEFAULT nextval('rhprocur_seq'::regclass));
+  
+--CARLOS RAMIREZ
+--EVALUACION Y DESEMPEÑO
+alter table rhclacur
+  drop column horini,
+  drop column horfin;
+
+alter table rhclacur
+  add column horini varchar(10),
+  add column horfin varchar(10);
+  
+--CARLOS RAMIREZ EVALUACION Y DESEMPEÑO
+alter table rhinscur
+  add column tipper varchar(1);
+  
+--CARLSO RAMIREZ #EVALUACION Y DESEMPEÑO
+CREATE SEQUENCE "rhdefran_seq";
+
+CREATE TABLE "rhdefran"
+(
+  "codran" VARCHAR(2)  NOT NULL,
+  "desran" VARCHAR(100)  NOT NULL,
+  "id" INTEGER  NOT NULL DEFAULT nextval('rhdefran_seq'::regclass)
+);
+
+COMMENT ON TABLE "rhdefran" IS 'Tabla definicion de Rangos';
+
+CREATE SEQUENCE "rhdefdetran_seq";
+
+CREATE TABLE "rhdefdetran"
+(
+  "codran" VARCHAR(2)  NOT NULL,
+  "valran" NUMERIC(14,2)  NOT NULL,
+  "desdet" VARCHAR(60)  NOT NULL,
+  "valdes" NUMERIC(14,2)  NOT NULL,
+  "valhas" NUMERIC(14,2)  NOT NULL,
+  "id" INTEGER  NOT NULL DEFAULT nextval('rhdefdetran_seq'::regclass)
+);
+
+COMMENT ON TABLE "rhdefdetran" IS 'Tabla detalle de Rangos';
+
+alter table "rhdefniv" 
+  add column "minpun" integer  NOT NULL,
+  add column "maxpun" integer  NOT NULL,
+  add column "totpes" integer  NOT NULL,
+  add column "codran" VARCHAR(2)  NOT NULL,
+  add column "tipcal" VARCHAR(1)  NOT NULL  ;
+  
+alter table rhevaconcom
+  add column feceval date;  
+  
+alter table rhevaempobj
+  add column feceval date
+  add column pesobj NUMERIC(14,2)  NOT NULL,
+  add column punobj NUMERIC(14,2)  NOT NULL;
+  
+  
