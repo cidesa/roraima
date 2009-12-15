da0a789f9a4e5caa26578e5722dc07b76f50cc67
dmartinez <dmartinez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-12-15 19:16:32 +0000

    script correcto de apernueper
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@35614 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- varios/script/datamodel/produccion/2009/12-diciembre/11-vie.sql
+++ varios/script/datamodel/produccion/2009/12-diciembre/11-vie.sql
@@ -13,7 +13,8 @@ CREATE SEQUENCE "apernueper_seq";
 CREATE TABLE "apernueper"
 (
   "nomtab" VARCHAR(255)  NOT NULL,
-  "ordeli" INTEGER  NOT NULL,
+  "orden" INTEGER  NOT NULL,
+  "codmod" VARCHAR(10),
   "id" INTEGER  NOT NULL DEFAULT nextval('apernueper_seq'::regclass),
   PRIMARY KEY ("id")
 );
