9955c800a7c29669608eb34dadd762a6a0851fd4
jlobaton <jlobaton@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-12-16 16:33:23 +0000

    Clase que faltaba en productions
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@35653 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ lib/model/Foringdisfuefin.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'foringdisfuefin'.
+ *
+ *
+ *
+ * @package    Roraima
+ * @subpackage lib.model
+ * @author     $Author: lhernandez $ <desarrollo@cidesa.com.ve>
+ * @version SVN: $Id: Foringdisfuefin.php 32428 2009-09-02 04:18:52Z lhernandez $
+ * 
+ * @copyright  Copyright 2007, Cide S.A.
+ * @license    http://opensource.org/licenses/gpl-2.0.php GPLv2
+ */
+class Foringdisfuefin extends BaseForingdisfuefin
+{
+
+ public function getNomext()
+  {
+  return Herramientas::getX('codfin','Fortipfin','Nomext',self::getCodfin());
+  }
+
+}
