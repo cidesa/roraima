d9b3d22edc832dfece844e116047791924849f2d
jlobaton <jlobaton@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-12-16 13:33:13 +0000

    Se identifico mejor la advertencia
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@35641 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/autenticacion/modules/cambioperiodo/templates/_info.php
+++ apps/autenticacion/modules/cambioperiodo/templates/_info.php
@@ -3,19 +3,26 @@
     <td width="10%" rowspan="3" valign="top">
        <div align="left">
        <p></p>
-        <p><img src="/images/alerta.png"  ></p>
+        <p><img src="/images/alerta.png" alt="Alerta"></p>
         <p></p>
        </div>
     </td>
-    <td width="90%" height="39">
+    <th width="90%" height="39">
      <fieldset id="sf_fieldset_none" class="">
-      <div class="form-row">
-        <p><font size="3" face="MS sans-serif"><?php echo __('.-El Usuario Destino no debe Existir') ?></font></p>
-        <p><font size="3" face="MS sans-serif"><?php echo __('.-Se generará un resplado del Usuario Origen') ?></font><font face="Arial, Helvetica, sans-serif"></font></p>
-        <p><font size="3" face="MS sans-serif"><?php echo __('.-Se Creará Usuario Destino según las especificaciones dadas') ?></font><font face="Arial, Helvetica, sans-serif"></font></p>
-        <p><font size="3" face="MS sans-serif"><?php echo __('.-Se realizarán  las actualizaciones sobre el Usuario Destino') ?></font><font face="Arial, Helvetica, sans-serif"></font></p>
+      <div class="form-error">
+	  <h2 align="center"><?php  echo __('::: A d v e r t e n c i a :::'); ?></h2>
+		<dl>
+			<dt align="left">
+		             <font size="3" face="MS sans-serif"><?php echo __('.-El Usuario Destino no debe Existir') ?></font>
+	        	<br><font size="3" face="MS sans-serif"><?php echo __('.-Se generará un resplado del Usuario Origen') ?></font><font face="Arial, Helvetica, sans-serif"></font>
+	        	<br><font size="3" face="MS sans-serif"><?php echo __('.-Se Creará Usuario Destino según las especificaciones dadas') ?></font><font face="Arial, Helvetica, sans-serif"></font>
+	        	<br><font size="3" face="MS sans-serif"><?php echo __('.-Se realizarán  las actualizaciones sobre el Usuario Destino') ?></font><font face="Arial, Helvetica, sans-serif"></font>
+
+			</dt>
+		</dl>
+
       </div>
      </fieldset>
-    </td>
+    </th>
   </tr>
 </table>
