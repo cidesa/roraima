ff2ff2d308b746c7cd21d664352799f5d35d3243
jlobaton <jlobaton@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2009-12-28 16:14:50 +0000

    error visual
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@35750 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/autenticacion/modules/cambioperiodo/templates/_preegr.php
+++ apps/autenticacion/modules/cambioperiodo/templates/_preegr.php
@@ -1,10 +1,11 @@
 <?php
-/* 
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
 ?>
+<br>
   <?php $value = object_checkbox_tag($empresa, 'getPreegr', array (
   'control_name' => 'empresa[preegr]',
 )); echo $value ? $value : '&nbsp;' ?>
