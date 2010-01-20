d554b6b3713e8139df499ea06384dc0deb144513
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-01-20 22:36:05 +0000

    Se solventaron detalles varios del ciudadanos, por revision de oro negro
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@36027 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/ciudadanos/modules/aciayudas/templates/_atrubros_id.php
+++ apps/ciudadanos/modules/aciayudas/templates/_atrubros_id.php
@@ -11,4 +11,5 @@
 <?php echo select_tag('atayudas[atrubros_id]', options_for_select($atayudas->getRubros(),$atayudas->getAtrubrosId()),array(
 'onchange' => "toAjaxUpdater('divrecaudos',1,getUrlModulo()+'ajax',this.value,'','')",
 )); ?>
+<?php echo "&nbsp;&nbsp;&nbsp;&nbsp;"."<a href=\"javascript: var w = window.open('/ciudadanos'+getEnv()+'.php/acirubros/create','','dependent=1,toolbar=0,directories=0,status=0,menubar=0,scrollbars=1,resizable=1,width=ancho,height=alto')\">Nuevo Rubro</a>" ?>
 </div>
