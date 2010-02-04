7108fec67e218211ed81cd3d2ef9dcd06938490a
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-02-04 00:22:26 +0000

    se elimino el del where la condicion fija de pascar='001' y se coloco de acuerdo a lo que trae la la tabla npasicaremp
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@36282 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/business/nomina/Nomina.class.php
+++ lib/business/nomina/Nomina.class.php
@@ -1889,9 +1889,9 @@ class Nomina {
 		  }else
 		  {
 	  		if ($token = "STAB") {
-	            $sql = "Select A.* from NPCOMOCP A,NPCARGOS B WHERE B.CODCAR='" . $tabla[0]["codcar"] . "' AND A.CODTIPCAR=B.CODTIP AND A.GRACAR=B.GRAOCP AND A.PASCAR='001' AND FECDES<=TO_DATE('" . $movconvar . "','DD/MM/YYYY') ORDER BY FECDES DESC";
+	            $sql = "Select A.* from NPCOMOCP A,NPCARGOS B WHERE B.CODCAR='" . $tabla[0]["codcar"] . "' AND A.CODTIPCAR=B.CODTIP AND A.GRACAR=B.GRAOCP AND A.PASCAR=coalesce('".$tabla[0]["paso"]."','001') AND FECDES<=TO_DATE('" . $movconvar . "','DD/MM/YYYY') ORDER BY FECDES DESC";
 	          } else {
-	            $sql = "Select ABS(SUM(case when a.pascar='001' then a.suecar else a.suecar*-1)) as suecar from NPCOMOCP A,NPCARGOS B WHERE B.CODCAR='" . $tabla[0]["codcar"] . "' AND A.CODTIPCAR=B.CODTIP AND A.GRACAR=B.GRAOCP AND (A.PASCAR='001' OR A.PASCAR='" . $tabla[0]["paso"] . "') AND FECDES<=TO_DATE('" . $movconvar . "','DD/MM/YYYY') ORDER BY FECDES DESC";
+	            $sql = "Select ABS(SUM(case when a.pascar=coalesce('".$tabla[0]["paso"]."','001') then a.suecar else a.suecar*-1)) as suecar from NPCOMOCP A,NPCARGOS B WHERE B.CODCAR='" . $tabla[0]["codcar"] . "' AND A.CODTIPCAR=B.CODTIP AND A.GRACAR=B.GRAOCP AND (A.PASCAR='001' OR A.PASCAR='" . $tabla[0]["paso"] . "') AND FECDES<=TO_DATE('" . $movconvar . "','DD/MM/YYYY') ORDER BY FECDES DESC";
 	          }
 	          if (Herramientas :: BuscarDatos($sql, & $tablaescala)) {
 	            $valor = $tablaescala[0]["suecar"];
