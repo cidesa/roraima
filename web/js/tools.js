3a9b653ceffbc922a444c8f8d57556a2a6509d7f
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-02-11 15:43:54 +0000

    Se cargo funcion para calcular la edad de una persona dad una fecha
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@36464 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- web/js/tools.js
+++ web/js/tools.js
@@ -884,13 +884,13 @@ function totalregistros(letra,posicion,filas)
         if ((IsNumeric(ano)==true)  && (ano<10000)  && (long==4))
           validacion_fecha=true;
         else
-          alert('A�o mal escrito');
+          alert('Año mal escrito');
       }
       else
         alert('Mes mal escrito');
     }
     else
-      alert('D�a mal escrito');
+      alert('Día mal escrito');
   }
   return validacion_fecha;
   }
@@ -1324,3 +1324,55 @@ function totalregistros2(letra,posicion,filas)
 function convertirMayusculas(id) {
     $(id).value=$(id).value.toUpperCase();
 }
+
+
+  function calcular_edad(fecha){
+
+      //calculo la fecha de hoy
+      hoy=new Date()
+      //alert(hoy)
+
+      //calculo la fecha que recibo
+      //La descompongo en un array
+      var array_fecha = fecha.split("/")
+      //si el array no tiene tres partes, la fecha es incorrecta
+      if (array_fecha.length!=3)
+         return false
+
+      //compruebo que los ano, mes, dia son correctos
+      var ano
+      ano = parseInt(array_fecha[2]);
+      if (isNaN(ano))
+         return false
+
+      var mes
+      mes = parseInt(array_fecha[1]);
+      if (isNaN(mes))
+         return false
+
+      var dia
+      dia = parseInt(array_fecha[0]);
+      if (isNaN(dia))
+         return false
+
+
+      //si el año de la fecha que recibo solo tiene 2 cifras hay que cambiarlo a 4
+      if (ano<=99)
+         ano +=1900
+
+      //resto los años de las dos fechas
+      edad=(1900+hoy.getYear())- ano - 1; //-1 porque no se si ha cumplido años ya este año
+
+      //si resto los meses y me da menor que 0 entonces no ha cumplido años. Si da mayor si ha cumplido
+      if (hoy.getMonth() + 1 - mes < 0) //+ 1 porque los meses empiezan en 0
+         return edad
+      if (hoy.getMonth() + 1 - mes > 0)
+         return edad+1
+
+      //entonces es que eran iguales. miro los dias
+      //si resto los dias y me da menor que 0 entonces no ha cumplido años. Si da mayor o igual si ha cumplido
+      if (hoy.getUTCDate() - dia >= 0)
+         return edad + 1
+
+      return edad
+  }
