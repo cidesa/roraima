1b75e513852a6eb62ca93b81f458da18c3f118a2
dmartinez <dmartinez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-03-12 16:24:41 +0000

    Actualización. Ajuste solicitados por suvinca. Psilva y vescalona
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@37076 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/business/compras/Orden_compra.class.php
+++ lib/business/compras/Orden_compra.class.php
@@ -37,8 +37,9 @@ class Orden_compra
 
 	$refere1 = Herramientas::getX_vacio('refere','cpimppag','refere',$caordcom->getOrdcom());
 	$refere2 = Herramientas::getX_vacio('refere','cpimpcau','refere',$caordcom->getOrdcom());
+	$refere0 = Herramientas::getX_vacio('refcom','cpimpcom','refcom',$caordcom->getOrdcom());
 
-	if (!empty($refere1) or (!empty($refere2)) and ($caordcom->getId()))
+	if (!empty($refere1) or (!empty($refere2) or (!empty($refere0))) and ($caordcom->getId()))
 	{
 		$coderror=109;
 		return false;
