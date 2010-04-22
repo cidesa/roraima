c451e74e61c20b4bf28b5e961bb022285787d228
dmartinez <dmartinez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-04-22 20:29:33 +0000

    ajuste el formulario
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@37730 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/business/compras/Compras.class.php
+++ lib/business/compras/Compras.class.php
@@ -7,7 +7,7 @@
  * @subpackage compras
  * @author     $Author$ <desarrollo@cidesa.com.ve>
  * @version SVN: $Id$
- * 
+ *
  * @copyright  Copyright 2007, Cide S.A.
  * @license    http://opensource.org/licenses/gpl-2.0.php GPLv2
  */
@@ -2697,6 +2697,7 @@ class Compras {
           $gridnuevorec[$indice2 -1][6] = $resultado->getCodcat();
         }
       }
+      $nopuedeaumentar=true;
       $z = 0;
       while ($z < count($gridnuevo)) {
         if ($gridnuevo[$z][2] != "") {
