07911db393c2e946075c8f028d00d8d26efc8eba
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-05-11 18:16:28 +0000

    se ajusto el formulario de reportes para que cuadrara con el reporte de conceptos
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@38078 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/nomina/modules/nomdefrepcon/templates/_grid.php
+++ apps/nomina/modules/nomdefrepcon/templates/_grid.php
@@ -4,3 +4,27 @@
 <?php echo grid_tag_v2($npdefrepcon->getGrid()); ?>
 
 </div>
+
+<script>
+function cambiardescripcion(id)
+{
+    var auxid = id.split("_");
+    var name = auxid[0];
+    var fil = auxid[1];
+    var col = auxid[2];
+    var colant = parseInt(auxid[2])-1;
+    var idcolant = name+'_'+fil+'_'+colant;
+    var val ='';
+    if($(idcolant))
+        val = $(idcolant).value;
+    var t = obtener_filas_grid('a', '1')
+    for(i=0;i<t;i++)
+    {
+        if(val == $('ax_'+i+'_1').value)
+        {
+            $('ax_'+i+'_2').value=$(id).value;
+        }
+    }
+
+}
+</script>
