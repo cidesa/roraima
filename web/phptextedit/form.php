334541e6680c134ea268e05a180e661dec679708
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-05-27 16:07:33 +0000

    Nueva funcionalidad para hacer update desde el panel de control.
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@38430 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ web/phptextedit/form.php
@@ -0,0 +1,9 @@
+<form action="processscript.php" method="post">
+<textarea rows="25" cols="40" name="content">
+<?
+$fn = "/home/lhernandez/fuentes/cidesa/prod/config/databases.yml";
+print htmlspecialchars(implode("",file($fn)));
+?> 
+</textarea><br>
+<input type="submit" value="Change!"> 
+</form>
