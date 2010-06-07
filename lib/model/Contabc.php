6d9327d1892175271cab4d4e79f078295ba17c2f
dmartinez <dmartinez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-06-07 19:53:21 +0000

    para validar clase
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@38653 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Contabc.php
+++ lib/model/Contabc.php
@@ -9,4 +9,88 @@
  */ 
 class Contabc extends BaseContabc
 {
+    protected $check="";
+	protected $stapin="";
+	protected $totdeb=0.00;
+	protected $totcre=0.00;
+        protected $totdif="0,00";
+        protected $obj=array();
+        protected $bloqfec="";
+        protected $numfilas=50;
+
+    public function getStapin(){
+    	switch ($this->getStacom()){
+    		case "A":
+    			return "ACTUALIZADO";
+    			break;
+    		case "D":
+    			return "DIFERIDO";
+    		break;
+    		case "E":
+    			return "DESCUADRADO";
+    	    break;
+    		case "N":
+    			return "ANULADO";
+    		break;
+    		case "R":
+    			return "REVERSADO";
+    		break;
+    	}
+    }
+
+    public function getFeccom2($format = 'd/m/Y') {
+    	return parent::getFeccom($format);
+    }
+
+/*    public function getTotdeb() {
+    	return 0;
+    }
+
+    public function getTotcre() {
+    	return 0;
+    }*/
+
+  public function getBloqfec()
+  {
+    $dato="";
+    $varemp = sfContext::getInstance()->getUser()->getAttribute('configemp');
+    if ($varemp)
+	if(array_key_exists('aplicacion',$varemp))
+	 if(array_key_exists('contabilidad',$varemp['aplicacion']))
+	   if(array_key_exists('modulos',$varemp['aplicacion']['contabilidad']))
+	     if(array_key_exists('confincom',$varemp['aplicacion']['contabilidad']['modulos'])){
+	       if(array_key_exists('bloqfec',$varemp['aplicacion']['contabilidad']['modulos']['confincom']))
+	       {
+	       	$dato=$varemp['aplicacion']['contabilidad']['modulos']['confincom']['bloqfec'];
+	       }
+         }
+     return $dato;
+  }
+
+  public function setBloqfec()
+  {
+  	return $this->bloqfec;
+  }
+
+  public function getNumfilas()
+  {
+    $dato=50;
+    $varemp = sfContext::getInstance()->getUser()->getAttribute('configemp');
+    if ($varemp)
+	if(array_key_exists('aplicacion',$varemp))
+	 if(array_key_exists('tesoreria',$varemp['aplicacion']))
+	   if(array_key_exists('modulos',$varemp['aplicacion']['tesoreria']))
+	     if(array_key_exists('confincomgen',$varemp['aplicacion']['tesoreria']['modulos'])){
+	       if(array_key_exists('numfilas',$varemp['aplicacion']['tesoreria']['modulos']['confincomgen']))
+	       {
+	       	$dato=$varemp['aplicacion']['tesoreria']['modulos']['confincomgen']['numfilas'];
+	       }
+         }
+     return $dato;
+  }
+
+  public function setNumfilas()
+  {
+  	return $this->numfilas;
+  }
 }
