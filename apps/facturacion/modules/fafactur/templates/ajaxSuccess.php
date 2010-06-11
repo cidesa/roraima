b740510193003ab75c3d96d85a85654fcb12b236
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-06-11 19:22:32 +0000

    requerimiento sistema de facturacion logicasa
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@38784 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/facturacion/modules/fafactur/templates/ajaxSuccess.php
+++ apps/facturacion/modules/fafactur/templates/ajaxSuccess.php
@@ -8,6 +8,10 @@ echo options_for_select($precios,'');
 <?php $value = get_partial('grid_fapeddes', array('fafactur' => $fafactur)); echo $value ? $value : '&nbsp;'; ?>
 <?php } else if ($ajaxs=='16') { ?>
 <?php $value = get_partial('grid_faartfac', array('type' => 'edit', 'fafactur' => $fafactur)); echo $value ? $value : '&nbsp;' ?>
+<?php echo object_input_hidden_tag($fafactur, 'getTotcanpreart', array('id' => 'fafactur_totcanpreart', 'name' => 'fafactur[totcanpreart]')) ?>
+  <?php echo object_input_hidden_tag($fafactur, 'getTottotart', array('id' => 'fafactur_tottotart', 'name' => 'fafactur[tottotart]')) ?>
+  <?php echo object_input_hidden_tag($fafactur, 'getTotmonrgo', array('id' => 'fafactur_totmonrgo', 'name' => 'fafactur[totmonrgo]')) ?>
+  <?php echo object_input_hidden_tag($fafactur, 'getTotprecio', array('id' => 'fafactur_totprecio', 'name' => 'fafactur[totprecio]')) ?>
 <?php } else{ ?>
 <?php echo select_tag('fafactur[listaart]', options_for_select($arreglo,''),array(
   'onfocus' => "colocarengrid(this.value,'$filagrid');",
