1e0c332778981be27d1287ca4c0d230d8b88ac7d
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-07-27 17:39:28 +0000

    Formulario de Niveles Presupuestarios en Formulacion
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@39733 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/Forpereje.php
+++ lib/model/Forpereje.php
@@ -17,26 +17,37 @@ class Forpereje extends BaseForpereje
 {
 	private $monpar = '';
 	private $porcen = '';
-	
+        protected $fecdes2;
+	protected $fechas2;
+
+
 	public function setMonpar($val){
-		
+
 		$this->monpar = $val;
 	}
-	
+
 	public function getMonpar(){
-		
+
 		return $this->monpar;
 	}
-	
+
 	public function setPorcen($val){
-		
+
 		$this->porcen = $val;
 	}
-	
+
 	public function getPorcen($val=true){
-		
+
 		if($val) return number_format($this->porcen,2,',','.');
-		else return $this->porcen;	
+		else return $this->porcen;
+	}
+
+	public function getFecdes2($format = 'd/m/Y') {
+		return parent::getFecdes($format);
+	}
+
+	public function getFechas2($format = 'd/m/Y') {
+		return parent::getFechas($format);
 	}
 	
 	
