229f09ffdce07742b550b5960e9cb8dc0da48998
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-08-19 16:27:14 +0000

    Modificaciones pasadas a produccion del modulo de documentos para monagas
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@40307 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/helper/GridHelper.php
+++ lib/helper/GridHelper.php
@@ -1123,7 +1123,7 @@ function grid_tag_v2($obj,$objelim = array())
              break;
            case 'c':  //Combo
              $tagw = '     <td class="grid_fila g_'.$name.'_c '.$name.'c'.$j.'" >';
-             $tagw .= select_tag($name.'x_'.$i.'_'.$jmasuno, options_for_select( (is_array($combo[$j]) && $combo[$j]!='') ? $combo[$j] : eval($combo[$j]) ,$get,'include_custom=Seleccione...'), 'name="grid'.$name.'['.$i.']['.$j.']" style="border:none" onBlur=" '.$blur.' '.$js[$j].' " '.$taghtml.' '.$js[$j].' '.$blur);
+             $tagw .= select_tag($name.'x_'.$i.'_'.$jmasuno, options_for_select( (is_array($combo[$j]) && $combo[$j]!='') ? $combo[$j] : call_user_func(array(ucfirst($tabla).'Peer',$combo[$j])) ,$get,'include_custom=Seleccione...'), 'name="grid'.$name.'['.$i.']['.$j.']" style="border:none" onBlur=" '.$blur.' '.$js[$j].' " '.$taghtml.' '.$js[$j].' '.$blur);
              $tagw .= $catobj.$btnobj.$tagId.'</td>';
              break;
            case 'k':   //check
