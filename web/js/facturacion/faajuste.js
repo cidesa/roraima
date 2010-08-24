f590d315ab9c6abfee167fb36a006eb6813b4029
dmartinez <dmartinez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-08-24 20:13:06 +0000

    Modificaciones al formulario de ajustes
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@40376 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- web/js/facturacion/faajuste.js
+++ web/js/facturacion/faajuste.js
@@ -1,4 +1,15 @@
- function cantidadaju(id)
+/**
+ * Librerías Javascript
+ *
+ * @package    Roraima
+ * @author     $Author$ <desarrollo@cidesa.com.ve>
+ * @version SVN: $Id$
+ *
+ * @copyright  Copyright 2007, Cide S.A.
+ * @license    http://opensource.org/licenses/gpl-2.0.php GPLv2
+ */
+
+ function cantidadaju(e,id)
  {
    if (e.keyCode==13 || e.keyCode==9)
    {
