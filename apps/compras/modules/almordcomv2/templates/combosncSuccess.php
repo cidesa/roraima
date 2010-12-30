7f4ad990b78cf7cec24622af1a1341ed3c4bd1f2
lhernandez <lhernandez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2010-12-30 20:16:28 +0000

    almordcomv2
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@41865 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

---
+++ apps/compras/modules/almordcomv2/templates/combosncSuccess.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Funciones de la vista.
+ *
+ * @package    Roraima
+ * @subpackage vistas
+ * @author     $Author: lhernandez $ <desarrollo@cidesa.com.ve>
+ * @version    SVN: $Id: combosncSuccess.php 32813 2009-09-08 16:19:47Z lhernandez $
+ */
+// date: 2007/04/09 17:27:37
+?>
+<?php use_helper('Object', 'Validation', 'Javascript') ?>
+<?php if ($tipo=='P')
+{
+ echo select_tag('caordcom[codedo]', options_for_select($estados,'','include_custom=Seleccione'),array('onChange'=> remote_function(array(
+ 'update'   => 'divMunicipios',
+ 'url'      => 'almordcomv2/combosnc?par=2',
+ 'with' => "'pais='+document.getElementById('caordcom_codpai').value+'&estado='+this.value"
+  ))));
+}
+else if ($tipo=='E')
+{
+	echo select_tag('caordcom[codmun]', options_for_select($municipio,'','include_custom=Seleccione'),array('onChange'=> remote_function(array(
+	'update'   => 'divParroquia',
+	'url'      => 'almordcomv2/combosnc?par=3',
+	'with' => "'pais='+document.getElementById('caordcom_codpai').value+'&estado='+document.getElementById('caordcom_codedo').value+'&municipio='+this.value"
+  ))));
+}
+?>
