7b7ff2d55abfb763a649e27faf8057c6ca0adc04
dmartinez <dmartinez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2011-01-19 15:10:53 +0000

    ajuste configurar el correlativo automatico ingresos aorozco
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@42096 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- apps/ingresos/modules/ingreging/templates/_refing.php
+++ apps/ingresos/modules/ingreging/templates/_refing.php
@@ -6,3 +6,13 @@
   //'onKeyPress'  => "javascript: enter(event,this.value);",
   'onBlur'  => "javascript: valor=this.value; if(valor!=''){ valor=valor.pad(8,'0',0); }else{ valor=valor.pad(8,'#',0) } $('cireging_refing').value=valor;$('cireging_refing').disabled=false;",
 )); echo $value ? $value : '&nbsp;' ?>
+<script language="JavaScript" type="text/javascript">
+var ids='<?php echo $cireging->getId()?>';
+  var correla='<?php echo $cireging->getMansolcor(); ?>';
+  if (correla=='S' && ids=='')
+  {
+  	$('cireging_refing').value='########';
+  	$('cireging_refing').readOnly=true;
+  }
+
+</script>
