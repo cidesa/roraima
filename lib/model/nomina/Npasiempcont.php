3564237be23d6dd10a8f6d04f984a02a1a82195f
cramirez <cramirez@bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3>
2011-02-24 17:50:50 +0000

    se modifico el grid para que no tardara tanto en cargar
    git-svn-id: https://repositorio.cidesa.com.ve/repositorio/web/php/siga/branches/productions/trunk@42760 bd7ca8a6-7b2b-0410-91d3-d9bcf4a077a3

--- lib/model/nomina/Npasiempcont.php
+++ lib/model/nomina/Npasiempcont.php
@@ -7,9 +7,9 @@
  *
  * @package    Roraima
  * @subpackage lib.model.nomina
- * @author     $Author$ <desarrollo@cidesa.com.ve>
- * @version SVN: $Id$
- * 
+ * @author     $Author: lhernandez $ <desarrollo@cidesa.com.ve>
+ * @version SVN: $Id: Npasiempcont.php 32413 2009-09-02 01:02:54Z lhernandez $
+ *
  * @copyright  Copyright 2007, Cide S.A.
  * @license    http://opensource.org/licenses/gpl-2.0.php GPLv2
  */
@@ -17,23 +17,17 @@ class Npasiempcont extends BaseNpasiempcont
 {
 	protected $montocalculado=0;
 	protected $codasi='';
-	  public function getDestipcon()
-  {
-  	  	  return Herramientas::getX('codtipcon','nptipcon','destipcon',self::getCodtipcon());
-  }
-
- /* public function getMontocalculado(){
-		$c= new Criteria();
-	    $c->add(NpasiempcontPeer::CODTIPCON,$this->npsalint->getCodcon(),Criteria::EQUAL);
-	    $c->add(NpasiempcontPeer::CODNOM,$this->npsalint->getCodnom(),Criteria::EQUAL);
-	    $c->addJoin(NpsalintPeer :: CODEMP, NpasiempcontPeer :: CODEMP);
-	    $per= NpasiempcontPeer::doSelectOne($c);
-	    if ($per){
-
-	    //$montocalculado= $per[0]->getMonasi() ;
-
-	    }
-  	return $montocalculado;
-  }*/
-
+        protected $objcon=array();
+	public function getDestipcon()
+        {
+          return Herramientas::getX('codtipcon','nptipcon','destipcon',self::getCodtipcon());
+        }
+        public function getNomnom()
+        {
+          return Herramientas::getX('Codnom','npnomina','nomnom',$this->codnom);
+        }
+        public function getNomemp()
+        {
+          return Herramientas::getX('codemp','nphojint','nomemp',self::getcodemp());
+        }
 }
